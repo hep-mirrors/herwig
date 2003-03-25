20f6d24933a637070e1caa9187d8654d92d9fb67
stephens <none@none>
2003-03-25 17:25:55 +0000

    Adding EnumParticles for Herwig specific particles

---
+++ Utilities/EnumParticles.h
@@ -0,0 +1,15 @@
+#ifndef HERWIG_EnumParticles_H
+#define HERWIG_EnumParticles_H
+
+#include "Pythia7/PDT/EnumParticles.h"
+
+namespace Herwig {
+namespace ExtraParticleID {
+enum ParticleCodes {
+  Cluster = 81 
+};
+
+}
+}
+
+#endif
