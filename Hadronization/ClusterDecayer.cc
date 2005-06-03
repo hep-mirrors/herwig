c58731506f1e5a90334bad93dc6d2cb15b4b3030
richardn <none@none>
2005-06-03 12:20:17 +0000

    ----------------------------------------------------------------------  Modified Files:  	ClusterDecayer.cc   changes to product on-shell particles if particle has a mass generator  ----------------------------------------------------------------------

--- Hadronization/ClusterDecayer.cc
+++ Hadronization/ClusterDecayer.cc
@@ -304,8 +304,14 @@ pair<PPtr,PPtr> ClusterDecayer::decayIntoTwoHadrons(tClusterPtr ptr)
 
   //cout << "Returned " << idPair.first << ", " << idPair.second << endl;
   // Create the two hadron particle objects with the specified id.
+  // temporary fix to product on-shell particles 
   PPtr ptrHad1 = getParticle(idPair.first);
   PPtr ptrHad2 = getParticle(idPair.second);
+  tPDPtr pdata= getParticleData(idPair.first);
+  PPtr ptrHad1 = pdata->produceParticle(pdata->mass());
+  pdata= getParticleData(idPair.second);
+  PPtr ptrHad2 = pdata->produceParticle(pdata->mass());
+
 
   if (!ptrHad1  ||  !ptrHad2) {
     ostringstream s;
