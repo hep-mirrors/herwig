59f679e14f8254b4539a56074d3cbb88c53b14e6
richardn <none@none>
2005-09-01 09:34:31 +0000

    * Decay/ScalarMeson/ScalarVectorVectorDecayer.icc (Repository): added new decayer for S -> VV

---
+++ Decay/ScalarMeson/ScalarVectorVectorDecayer.cc
@@ -0,0 +1,256 @@
+// -*- C++ -*-
+//
+// This is the implementation of the non-inlined, non-templated member
+// functions of the ScalarVectorVectorDecayer class.
+//
+
+#include "ScalarVectorVectorDecayer.h"
+#include "ThePEG/PDT/DecayMode.h"
+#include "ThePEG/Interface/ClassDocumentation.h"
+#include "ThePEG/Interface/ParVector.h"
+
+#ifdef ThePEG_TEMPLATES_IN_CC_FILE
+// #include "ScalarVectorVectorDecayer.tcc"
+#endif
+
+#include "ThePEG/Persistency/PersistentOStream.h"
+#include "ThePEG/Persistency/PersistentIStream.h"
+#include "Herwig++/Utilities/Kinematics.h"
+#include "ThePEG/PDT/DecayMode.h"
+#include "Herwig++/Helicity/WaveFunction/ScalarWaveFunction.h"
+#include "Herwig++/Helicity/WaveFunction/VectorWaveFunction.h"
+
+namespace Herwig {
+using namespace ThePEG;
+using ThePEG::Helicity::RhoDMatrix;
+using ThePEG::Helicity::LorentzPolarizationVector;
+using Herwig::Helicity::ScalarWaveFunction;
+using Herwig::Helicity::VectorWaveFunction;
+using Herwig::Helicity::incoming;
+using Herwig::Helicity::outgoing;
+
+
+inline ScalarVectorVectorDecayer::ScalarVectorVectorDecayer() 
+{
+  // f_0(1370) to rho rho
+  _incoming.push_back(10221);_outgoing1.push_back(113);_outgoing2.push_back(113);
+  _incoming.push_back(10221);_outgoing1.push_back(213);_outgoing2.push_back(-213);
+  _coupling.push_back(11.26/GeV);_maxweight.push_back(20.);
+  _coupling.push_back(15.92/GeV);_maxweight.push_back(20.);
+  // f_0(1500) to rho rho
+  _incoming.push_back(9030221);_outgoing1.push_back(113);_outgoing2.push_back(113);
+  _incoming.push_back(9030221);_outgoing1.push_back(213);_outgoing2.push_back(-213);
+  _coupling.push_back(1.691/GeV);_maxweight.push_back(20.);
+  _coupling.push_back(2.391/GeV);_maxweight.push_back(20.);
+  // chi_c0 decays
+  _incoming.push_back(10441);_outgoing1.push_back(443);_outgoing2.push_back(22);
+  _coupling.push_back(1./GeV);_maxweight.push_back(1.);
+  _incoming.push_back(10441);_outgoing1.push_back(323);_outgoing2.push_back(-323);
+  _incoming.push_back(10441);_outgoing1.push_back(313);_outgoing2.push_back(-313);
+  _coupling.push_back(1./GeV);_maxweight.push_back(1.);
+  _coupling.push_back(1./GeV);_maxweight.push_back(1.);
+  _incoming.push_back(10441);_outgoing1.push_back(333);_outgoing2.push_back(333);
+  _coupling.push_back(1./GeV);_maxweight.push_back(1.);
+  _incoming.push_back(10441);_outgoing1.push_back(22);_outgoing2.push_back(22);
+  _coupling.push_back(1./GeV);_maxweight.push_back(1.);
+}
+
+ScalarVectorVectorDecayer::~ScalarVectorVectorDecayer() {}
+
+bool ScalarVectorVectorDecayer::accept(const DecayMode & dm) const {
+  // is this mode allowed
+  bool allowed(false);
+  // check that at least some modes exist
+  if(_incoming.size()==0){return false;}
+  // must be two outgoing particles
+  if(dm.products().size()!=2){return allowed;}
+  // ids of the particles
+  int id0(dm.parent()->id());
+  ParticleMSet::const_iterator pit(dm.products().begin());
+  int id1((**pit).id());++pit;
+  int id2((**pit).id());
+  // loop over the modes and see if this is one of them
+  unsigned int ix=0;
+  do
+    {
+      if(_incoming[ix]==id0)
+	{if((_outgoing1[ix]==id1&&_outgoing2[ix]==id2)||
+	    (_outgoing1[ix]==id2&&_outgoing2[ix]==id1)){allowed=true;}}
+      ++ix;
+    }
+  while(!allowed&&ix<_incoming.size());
+  return allowed;
+}
+
+ParticleVector ScalarVectorVectorDecayer::decay(const DecayMode & dm,
+				  const Particle & parent) const {
+  ParticleVector children = dm.produceProducts();
+  // workout which mode we are doing
+  int imode(-1);
+  int id(parent.id());
+  ParticleMSet::const_iterator pit(dm.products().begin());
+  int id1((**pit).id());++pit;
+  int id2((**pit).id());
+  unsigned int ix(0);
+  do
+    {
+      if(_incoming[ix]==id)
+	{if((id1==_outgoing1[ix]&&id2==_outgoing2[ix])||
+	     (id2==_outgoing1[ix]&&id1==_outgoing2[ix])){imode=ix;}}
+      ++ix;
+    }
+  while(imode<0&&ix<_incoming.size());
+  // perform the decay
+  return generate(false,false,imode,parent);
+}
+
+
+void ScalarVectorVectorDecayer::persistentOutput(PersistentOStream & os) const {
+  os << _coupling << _incoming << _outgoing1 << _outgoing2 << _maxweight;
+}
+
+void ScalarVectorVectorDecayer::persistentInput(PersistentIStream & is, int) {
+  is >> _coupling >> _incoming >> _outgoing1 >> _outgoing2 >> _maxweight;
+}
+
+ClassDescription<ScalarVectorVectorDecayer> ScalarVectorVectorDecayer::initScalarVectorVectorDecayer;
+// Definition of the static class description member.
+
+void ScalarVectorVectorDecayer::Init() {
+
+  static ClassDocumentation<ScalarVectorVectorDecayer> documentation
+    ("The \\classname{ScalarVectorVectorDecayer} class is designed for"
+     " the decay of a pseduoscalar meson to two spin-1 particles.");
+
+  static ParVector<ScalarVectorVectorDecayer,int> interfaceIncoming
+    ("Incoming",
+     "The PDG code for the incoming particle",
+     &ScalarVectorVectorDecayer::_incoming,
+     0, 0, 0, -10000000, 10000000, false, false, true);
+
+  static ParVector<ScalarVectorVectorDecayer,int> interfaceOutcoming1
+    ("FirstOutgoing",
+     "The PDG code for the first outgoing particle",
+     &ScalarVectorVectorDecayer::_outgoing1,
+     0, 0, 0, -10000000, 10000000, false, false, true);
+
+  static ParVector<ScalarVectorVectorDecayer,int> interfaceOutcoming2
+    ("SecondOutgoing",
+     "The PDG code for the second outgoing particle",
+     &ScalarVectorVectorDecayer::_outgoing2,
+     0, 0, 0, -10000000, 10000000, false, false, true);
+
+  static ParVector<ScalarVectorVectorDecayer,InvEnergy> interfaceCoupling
+    ("Coupling",
+     "The coupling for the decay mode",
+     &ScalarVectorVectorDecayer::_coupling,
+     0, 0, 0, 0., 10000/GeV, false, false, true);
+
+  static ParVector<ScalarVectorVectorDecayer,double> interfaceMaxWeight
+    ("MaxWeight",
+     "The maximum weight for the decay mode",
+     &ScalarVectorVectorDecayer::_maxweight,
+     0, 0, 0, 0., 200., false, false, true);
+
+}
+
+double ScalarVectorVectorDecayer::me2(bool vertex, const int ichan,
+				      const Particle & inpart,
+				      const ParticleVector & decay) const
+{
+  // workaround for gcc 3.2.3 bug
+  //ALB ScalarWaveFunction(const_ptr_cast<tPPtr>(&inpart),incoming,true,vertex);
+  tPPtr mytempInpart = const_ptr_cast<tPPtr>(&inpart);
+  ScalarWaveFunction(mytempInpart,incoming,true,vertex);
+  // set up the spin info for the outgoing particles
+  bool photon[2]={false,false};
+  vector<LorentzPolarizationVector> wave[2];
+  for(unsigned int ix=0;ix<2;++ix)
+    {
+      if(decay[ix]->id()==ParticleID::gamma){photon[ix]=true;}
+
+      // workaround for gcc 3.2.3 bug
+      //ALB VectorWaveFunction(wave[ix],decay[ix],outgoing,true,photon[ix],vertex);
+      vector<LorentzPolarizationVector> mytempLPV; 
+      VectorWaveFunction(mytempLPV,decay[ix],outgoing,true,photon[ix],vertex);
+      wave[ix]=mytempLPV;
+    }
+  // now compute the matrix element
+  DecayMatrixElement newME(PDT::Spin0,PDT::Spin1,PDT::Spin1);
+  double fact(_coupling[imode()]/inpart.mass());
+  Energy p1p2(decay[0]->momentum()*decay[1]->momentum());
+  unsigned int ix,iy;
+  for(ix=0;ix<3;++ix)
+    {for(iy=0;iy<3;++iy)
+	{newME(0,ix,iy)=fact*(p1p2*wave[0][ix]*wave[1][iy]-
+			      (wave[1][iy]*decay[0]->momentum())*
+			      (wave[0][ix]*decay[1]->momentum()));}}
+  ME(newME);
+  RhoDMatrix rhoin(PDT::Spin0); rhoin.average();
+  return newME.contract(rhoin).real();
+}
+
+// output the setup info for the particle database
+void ScalarVectorVectorDecayer::dataBaseOutput(ofstream & output)
+{
+  output << "update decayers set parameters=\"";
+  // parameters for the DecayIntegrator base class
+  output << "set " << fullName() << ":Iteration " << _niter << "\n";
+  output << "set " << fullName() << ":Ntry " << _ntry << "\n";
+  output << "set " << fullName() << ":Points " << _npoint << "\n";
+  for(unsigned int ix=0;ix<_incoming.size();++ix)
+    {
+      if(ix<_initsize)
+	{
+	  output << "set " << fullName() << ":Incoming   " << ix << " "
+		 << _incoming[ix]   << "\n";
+	  output << "set " << fullName() << ":FirstOutgoing  " << ix << " "
+		 << _outgoing1[ix]  << "\n";
+	  output << "set " << fullName() << ":SecondOutgoing " << ix << " "
+		 << _outgoing2[ix]  << "\n";
+	  output << "set " << fullName() << ":Coupling   " << ix << " "
+		 << _coupling[ix]   << "\n";
+	  output << "set " << fullName() << ":MaxWeight  " << ix << " "
+		 << _maxweight[ix]  << "\n";
+	}
+      else
+	{
+	  output << "insert " << fullName() << ":Incoming   " << ix << " "
+		 << _incoming[ix]   << "\n";
+	  output << "insert " << fullName() << ":FirstOutgoing  " << ix << " "
+		 << _outgoing1[ix]  << "\n";
+	  output << "insert " << fullName() << ":SecondOutgoing " << ix << " "
+		 << _outgoing2[ix]  << "\n";
+	  output << "insert " << fullName() << ":Coupling   " << ix << " "
+		 << _coupling[ix]   << "\n";
+	  output << "insert " << fullName() << ":MaxWeight  " << ix << " "
+		 << _maxweight[ix]  << "\n";
+	}
+    }
+  output << "\n\" where BINARY ThePEGName=\"" << fullName() << "\";" << endl;
+}
+
+// specify the 1-2 matrix element to be used in the running width calculation
+bool ScalarVectorVectorDecayer::twoBodyMEcode(const DecayMode & dm, int & itype,
+					       double & coupling) const
+{
+  int imode(-1);
+  int id(dm.parent()->id());
+  ParticleMSet::const_iterator pit(dm.products().begin());
+  int id1((**pit).id());++pit;
+  int id2((**pit).id());
+  unsigned int ix(0);
+  do
+    {
+      if(_incoming[ix]==id)
+	{if((id1==_outgoing1[ix]&&id2==_outgoing2[ix])||
+	     (id2==_outgoing1[ix]&&id1==_outgoing2[ix])){imode=ix;}}
+      ++ix;
+    }
+  while(imode<0&&ix<_incoming.size());
+  coupling=_coupling[imode]*dm.parent()->mass();
+  itype = 12;
+  return id1==_outgoing1[imode]&&id2==_outgoing2[imode];
+}
+
+}
---
+++ Decay/ScalarMeson/ScalarVectorVectorDecayer.h
@@ -0,0 +1,271 @@
+// -*- C++ -*-
+#ifndef HERWIG_ScalarVectorVectorDecayer_H
+#define HERWIG_ScalarVectorVectorDecayer_H
+//
+// This is the declaration of the ScalarVectorVectorDecayer class.
+//
+
+#include "Herwig++/Decay/DecayIntegrator.h"
+#include "Herwig++/Decay/DecayPhaseSpaceMode.h"
+#include "ScalarVectorVectorDecayer.fh"
+
+namespace Herwig {
+
+using namespace ThePEG;
+
+/**
+ * The ScalarVectorVectorDecayer class is designed to perform the decay of 
+ * a scalar meson to two spin-1 particles. The matrix element is taken
+ * to have the form
+ * \f[\mathcal{M} = g \epsilon_1\cdot\epsilon_2\f] 
+ *  where \f$\epsilon_{1,2}\f$ are the polarzation
+ *  vectors of the outgoing vector particles.
+ *
+ * The incoming scalar meson, the outgoing vectors and the coupling can
+ * be specified using the relevant interfaces.
+ *
+ * @see DecayIntegrator
+ */
+class ScalarVectorVectorDecayer: public DecayIntegrator {
+
+public:
+
+  /** @name Standard constructors and destructors. */
+  //@{
+  /**
+   * The default constructor.
+   */
+  inline ScalarVectorVectorDecayer();
+
+  /**
+   * The copy constructor.
+   */
+  inline ScalarVectorVectorDecayer(const ScalarVectorVectorDecayer &);
+
+  /**
+   * The destructor.
+   */
+  virtual ~ScalarVectorVectorDecayer();
+  //@}
+
+public:
+
+  /** @name Virtual functions required by the Decayer and DecayIntegrator classes. */
+  //@{
+  /**
+   * Check if this decayer can perfom the decay specified by the
+   * given decay mode.
+   * @param dm the DecayMode describing the decay.
+   * @return true if this decayer can handle the given mode, otherwise false.
+   */
+  virtual bool accept(const DecayMode & dm) const;
+
+  /**
+   * Perform a decay for a given DecayMode and a given Particle instance.
+   * @param dm the DecayMode describing the decay.
+   * @param p the Particle instance to be decayed.
+   * @return a ParticleVector containing the decay products.
+   */
+  virtual ParticleVector decay(const DecayMode & dm, const Particle & p) const;
+
+  /**
+   * Return the matrix element squared for a given mode and phase-space channel.
+   * @param vertex Output the information on the vertex for spin correlations
+   * @param ichan The channel we are calculating the matrix element for. 
+   * @param part The decaying Particle.
+   * @param decay The particles produced in the decay.
+   * @return The matrix element squared for the phase-space configuration.
+   */
+  double me2(bool vertex, const int ichan,const Particle & part,
+	     const ParticleVector & decay) const;
+
+  /**
+   * Specify the \f$1\to2\f$ matrix element to be used in the running width calculation.
+   * @param dm The DecayMode
+   * @param mecode The code for the matrix element as described
+   *               in the GenericWidthGenerator class, in this case 3.
+   * @param coupling The coupling for the matrix element.
+   * @return True or False if this mode can be handled.
+   */
+  bool twoBodyMEcode(const DecayMode & dm, int & mecode, double & coupling) const;
+
+  /**
+   * Output the setup information for the particle database
+   */
+  void dataBaseOutput(ofstream &);
+  //@}
+
+public:
+
+  /** @name Functions used by the persistent I/O system. */
+  //@{
+  /**
+   * Function used to write out object persistently.
+   * @param os the persistent output stream written to.
+   */
+  void persistentOutput(PersistentOStream & os) const;
+
+  /**
+   * Function used to read in object persistently.
+   * @param is the persistent input stream read from.
+   * @param version the version number of the object when written.
+   */
+  void persistentInput(PersistentIStream & is, int version);
+  //@}
+
+  /**
+   * The standard Init function used to initialize the interfaces.
+   * Called exactly once for each class by the class description system
+   * before the main function starts or
+   * when this class is dynamically loaded.
+   */
+  static void Init();
+
+protected:
+
+  /** @name Clone Methods. */
+  //@{
+  /**
+   * Make a simple clone of this object.
+   * @return a pointer to the new object.
+   */
+  inline virtual IBPtr clone() const;
+
+  /** Make a clone of this object, possibly modifying the cloned object
+   * to make it sane.
+   * @return a pointer to the new object.
+   */
+  inline virtual IBPtr fullclone() const;
+  //@}
+
+protected:
+
+  /** @name Standard Interfaced functions. */
+  //@{
+  /**
+   * Check sanity of the object during the setup phase.
+   */
+  inline virtual void doupdate() throw(UpdateException);
+
+  /**
+   * Initialize this object after the setup phase before saving an
+   * EventGenerator to disk.
+   * @throws InitException if object could not be initialized properly.
+   */
+  inline virtual void doinit() throw(InitException);
+
+  /**
+   * Initialize this object. Called in the run phase just before
+   * a run begins.
+   */
+  inline virtual void doinitrun();
+
+  /**
+   * Finalize this object. Called in the run phase just after a
+   * run has ended. Used eg. to write out statistics.
+   */
+  inline virtual void dofinish();
+
+  /**
+   * Rebind pointer to other Interfaced objects. Called in the setup phase
+   * after all objects used in an EventGenerator has been cloned so that
+   * the pointers will refer to the cloned objects afterwards.
+   * @param trans a TranslationMap relating the original objects to
+   * their respective clones.
+   * @throws RebindException if no cloned object was found for a given
+   * pointer.
+   */
+  inline virtual void rebind(const TranslationMap & trans)
+    throw(RebindException);
+
+  /**
+   * Return a vector of all pointers to Interfaced objects used in this
+   * object.
+   * @return a vector of pointers.
+   */
+  inline virtual IVector getReferences();
+  //@}
+
+private:
+
+  /**
+   * The static object used to initialize the description of this class.
+   * Indicates that this is a concrete class with persistent data.
+   */
+  static ClassDescription<ScalarVectorVectorDecayer> initScalarVectorVectorDecayer;
+
+  /**
+   * The assignment operator is private and must never be called.
+   * In fact, it should not even be implemented.
+   */
+  ScalarVectorVectorDecayer & operator=(const ScalarVectorVectorDecayer &);
+
+private:
+
+  /**
+   * the PDG code for the incoming particle
+   */
+  vector<int> _incoming;
+
+  /**
+   * the PDG code for the first outgoing particle
+   */
+  vector<int> _outgoing1;
+
+  /**
+   * the PDG code for the second outgoing particle
+   */
+  vector<int> _outgoing2;
+
+  /**
+   * the coupling for the decay, \f$g\f$.
+   */
+  vector<InvEnergy> _coupling;
+
+  /**
+   * the maximum weight for the decay
+   */
+  vector<double> _maxweight;
+
+  /**
+   *  initial number of modes
+   */
+  unsigned int _initsize;
+
+};
+
+}
+
+#include "ThePEG/Utilities/ClassTraits.h"
+
+namespace ThePEG {
+
+/** This template specialization informs ThePEG about the
+ *  base classes of ScalarVectorVectorDecayer. */
+template <>
+struct BaseClassTrait<Herwig::ScalarVectorVectorDecayer,1> {
+  /** Typedef of the first base class of ScalarVectorVectorDecayer. */
+  typedef Herwig::DecayIntegrator NthBase;
+};
+
+/** This template specialization informs ThePEG about the name of
+ *  the ScalarVectorVectorDecayer class and the shared object where it is defined. */
+template <>
+struct ClassTraits<Herwig::ScalarVectorVectorDecayer>
+  : public ClassTraitsBase<Herwig::ScalarVectorVectorDecayer> {
+  /** Return a platform-independent class name */
+  static string className() { return "Herwig++::ScalarVectorVectorDecayer"; }
+  /** Return the name of the shared library be loaded to get
+   *  access to the ScalarVectorVectorDecayer class and every other class it uses
+   *  (except the base class). */
+  static string library() { return "libHwSMDecay.so"; }
+};
+
+}
+
+#include "ScalarVectorVectorDecayer.icc"
+#ifndef ThePEG_TEMPLATES_IN_CC_FILE
+// #include "ScalarVectorVectorDecayer.tcc"
+#endif
+
+#endif /* HERWIG_ScalarVectorVectorDecayer_H */
