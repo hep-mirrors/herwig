f03655d1f1f95347f755bea4065ccf6e9acd54c6
richardn <none@none>
2005-09-13 08:58:24 +0000

    *** empty log message ***

---
+++ Decay/HwDecayerBase.cc
@@ -0,0 +1,73 @@
+// -*- C++ -*-
+//
+// This is the implementation of the non-inlined, non-templated member
+// functions of the HwDecayerBase class.
+//
+
+#include "HwDecayerBase.h"
+#include "ThePEG/Interface/ClassDocumentation.h"
+#include "ThePEG/PDT/DecayMode.h"
+#include "ThePEG/Interface/Switch.h"
+
+#ifdef ThePEG_TEMPLATES_IN_CC_FILE
+// #include "HwDecayerBase.tcc"
+#endif
+
+#include "ThePEG/Persistency/PersistentOStream.h"
+#include "ThePEG/Persistency/PersistentIStream.h"
+
+using namespace Herwig;
+
+HwDecayerBase::~HwDecayerBase() {}
+
+bool HwDecayerBase::accept(const DecayMode & dm) const {
+  return false;
+}
+
+ParticleVector HwDecayerBase::decay(const DecayMode & dm,
+				  const Particle & parent) const {
+  ParticleVector children = dm.produceProducts();
+  return children;
+}
+
+
+void HwDecayerBase::persistentOutput(PersistentOStream & os) const {
+  os << _initialize;
+}
+
+void HwDecayerBase::persistentInput(PersistentIStream & is, int) {
+  is >> _initialize;
+}
+
+AbstractClassDescription<HwDecayerBase> HwDecayerBase::initHwDecayerBase;
+// Definition of the static class description member.
+
+void HwDecayerBase::Init() {
+
+  static ClassDocumentation<HwDecayerBase> documentation
+    ("The HwDecayerBase class is the base class for Decayers in Hw++.");
+
+  static Switch<HwDecayerBase,bool> interfaceInitialize
+    ("Initialize",
+     "Initialization of the phase space calculation",
+     &HwDecayerBase::_initialize, false, false, false);
+  static SwitchOption interfaceInitializeon
+    (interfaceInitialize,
+     "on",
+     "At initialisation find max weight and optimise the integration",
+     true);
+  static SwitchOption interfaceInitializeoff
+    (interfaceInitialize,
+     "off",
+     "Use the maximum weight and channel weights supplied for the integration",
+     false);
+
+}
+
+void HwDecayerBase::dataBaseOutput(ofstream & output,bool header) const
+{
+  // header for MySQL
+  if(header){output << "update decayers set parameters=\"";}
+  // footer for MySQL
+  if(header){output << " where ThePEGName=\" " << fullName() << "\";";}
+}
---
+++ Decay/HwDecayerBase.h
@@ -0,0 +1,218 @@
+// -*- C++ -*-
+#ifndef HERWIG_HwDecayerBase_H
+#define HERWIG_HwDecayerBase_H
+//
+// This is the declaration of the HwDecayerBase class.
+//
+
+#include "ThePEG/PDT/Decayer.h"
+#include "HwDecayerBase.fh"
+
+namespace Herwig {
+
+using namespace ThePEG;
+
+/**
+ * The HwDecayerBase class is the base class for Decayers in Herwig++. It inherits
+ * from the Decayer class of ThePEG and implements additional functionality for the 
+ * output of the results to the particle database and initialization of the datbase.
+ *
+ */
+class HwDecayerBase: public Decayer {
+
+public:
+
+  /** @name Standard constructors and destructors. */
+  //@{
+  /**
+   * The default constructor.
+   */
+  inline HwDecayerBase();
+
+  /**
+   * The copy constructor.
+   */
+  inline HwDecayerBase(const HwDecayerBase &);
+
+  /**
+   * The destructor.
+   */
+  virtual ~HwDecayerBase();
+  //@}
+
+public:
+
+  /** @name Virtual functions required by the Decayer class. */
+  //@{
+  /**
+   * Check if this decayer can perfom the decay specified by the
+   * given decay mode.
+   * @param dm the DecayMode describing the decay.
+   * @return true if this decayer can handle the given mode, otherwise false.
+   */
+  virtual bool accept(const DecayMode & dm) const;
+
+  /**
+   * Perform a decay for a given DecayMode and a given Particle instance.
+   * @param dm the DecayMode describing the decay.
+   * @param p the Particle instance to be decayed.
+   * @return a ParticleVector containing the decay products.
+   */
+  virtual ParticleVector decay(const DecayMode & dm, const Particle & p) const;
+  //@}
+
+public:
+
+  /** @name Functions used by the persistent I/O system. */
+  //@{
+  /**
+   * Function used to write out object persistently.
+   * @param os the persistent output stream written to.
+   */
+  void persistentOutput(PersistentOStream & os) const;
+
+  /**
+   * Function used to read in object persistently.
+   * @param is the persistent input stream read from.
+   * @param version the version number of the object when written.
+   */
+  void persistentInput(PersistentIStream & is, int version);
+  //@}
+
+  /**
+   * The standard Init function used to initialize the interfaces.
+   * Called exactly once for each class by the class description system
+   * before the main function starts or
+   * when this class is dynamically loaded.
+   */
+  static void Init();
+
+public:
+
+  /**
+   *  Functions for the Herwig decayer
+   */
+  //@{
+
+  /**
+   * Output the setup information for the particle database
+   * @param os The stream to output the information to
+   * @param header Whether or not to output the information for MySQL
+   */
+  virtual void dataBaseOutput(ofstream & os,bool header) const;
+
+  /**
+   *  Access to the initialize variable
+   */
+  inline bool initialize() const;
+  //@}
+protected:
+
+  /** @name Standard Interfaced functions. */
+  //@{
+  /**
+   * Check sanity of the object during the setup phase.
+   */
+  inline virtual void doupdate() throw(UpdateException);
+
+  /**
+   * Initialize this object after the setup phase before saving an
+   * EventGenerator to disk.
+   * @throws InitException if object could not be initialized properly.
+   */
+  inline virtual void doinit() throw(InitException);
+
+  /**
+   * Initialize this object. Called in the run phase just before
+   * a run begins.
+   */
+  inline virtual void doinitrun();
+
+  /**
+   * Finalize this object. Called in the run phase just after a
+   * run has ended. Used eg. to write out statistics.
+   */
+  inline virtual void dofinish();
+
+  /**
+   * Rebind pointer to other Interfaced objects. Called in the setup phase
+   * after all objects used in an EventGenerator has been cloned so that
+   * the pointers will refer to the cloned objects afterwards.
+   * @param trans a TranslationMap relating the original objects to
+   * their respective clones.
+   * @throws RebindException if no cloned object was found for a given
+   * pointer.
+   */
+  inline virtual void rebind(const TranslationMap & trans)
+    throw(RebindException);
+
+  /**
+   * Return a vector of all pointers to Interfaced objects used in this
+   * object.
+   * @return a vector of pointers.
+   */
+  inline virtual IVector getReferences();
+  //@}
+
+private:
+
+  /**
+   * The static object used to initialize the description of this class.
+   * Indicates that this is an abstract class with persistent data.
+   */
+  static AbstractClassDescription<HwDecayerBase> initHwDecayerBase;
+
+  /**
+   * The assignment operator is private and must never be called.
+   * In fact, it should not even be implemented.
+   */
+  HwDecayerBase & operator=(const HwDecayerBase &);
+
+private:
+
+  /**
+   * perform initialisation
+   */
+  bool _initialize;
+
+};
+
+}
+
+#include "ThePEG/Utilities/ClassTraits.h"
+
+namespace ThePEG {
+
+/** @cond TRAITSPECIALIZATIONS */
+
+/** This template specialization informs ThePEG about the
+ *  base classes of HwDecayerBase. */
+template <>
+struct BaseClassTrait<Herwig::HwDecayerBase,1> {
+  /** Typedef of the first base class of HwDecayerBase. */
+  typedef Decayer NthBase;
+};
+
+/** This template specialization informs ThePEG about the name of
+ *  the HwDecayerBase class and the shared object where it is defined. */
+template <>
+struct ClassTraits<Herwig::HwDecayerBase>
+  : public ClassTraitsBase<Herwig::HwDecayerBase> {
+  /** Return a platform-independent class name */
+  static string className() { return "Herwig::HwDecayerBase"; }
+  /** Return the name of the shared library be loaded to get
+   *  access to the HwDecayerBase class and every other class it uses
+   *  (except the base class). */
+  static string library() { return "HwDecayerBase.so"; }
+};
+
+/** @endcond */
+
+}
+
+#include "HwDecayerBase.icc"
+#ifndef ThePEG_TEMPLATES_IN_CC_FILE
+// #include "HwDecayerBase.tcc"
+#endif
+
+#endif /* HERWIG_HwDecayerBase_H */
