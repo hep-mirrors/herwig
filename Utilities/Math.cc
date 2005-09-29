ffce91a9e60a8fb86a82d645f6fb380d93b923b1
dgrell <none@none>
2005-09-29 14:53:51 +0000

    Activated Mambo decayer for 5-body decays

--- Utilities/Math.cc
+++ Utilities/Math.cc
@@ -5,18 +5,18 @@ namespace Herwig {
 namespace Math {
 using ThePEG::Complex;
 
-  namespace {
-Complex Li2Prod(Complex y,Complex y2)
-{
-  static double a1   =-0.250000000000000e0,a2   =-0.111111111111111e0;
-  static double a3   =-0.010000000000000e0,a4   =-0.017006802721088e0;
-  static double a5   =-0.019444444444444e0,a6   =-0.020661157024793e0;
-  static double a7   =-0.021417300648069e0,a8   =-0.021948866377231e0;
-  static double a9   =-0.022349233811171e0,a10  =-0.022663689135191e0;
-  return y*(1.+a1*y*(1.+a2*y*(1.+a3*y2*(1.+a4*y2*(1.+a5*y2*
-	(1.+a6*y2*(1.+a7*y2*(1.+a8*y2*(1.+a9*y2*(1.+a10*y2))))))))));
-}
+namespace {
+  inline Complex Li2Prod(Complex y,Complex y2)
+  {
+    static const double a1   =-0.250000000000000e0,a2   =-0.111111111111111e0;
+    static const double a3   =-0.010000000000000e0,a4   =-0.017006802721088e0;
+    static const double a5   =-0.019444444444444e0,a6   =-0.020661157024793e0;
+    static const double a7   =-0.021417300648069e0,a8   =-0.021948866377231e0;
+    static const double a9   =-0.022349233811171e0,a10  =-0.022663689135191e0;
+    return y*(1.+a1*y*(1.+a2*y*(1.+a3*y2*(1.+a4*y2*(1.+a5*y2*
+	   (1.+a6*y2*(1.+a7*y2*(1.+a8*y2*(1.+a9*y2*(1.+a10*y2))))))))));
   }
+}
 
 Complex Li2(Complex x)
 {
