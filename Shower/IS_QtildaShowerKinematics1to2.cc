133473555472bcd4f3b27b8c4dae5edac595cd3a
dgrell <none@none>
2006-05-12 13:11:54 +0000

    Fixed #35 in old Shower

--- Shower/IS_QtildaShowerKinematics1to2.cc
+++ Shower/IS_QtildaShowerKinematics1to2.cc
@@ -55,7 +55,7 @@ updateParent(const tShowerParticlePtr theParent,
   }
   double mz = 1.-c1->showerKinematics()->z();
   double cphi = cos(c1->showerKinematics()->phi());
-  double sphi = sqrt(1.-sqr(cphi));
+  double sphi = sin(c1->showerKinematics()->phi());
 //   Energy pt = sqrt(sqr((1.-mz)*c1->showerKinematics()->qtilde()) - 
 // 		   (1.-mz)*sqr(kinCutoff));
   Energy pt = sqrt(sqr(mz*c1->showerKinematics()->qtilde()) - 
