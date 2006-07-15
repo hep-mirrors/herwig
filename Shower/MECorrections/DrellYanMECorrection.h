d3e9b6cb8bc55860657ee637efe01005057dda0f
richardn <none@none>
2006-07-15 09:10:24 +0000

    changes to rename the libraries now only one shower

--- Shower/MECorrections/DrellYanMECorrection.h
+++ Shower/MECorrections/DrellYanMECorrection.h
@@ -205,7 +205,7 @@ struct ClassTraits<Herwig::DrellYanMECorrection>
    * excepted). In this case the listed libraries will be dynamically
    * linked in the order they are specified.
    */
-  static string library() { return "HwNewShower.so"; }
+  static string library() { return "HwShower.so"; }
 };
 
 /** @endcond */
--- Shower/MECorrections/MECorrectionBase.h
+++ Shower/MECorrections/MECorrectionBase.h
@@ -183,7 +183,7 @@ struct ClassTraits<Herwig::MECorrectionBase>
    * excepted). In this case the listed libraries will be dynamically
    * linked in the order they are specified.
    */
-  static string library() { return "HwNewShower.so"; }
+  static string library() { return "HwShower.so"; }
 };
 
 /** @endcond */
