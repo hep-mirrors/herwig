0f4feedec8f058cf199fbf1c6fd9dad748da4337
richardn <none@none>
2006-07-18 13:13:22 +0000

    change to class name for ThePEG

--- PDF/MRST.h
+++ PDF/MRST.h
@@ -337,7 +337,7 @@ struct BaseClassTrait<Herwig::MRST,1> {
 template <>
 struct ClassTraits<Herwig::MRST>: public ClassTraitsBase<Herwig::MRST> {
   /** Return a platform-independent class name */
-  static string className() { return "Herwig++::PDF::MRST"; }
+  static string className() { return "Herwig++::MRST"; }
   /**
    * The name of a file containing the dynamic library where the class
    * MRST is implemented. It may also include several, space-separated,
