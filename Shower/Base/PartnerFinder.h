d6ded7cb0d8e32523f044f77c1b7ae20b18aed94
richardn <none@none>
2006-09-07 09:53:48 +0000

    new structure for PartnerFinder implemented
    --HG--
    branch : nason

--- Shower/Base/PartnerFinder.h
+++ Shower/Base/PartnerFinder.h
@@ -4,8 +4,8 @@
 //
 // This is the declaration of the PartnerFinder class.
 //
-
 #include "Herwig++/Shower/ShowerConfig.h"
+#include "Herwig++/Shower/Evolver.fh"
 #include "ThePEG/Interface/Interfaced.h"
 #include "Herwig++/Shower/ShowerVariables.h"
 #include "PartnerFinder.fh"
@@ -22,86 +22,51 @@ typedef pair<tShowerParticlePtr,tShowerParticlePtr> ShowerPPair;
 /** \ingroup Shower
  *
  *  This class is responsible of two related tasks: 
- *  -#   it finds the partners (pairs of ShowerParticle objects)
- *       for each interaction types (QCD,QED,EWK...) and within the
- *       scale range specified by the ShowerVariables object;
- *
- *  -#   for each pair of partners (and interaction therefore)
+ *  -  it finds the partners (pairs of ShowerParticle objects)
+ *      for each interaction types (QCD,QED,EWK...).
+ *  -   for each pair of partners (and interaction therefore)
  *       it sets the initial evolution scales of both of them.
+ *
+ *  In general the finding of the partners is performed by this class but
+ *  the calculation of the initial evolution scales should be implemented
+ *  for different shower evolution models in classes inheriting from this one.
  *  Notice that a given particle has, in general, a different partner
  *  for each different interaction; however, given a partner, its 
  *  initial evolution scale, Q, is purely a kinematical relationship 
  *  between the pair, without dependence on the dynamics (i.e. type of interaction).
- *  Therefore, there must be different methods for finding partners of different
- *  interaction types, but an unique common method to calculate the initial evolving
- *  showering scale.
- *  Notice that:
- *  -    to be more general, one should define an abstract class 
- *       AbsPartnerFinder which has, exactly like the present PartnerFinder
- *       class, the definition of the methods: 
- *       - setQCDInitialEvolutionScales
- *       - setQEDInitialEvolutionScales 
- *       - setEWKInitialEvolutionScales 
- *     which are quite general, whereas the other one is declared pure virtual, 
- *     without definition: 
- *       - virtual ...  calculateInitialEvolutionScales() = 0; 
- *     and then having a concrete PartonFinder class which inherits
- *     from AbsPartnerFinder and provides a definition for this virtual method. 
- *     In fact, it is only in this method that a specific choice of ordering 
- *     variable must be made. Therefore, if we wanted a different choice, 
- *     we could define another class, AlternativePartonFinder, which also 
- *     inherits from AbsPartnerFinder, but provides a different definition 
- *     of the method calculateInitialEvolutionScales.
- *
- *  The following changes to this class need to be considered
- *  - If the method  calculateInitialEvolutionScales
- *    is not used anywhere other than anthoer part of this class,
- *    then it should be moved in the private part of the class.
- *  - It could be easy and straightforward to add to 
- *    setQCDInitialEvolutionScales  
- *    (and, maybe, but less likely, also for the other ones)
- *    either a pointer to the Collision object (which is
- *    already available in the class InsideRangeShowerEvolver
- *    that calls this class Partner Finder) --- in the
- *    case some information from the hard subprocess is
- *    necessary to find colour partners (expecially in the
- *    case of baryon number nonconserving processes in 
- *    Rp violating Susy) --- and/or a boolean flag that
- *    tells whether or not the input particles are
- *    associates to a decay --- and therefore finding
- *    the colour partners, at least for the scale of
- *    the decay, should not involved searches through
- *    the particle history.
  *
  * @see \ref PartnerFinderInterfaces "The interfaces"
  * defined for PartnerFinder.
  */
 class PartnerFinder: public Interfaced {
 
+  /**
+   *  Evolver is a friend to set the showerVariables
+   */
+  friend class Evolver;
+
 public:
 
-  /** @name Standard constructors and destructors. */
-  //@{
   /**
    * The default constructor.
    */
   inline PartnerFinder();
-  //@}
-
-public:
 
   /**
    * Given in input a collection of particles (ShowerParticle objects),
    * each of these methods set the initial evolution scales of those particles, 
    * between the ones given in input, that do not have yet their evolution scale set, 
-   * according to a given interaction type (QCD, QED, EWK,...), and within the 
-   * constraints specified in the showerVariables object. 
+   * according to a given interaction type (QCD, QED, EWK,...). 
    * The input collection of particles can be either the full collection of 
    * showering particles (kept in the main class ShowerHandler,
    * in the case isDecayCase is false, or simply, in the case isDecayCase 
    * is true, the decaying particle and its decay products.    
    * The methods returns true, unless something wrong (inconsistencies,
    * or undefined values) happens.
+   *
+   * These methods are virtual but in most cases inheriting classes should not
+   * need to overide them as they simply find the relevant partner and call
+   * one of the calculateScale members to calculate the scale.
    */
   //@{
   /**
@@ -109,7 +74,7 @@ public:
    * @param particles        The particles to be considered
    * @param isDecayCase      Whether or not this is a decay
    */
-  bool setQCDInitialEvolutionScales(const ShowerParticleVector &particles,
+  virtual bool setQCDInitialEvolutionScales(const ShowerParticleVector &particles,
                                     const bool isDecayCase);
 
   /**
@@ -117,7 +82,7 @@ public:
    * @param particles        The particles to be considered
    * @param isDecayCase      Whether or not this is a decay
    */
-  bool setQEDInitialEvolutionScales(const ShowerParticleVector &particles,
+  virtual bool setQEDInitialEvolutionScales(const ShowerParticleVector &particles,
                                     const bool isDecayCase);
 
   /**
@@ -125,46 +90,9 @@ public:
    * @param particles        The particles to be considered
    * @param isDecayCase      Whether or not this is a decay
    */
-  bool setEWKInitialEvolutionScales(const ShowerParticleVector &particles,
-                                    const bool isDecayCase);
+  virtual bool setEWKInitialEvolutionScales(const ShowerParticleVector &particles,
+					    const bool isDecayCase);
   //@}
-
-  /**
-   * Given a pair of particles, supposedly partners w.r.t. an interaction,
-   * this method returns their initial evolution scales as a pair.
-   * If something wrong happens, it returns the null (Energy(),Energy()) pair. 
-   * This method is used by the above setXXXInitialEvolutionScales 
-   * methods.
-   */
-  //@{
-  /**
-   *  General method to calculate the initial evolution scales
-   */
-  pair<Energy,Energy> calculateInitialEvolutionScales(const ShowerPPair &,
-                                           const bool isDecayCase);
-
-  /**
-   *  Calculate the initial evolution scales for two final-state particles
-   */
-  pair<Energy,Energy> calculateFinalFinalScales(const ShowerPPair &);
-
-  /**
-   *  Calculate the initial evolution scales for two initial-state particles
-   */
-  pair<Energy,Energy> calculateInitialInitialScales(const ShowerPPair &);
-
-  /**
-   *  Calculate the initial evolution scales for one initial 
-   *  and one final-state particles
-   */
-  pair<Energy,Energy> calculateInitialFinalScales(const ShowerPPair &,
-                                       const bool isDecayCase);
-  //@}
-
-  /**
-   *  Set the shower variables, only used by Evolver in doinit
-   */
-  inline void setShowerVariables(ShowerVarsPtr);
 
 public:
 
@@ -194,20 +122,48 @@ public:
 
 protected:
 
-  /** @name Clone Methods. */
+  /**
+   * Given a pair of particles, supposedly partners w.r.t. an interaction,
+   * this method returns their initial evolution scales as a pair.
+   * If something wrong happens, it returns the null (Energy(),Energy()) pair. 
+   * This method is used by the above setXXXInitialEvolutionScales 
+   * methods.
+   * These methods must be overiden in inheriting classes
+   */
   //@{
   /**
-   * Make a simple clone of this object.
-   * @return a pointer to the new object.
+   *  General method to calculate the initial evolution scales
    */
-  inline virtual IBPtr clone() const;
+  virtual pair<Energy,Energy> calculateInitialEvolutionScales(const ShowerPPair &,
+                                           const bool isDecayCase);
 
-  /** Make a clone of this object, possibly modifying the cloned object
-   * to make it sane.
-   * @return a pointer to the new object.
+  /**
+   *  Calculate the initial evolution scales for two final-state particles
    */
-  inline virtual IBPtr fullclone() const;
+  virtual pair<Energy,Energy> calculateFinalFinalScales(const ShowerPPair &)=0;
+
+  /**
+   *  Calculate the initial evolution scales for two initial-state particles
+   */
+  virtual pair<Energy,Energy> calculateInitialInitialScales(const ShowerPPair &)=0;
+
+  /**
+   *  Calculate the initial evolution scales for one initial 
+   *  and one final-state particles
+   */
+  virtual pair<Energy,Energy> calculateInitialFinalScales(const ShowerPPair &,
+							  const bool isDecayCase)=0;
   //@}
+
+  /**
+   *  Set the shower variables, only used by Evolver in doinit
+   */
+  inline void setShowerVariables(ShowerVarsPtr);
+
+  /**
+   *  Access to the ShowerVariables in inheriting classes
+   */
+  inline tShowerVarsPtr showerVariables() const;
 
 private:
 
@@ -215,7 +171,7 @@ private:
    * The static object used to initialize the description of this class.
    * Indicates that this is a concrete class with persistent data.
    */
-  static ClassDescription<PartnerFinder> initPartnerFinder;
+  static AbstractClassDescription<PartnerFinder> initPartnerFinder;
 
   /**
    * The assignment operator is private and must never be called.
--- Shower/Fortran/FortranFinder.h
+++ Shower/Fortran/FortranFinder.h
@@ -1,14 +1,14 @@
 // -*- C++ -*-
-#ifndef HERWIG_PartnerFinder_H
-#define HERWIG_PartnerFinder_H
+#ifndef HERWIG_FortranFinder_H
+#define HERWIG_FortranFinder_H
 //
-// This is the declaration of the PartnerFinder class.
+// This is the declaration of the FortranFinder class.
 //
 
 #include "Herwig++/Shower/ShowerConfig.h"
-#include "ThePEG/Interface/Interfaced.h"
+#include "Herwig++/Shower/Base/PartnerFinder.h"
 #include "Herwig++/Shower/ShowerVariables.h"
-#include "PartnerFinder.fh"
+#include "FortranFinder.fh"
 
 namespace Herwig {
 
@@ -21,113 +21,30 @@ typedef pair<tShowerParticlePtr,tShowerParticlePtr> ShowerPPair;
 
 /** \ingroup Shower
  *
- *  This class is responsible of two related tasks: 
- *  -#   it finds the partners (pairs of ShowerParticle objects)
- *       for each interaction types (QCD,QED,EWK...) and within the
- *       scale range specified by the ShowerVariables object;
+ *  The FortranFinder class is responsible for finding the partners and setting the 
+ *  initial evolution scales for the original FORTRAN HERWIG shower evolution.
  *
- *  -#   for each pair of partners (and interaction therefore)
- *       it sets the initial evolution scales of both of them.
- *  Notice that a given particle has, in general, a different partner
- *  for each different interaction; however, given a partner, its 
- *  initial evolution scale, Q, is purely a kinematical relationship 
- *  between the pair, without dependence on the dynamics (i.e. type of interaction).
- *  Therefore, there must be different methods for finding partners of different
- *  interaction types, but an unique common method to calculate the initial evolving
- *  showering scale.
- *  Notice that:
- *  -    to be more general, one should define an abstract class 
- *       AbsPartnerFinder which has, exactly like the present PartnerFinder
- *       class, the definition of the methods: 
- *       - setQCDInitialEvolutionScales
- *       - setQEDInitialEvolutionScales 
- *       - setEWKInitialEvolutionScales 
- *     which are quite general, whereas the other one is declared pure virtual, 
- *     without definition: 
- *       - virtual ...  calculateInitialEvolutionScales() = 0; 
- *     and then having a concrete PartonFinder class which inherits
- *     from AbsPartnerFinder and provides a definition for this virtual method. 
- *     In fact, it is only in this method that a specific choice of ordering 
- *     variable must be made. Therefore, if we wanted a different choice, 
- *     we could define another class, AlternativePartonFinder, which also 
- *     inherits from AbsPartnerFinder, but provides a different definition 
- *     of the method calculateInitialEvolutionScales.
- *
- *  The following changes to this class need to be considered
- *  - If the method  calculateInitialEvolutionScales
- *    is not used anywhere other than anthoer part of this class,
- *    then it should be moved in the private part of the class.
- *  - It could be easy and straightforward to add to 
- *    setQCDInitialEvolutionScales  
- *    (and, maybe, but less likely, also for the other ones)
- *    either a pointer to the Collision object (which is
- *    already available in the class InsideRangeShowerEvolver
- *    that calls this class Partner Finder) --- in the
- *    case some information from the hard subprocess is
- *    necessary to find colour partners (expecially in the
- *    case of baryon number nonconserving processes in 
- *    Rp violating Susy) --- and/or a boolean flag that
- *    tells whether or not the input particles are
- *    associates to a decay --- and therefore finding
- *    the colour partners, at least for the scale of
- *    the decay, should not involved searches through
- *    the particle history.
- *
- * @see \ref PartnerFinderInterfaces "The interfaces"
- * defined for PartnerFinder.
+ * @see \ref FortranFinderInterfaces "The interfaces"
+ * defined for FortranFinder.
  */
-class PartnerFinder: public Interfaced {
+class FortranFinder: public PartnerFinder {
 
 public:
 
-  /** @name Standard constructors and destructors. */
-  //@{
   /**
    * The default constructor.
    */
-  inline PartnerFinder();
-  //@}
-
-public:
+  inline FortranFinder();
 
   /**
-   * Given in input a collection of particles (ShowerParticle objects),
-   * each of these methods set the initial evolution scales of those particles, 
-   * between the ones given in input, that do not have yet their evolution scale set, 
-   * according to a given interaction type (QCD, QED, EWK,...), and within the 
-   * constraints specified in the showerVariables object. 
-   * The input collection of particles can be either the full collection of 
-   * showering particles (kept in the main class ShowerHandler,
-   * in the case isDecayCase is false, or simply, in the case isDecayCase 
-   * is true, the decaying particle and its decay products.    
-   * The methods returns true, unless something wrong (inconsistencies,
-   * or undefined values) happens.
+   * The standard Init function used to initialize the interfaces.
+   * Called exactly once for each class by the class description system
+   * before the main function starts or
+   * when this class is dynamically loaded.
    */
-  //@{
-  /**
-   * Set the initial QCD scales
-   * @param particles        The particles to be considered
-   * @param isDecayCase      Whether or not this is a decay
-   */
-  bool setQCDInitialEvolutionScales(const ShowerParticleVector &particles,
-                                    const bool isDecayCase);
+  static void Init();
 
-  /**
-   * Set the initial QED scales
-   * @param particles        The particles to be considered
-   * @param isDecayCase      Whether or not this is a decay
-   */
-  bool setQEDInitialEvolutionScales(const ShowerParticleVector &particles,
-                                    const bool isDecayCase);
-
-  /**
-   * Set the initial electroweak scales
-   * @param particles        The particles to be considered
-   * @param isDecayCase      Whether or not this is a decay
-   */
-  bool setEWKInitialEvolutionScales(const ShowerParticleVector &particles,
-                                    const bool isDecayCase);
-  //@}
+protected:
 
   /**
    * Given a pair of particles, supposedly partners w.r.t. an interaction,
@@ -138,59 +55,22 @@ public:
    */
   //@{
   /**
-   *  General method to calculate the initial evolution scales
-   */
-  pair<Energy,Energy> calculateInitialEvolutionScales(const ShowerPPair &,
-                                           const bool isDecayCase);
-
-  /**
    *  Calculate the initial evolution scales for two final-state particles
    */
-  pair<Energy,Energy> calculateFinalFinalScales(const ShowerPPair &);
+  virtual pair<Energy,Energy> calculateFinalFinalScales(const ShowerPPair &);
 
   /**
    *  Calculate the initial evolution scales for two initial-state particles
    */
-  pair<Energy,Energy> calculateInitialInitialScales(const ShowerPPair &);
+  virtual pair<Energy,Energy> calculateInitialInitialScales(const ShowerPPair &);
 
   /**
    *  Calculate the initial evolution scales for one initial 
    *  and one final-state particles
    */
-  pair<Energy,Energy> calculateInitialFinalScales(const ShowerPPair &,
-                                       const bool isDecayCase);
+  virtual pair<Energy,Energy> calculateInitialFinalScales(const ShowerPPair &,
+							  const bool isDecayCase);
   //@}
-
-  /**
-   *  Set the shower variables, only used by Evolver in doinit
-   */
-  inline void setShowerVariables(ShowerVarsPtr);
-
-public:
-
-  /** @name Functions used by the persistent I/O system. */
-  //@{
-  /**
-   * Function used to write out object persistently.
-   * @param os the persistent output stream written to.
-   */
-  void persistentOutput(PersistentOStream & os) const;
-
-  /**
-   * Function used to read in object persistently.
-   * @param is the persistent input stream read from.
-   * @param version the version number of the object when written.
-   */
-  void persistentInput(PersistentIStream & is, int version);
-  //@}
-
-  /**
-   * The standard Init function used to initialize the interfaces.
-   * Called exactly once for each class by the class description system
-   * before the main function starts or
-   * when this class is dynamically loaded.
-   */
-  static void Init();
 
 protected:
 
@@ -215,25 +95,13 @@ private:
    * The static object used to initialize the description of this class.
    * Indicates that this is a concrete class with persistent data.
    */
-  static ClassDescription<PartnerFinder> initPartnerFinder;
+  static NoPIOClassDescription<FortranFinder> initFortranFinder;
 
   /**
    * The assignment operator is private and must never be called.
    * In fact, it should not even be implemented.
    */
-  PartnerFinder & operator=(const PartnerFinder &);
-
-private:
-
-  /**
-   *  Approach to use for setting the colour partners
-   */
-  int _approach;
-
-  /**
-   *  Pointer to the ShowerVariables object
-   */
-  ShowerVarsPtr _showerVariables;
+  FortranFinder & operator=(const FortranFinder &);
 
 };
 
@@ -246,24 +114,24 @@ namespace ThePEG {
 /** @cond TRAITSPECIALIZATIONS */
 
 /** This template specialization informs ThePEG about the
- *  base classes of PartnerFinder. */
+ *  base classes of FortranFinder. */
 template <>
-struct BaseClassTrait<Herwig::PartnerFinder,1> {
-  /** Typedef of the first base class of PartnerFinder. */
-  typedef Interfaced NthBase;
+struct BaseClassTrait<Herwig::FortranFinder,1> {
+  /** Typedef of the first base class of FortranFinder. */
+  typedef Herwig::PartnerFinder NthBase;
 };
 
 /** This template specialization informs ThePEG about the name of
- *  the PartnerFinder class and the shared object where it is defined. */
+ *  the FortranFinder class and the shared object where it is defined. */
 template <>
-struct ClassTraits<Herwig::PartnerFinder>
-  : public ClassTraitsBase<Herwig::PartnerFinder> {
+struct ClassTraits<Herwig::FortranFinder>
+  : public ClassTraitsBase<Herwig::FortranFinder> {
   /** Return a platform-independent class name */
-  static string className() { return "Herwig++::PartnerFinder"; }
+  static string className() { return "Herwig++::FortranFinder"; }
   /**
    * The name of a file containing the dynamic library where the class
-   * PartnerFinder is implemented. It may also include several, space-separated,
-   * libraries if the class PartnerFinder depends on other classes (base classes
+   * FortranFinder is implemented. It may also include several, space-separated,
+   * libraries if the class FortranFinder depends on other classes (base classes
    * excepted). In this case the listed libraries will be dynamically
    * linked in the order they are specified.
    */
@@ -274,9 +142,9 @@ struct ClassTraits<Herwig::PartnerFinder>
 
 }
 
-#include "PartnerFinder.icc"
+#include "FortranFinder.icc"
 #ifndef ThePEG_TEMPLATES_IN_CC_FILE
-// #include "PartnerFinder.tcc"
+// #include "FortranFinder.tcc"
 #endif
 
-#endif /* HERWIG_PartnerFinder_H */
+#endif /* HERWIG_FortranFinder_H */
