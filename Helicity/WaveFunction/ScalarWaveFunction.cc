3ef2461eda98e26e681085742770f77a48b16dc0
richardn <none@none>
2007-05-25 08:20:01 +0000

    minor clean up

--- Helicity/WaveFunction/ScalarWaveFunction.cc
+++ Helicity/WaveFunction/ScalarWaveFunction.cc
@@ -12,31 +12,30 @@ namespace Herwig {
 namespace Helicity {
 
 // special set-up constructor
-ScalarWaveFunction::ScalarWaveFunction(tPPtr part,Direction dir,bool time,bool vertex)
-{
-  if(vertex)
-    {
-      tScalarSpinPtr inspin;
-      if(part->spinInfo()){inspin=dynamic_ptr_cast<tScalarSpinPtr>(part->spinInfo());}
-      if(dir==outgoing)
-	{if(!inspin){part->spinInfo(new_ptr(ScalarSpinInfo(part->momentum(),time)));}}
-      else
-	{
-	  if(inspin){inspin->decay();}
-	  else
-	    {
-	      if(part->spinInfo())
-		{throw ThePEG::Helicity::HelicityConsistencyError() 
-		    << "Wrong type of SpinInfo for the incoming particle in "
-		    << "ScalarWaveFunction() " << part->PDGName()
-		    << Exception::warning;}
-	      SpinPtr newspin(new_ptr(ScalarSpinInfo(part->momentum(),time)));
-	      inspin= dynamic_ptr_cast<tScalarSpinPtr>(newspin);
-	      if(time) inspin->decayed(true);
-	      part->spinInfo(newspin);
-	    }
-	}
+ScalarWaveFunction::ScalarWaveFunction(tPPtr part,Direction dir,bool time,bool vertex) {
+  if(vertex) {
+    tScalarSpinPtr inspin;
+    if(part->spinInfo()) inspin=dynamic_ptr_cast<tScalarSpinPtr>(part->spinInfo());
+    if(dir==outgoing) {
+      if(!inspin) part->spinInfo(new_ptr(ScalarSpinInfo(part->momentum(),time)));
     }
+    else {
+      if(inspin) {
+	inspin->decay();
+      }
+      else {
+	if(part->spinInfo()) 
+	  throw ThePEG::Helicity::HelicityConsistencyError() 
+	    << "Wrong type of SpinInfo for the incoming particle in "
+	    << "ScalarWaveFunction() " << part->PDGName()
+	    << Exception::warning;
+	SpinPtr newspin(new_ptr(ScalarSpinInfo(part->momentum(),time)));
+	inspin= dynamic_ptr_cast<tScalarSpinPtr>(newspin);
+	if(time) inspin->decayed(true);
+	part->spinInfo(newspin);
+      }
+    }
+  }
   direction(dir);
   setMomentum(part->momentum());
   checkParticle(part->dataPtr());
