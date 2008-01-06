f4220382c5c67a4d5fae9164d65e1901956ac0ce
dph3mag <none@none>
2008-01-06 15:16:58 +0000

    Added the higgs-gluon-gluon effective vertex for the NMSSM

---
+++ Models/Susy/NMSSM/NMSSMGGHVertex.cc
@@ -0,0 +1,157 @@
+// -*- C++ -*-
+//
+// This is the implementation of the non-inlined, non-templated member
+// functions of the NMSSMGGHVertex class.
+//
+
+#include "NMSSMGGHVertex.h"
+#include "ThePEG/Interface/ClassDocumentation.h"
+#include "ThePEG/Persistency/PersistentOStream.h"
+#include "ThePEG/Persistency/PersistentIStream.h"
+#include "Herwig++/Models/Susy/NMSSM/NMSSM.h"
+
+using namespace Herwig;
+
+NMSSMGGHVertex::NMSSMGGHVertex() : 
+  _sw(0.), _cw(0.), _mw(), _mz(), _sb(0.), _cb(0.), 
+  _masslast(make_pair(0.*MeV,0.*MeV)), _q2last(0.*MeV2), _couplast(0.), 
+  _hlast(0), _recalc(true) {
+  //PDG codes for particles at vertices
+  vector<int> gluon(5,21), third(5);
+  third[0] = 25;
+  third[1] = 35;
+  third[2] = 36;
+  third[3] = 45;
+  third[4] = 46;
+  setList(gluon, gluon, third);
+}
+
+void NMSSMGGHVertex::doinit() throw(InitException) {
+  _theSM = dynamic_ptr_cast<tcHwSMPtr>(generator()->standardModel());
+  if( !_theSM ) {
+    throw InitException() << "NMSSMGGHVertex::doinit - The SM pointer is null!"
+			  << Exception::abortnow;
+  }
+  // SM parameters
+  _sw = sqrt(_theSM->sin2ThetaW());
+  _cw = sqrt(1. - _theSM->sin2ThetaW());
+  _mw = getParticleData(24)->mass();
+  _mz = getParticleData(23)->mass();
+  _top = getParticleData(6);
+  _bt = getParticleData(5);
+
+  //NMSSM parameters
+  tcNMSSMPtr nmssm = dynamic_ptr_cast<tcNMSSMPtr>(_theSM);
+  _mixS = nmssm->CPevenHiggsMix();
+  _mixP = nmssm->CPoddHiggsMix();
+  _mixQt = nmssm->stopMix();
+  _mixQb = nmssm->sbottomMix();
+
+  // resize vectors here and use setNParticles method
+  // to the set the actual number in the loop.
+  // Also only the top mass hass to be calculated at runtime
+  masses.resize(5, Energy());
+  masses[1] = getParticleData(1000005)->mass();
+  masses[2] = getParticleData(2000005)->mass();
+  masses[3] = getParticleData(1000006)->mass();
+  masses[4] = getParticleData(2000006)->mass();
+  type.resize(5, PDT::Spin0);
+  type[0] = PDT::Spin1Half;
+  couplings.resize(5);
+
+  orderInGem(1);
+  orderInGs(2);
+
+  VVSLoopVertex::doinit();
+}
+
+void NMSSMGGHVertex::persistentOutput(PersistentOStream & os) const {
+  os << _theSM << _sw << _cw << ounit(_mw, GeV) << ounit(_mz, GeV)
+     << _top << _bt << _mixS << _mixP << _mixQt << _mixQb << _sb 
+     << _cb; 
+}
+
+void NMSSMGGHVertex::persistentInput(PersistentIStream & is, int) {
+  is >> _theSM >> _sw >> _cw >> iunit(_mw, GeV) >> iunit(_mz, GeV)
+     >> _top >> _bt >> _mixS >> _mixP >> _mixQt >> _mixQb >> _sb
+     >> _cb; 
+}
+
+ClassDescription<NMSSMGGHVertex> NMSSMGGHVertex::initNMSSMGGHVertex;
+// Definition of the static class description member.
+
+void NMSSMGGHVertex::Init() {
+
+  static ClassDocumentation<NMSSMGGHVertex> documentation
+    ("The effective coupling of a higgs to a pair of gluons in the "
+     "NMSSM.");
+
+}
+
+void NMSSMGGHVertex::setCoupling(Energy2 q2, tcPDPtr p1, tcPDPtr p2,
+				 tcPDPtr p3) {
+  long hid(p3->id());
+  if( q2 != _q2last ) {
+    _masslast.first = _theSM->mass(q2, _bt);
+    _masslast.second = _theSM->mass(q2, _top);
+    double alpha = _theSM->alphaEM(q2);
+    double alphas = _theSM->alphaS(q2);
+    _couplast = 4.*Constants::pi*alphas*sqrt(Constants::pi*alpha)/_sw;
+    _q2last = q2;
+    _recalc = true;
+  }
+  setNorm(_couplast);
+
+  if( hid != _hlast ) {
+    _hlast = hid;
+    _recalc = true;
+    if( hid % 5 == 0 ) {
+      setNParticles(5);
+      int iloc = (hid - 25)/10;
+      //top quark
+      masses[0] = _masslast.second;
+      Complex cpl = -_masslast.second*(*_mixS)(iloc, 1)/_sb/_mw;
+      couplings[0].first = cpl;
+      couplings[0].second = cpl;
+      //~b_1
+      double f1 = _masslast.first/_mw/_cb;
+      complex<Energy> f2 = 0.5*_mz*(_cb*(*_mixS)(iloc,0) 
+				    - _sb*(*_mixS)(iloc,1))/_cw;
+      Complex mix = (*_mixQb)(0, 0);
+      cpl = ( f2*(1. - 2.*sqr(_sw)/3.) 
+	      - f1*_masslast.first*(*_mixS)(iloc,0) )*mix*mix 
+	* UnitRemoval::InvE;
+      couplings[1].first = cpl; couplings[1].second = cpl; 
+      //~b_2
+      mix = (*_mixQb)(1, 1);
+      cpl = -( f2*2.*sqr(_sw)/3. + f1*_masslast.first*(*_mixS)(iloc,0) )*mix*mix
+	* UnitRemoval::InvE;
+      couplings[2].first = cpl; couplings[2].second = cpl; 
+      //~t_1
+      f1 = _masslast.second/_mw/_sb;
+      mix = (*_mixQt)(0, 0);
+      cpl = -( f2*(1. - 4.*sqr(_sw)/3.) + f1*_masslast.second*(*_mixS)(iloc,1) )
+	*mix*mix*UnitRemoval::InvE;
+      couplings[3].first = cpl; couplings[3].second = cpl; 
+      //~t_2
+      mix = (*_mixQt)(1, 1);
+      cpl = -( f2*4.*sqr(_sw)/3. + f1*_masslast.second*(*_mixS)(iloc,1) )
+	*mix*mix*UnitRemoval::InvE;
+      couplings[4].first = cpl; couplings[4].second = cpl; 
+    }
+    else {
+      setNParticles(1);
+      int iloc = (hid - 36)/10;
+      //top quark
+      masses[0] = _masslast.second;
+      Complex cpl = Complex(0., 1.)*_masslast.second*(*_mixP)(iloc, 1)/_sb/_mw;
+      couplings[0].first = cpl;
+      couplings[0].second = -cpl;
+    }
+  }
+
+  if( _recalc ) {
+    VVSLoopVertex::setCoupling(q2, p1, p2, p3);
+    _recalc = false;
+  } 
+}
---
+++ Models/Susy/NMSSM/NMSSMGGHVertex.h
@@ -0,0 +1,251 @@
+// -*- C++ -*-
+#ifndef HERWIG_NMSSMGGHVertex_H
+#define HERWIG_NMSSMGGHVertex_H
+//
+// This is the declaration of the NMSSMGGHVertex class.
+//
+
+#include "Herwig++/Models/General/VVSLoopVertex.h"
+#include "Herwig++/Models/StandardModel/StandardModel.fh"
+#include "Herwig++/Models/Susy/MixingMatrix.h"
+#include "NMSSMGGHVertex.fh"
+
+namespace Herwig {
+using namespace ThePEG;
+
+/**
+ * This class implements the effective vertex for a higgs coupling
+ * to a pair of gluons in the NMSSM.
+ *
+ * @see \ref NMSSMGGHVertexInterfaces "The interfaces"
+ * defined for NMSSMGGHVertex.
+ */
+class NMSSMGGHVertex: public VVSLoopVertex {
+
+public:
+
+  /** @name Standard constructors and destructors. */
+  //@{
+  /**
+   * The default constructor.
+   */
+  NMSSMGGHVertex();
+  //@}
+
+public:
+
+  /** @name Functions used by the persistent I/O system. */
+  //@{
+  /**
+   * Function used to write out object persistently.
+   * @param os the persistent output stream written to.
+   */
+  void persistentOutput(PersistentOStream & os) const;
+
+  /**
+   * Function used to read in object persistently.
+   * @param is the persistent input stream read from.
+   * @param version the version number of the object when written.
+   */
+  void persistentInput(PersistentIStream & is, int version);
+  //@}
+
+  /**
+   * The standard Init function used to initialize the interfaces.
+   * Called exactly once for each class by the class description system
+   * before the main function starts or
+   * when this class is dynamically loaded.
+   */
+  static void Init();
+
+  /** 
+   * Calculate couplings
+   *@param q2 Scale at which to evaluate coupling
+   *@param p1 ParticleData pointer to first particle
+   *@param p2 ParticleData pointer to second particle
+   *@param p3 ParticleData pointer to third particle
+   */
+  virtual void setCoupling(Energy2 q2, tcPDPtr p1, tcPDPtr p2,
+			   tcPDPtr p3);
+
+protected:
+
+  /** @name Clone Methods. */
+  //@{
+  /**
+   * Make a simple clone of this object.
+   * @return a pointer to the new object.
+   */
+  inline virtual IBPtr clone() const;
+
+  /** Make a clone of this object, possibly modifying the cloned object
+   * to make it sane.
+   * @return a pointer to the new object.
+   */
+  inline virtual IBPtr fullclone() const;
+  //@}
+
+protected:
+
+  /** @name Standard Interfaced functions. */
+  //@{
+  /**
+   * Initialize this object after the setup phase before saving an
+   * EventGenerator to disk.
+   * @throws InitException if object could not be initialized properly.
+   */
+  virtual void doinit() throw(InitException);
+  //@}
+
+private:
+
+  /**
+   * The static object used to initialize the description of this class.
+   * Indicates that this is a concrete class with persistent data.
+   */
+  static ClassDescription<NMSSMGGHVertex> initNMSSMGGHVertex;
+
+  /**
+   * The assignment operator is private and must never be called.
+   * In fact, it should not even be implemented.
+   */
+  NMSSMGGHVertex & operator=(const NMSSMGGHVertex &);
+
+private:
+
+  /** @name Stored parameters. */
+  //@{
+  /**
+   * The SM pointer 
+   */
+  tcHwSMPtr _theSM;
+  
+  /**
+   * \f$ \sin\theta_W\f$ 
+   */
+  double _sw;
+
+  /**
+   * \f$ \cos\theta_W\f$ 
+   */
+  double _cw;
+
+  /**
+   * \f$ M_W\f$
+   */
+  Energy _mw;
+
+  /**
+   * \f$ M_Z \f$
+   */
+  Energy _mz;
+  
+  /**
+   * A pointer to the top quark
+   */
+  tPDPtr _top;
+
+  /**
+   * A pointer to the bottom quark
+   */
+  tPDPtr _bt;
+
+  /**
+   * CP-even Higgs mixing matrix 
+   */
+  MixingMatrixPtr _mixS;
+
+  /**
+   * CP-even Higgs mixing matrix 
+   */
+  MixingMatrixPtr _mixP;
+
+  /**
+   * \f$\tilde{t}\f$ mixing matrix  
+   */
+  MixingMatrixPtr _mixQt;
+
+  /**
+   * \f$\tilde{b}\f$ mixing matrix  
+   */
+  MixingMatrixPtr _mixQb;
+
+  /**
+   * \f$ \sin\beta\f$ 
+   */
+  double _sb;
+
+  /**
+   * \f$ \cos\beta\f$ 
+   */
+  double _cb;
+  
+  /**
+   * The top and bottom quark masses calculated at the last value
+   * of \f$q^2\f$ 
+   */
+  pair<Energy, Energy> _masslast;
+
+  /**
+   * The scale at which the coupling was last evaluated 
+   */
+  Energy2 _q2last;
+
+  /**
+   * The value of the overall normalisation when the coupling was last 
+   * evaluated.
+   */
+  double _couplast;
+  
+  /**
+   * The PDG code of the Higgs particle when the vertex was last evaluated 
+   */
+  long _hlast;
+
+  /**
+   * Whether the tensor coefficient need recalculating or not 
+   */
+  bool _recalc;
+  //@}
+};
+
+}
+
+#include "ThePEG/Utilities/ClassTraits.h"
+
+namespace ThePEG {
+
+/** @cond TRAITSPECIALIZATIONS */
+
+/** This template specialization informs ThePEG about the
+ *  base classes of NMSSMGGHVertex. */
+template <>
+struct BaseClassTrait<Herwig::NMSSMGGHVertex,1> {
+  /** Typedef of the first base class of NMSSMGGHVertex. */
+  typedef Herwig::VVSLoopVertex NthBase;
+};
+
+/** This template specialization informs ThePEG about the name of
+ *  the NMSSMGGHVertex class and the shared object where it is defined. */
+template <>
+struct ClassTraits<Herwig::NMSSMGGHVertex>
+  : public ClassTraitsBase<Herwig::NMSSMGGHVertex> {
+  /** Return a platform-independent class name */
+  static string className() { return "Herwig::NMSSMGGHVertex"; }
+  /**
+   * The name of a file containing the dynamic library where the class
+   * NMSSMGGHVertex is implemented. It may also include several, space-separated,
+   * libraries if the class NMSSMGGHVertex depends on other classes (base classes
+   * excepted). In this case the listed libraries will be dynamically
+   * linked in the order they are specified.
+   */
+  static string library() { return "HwNMSSM.so"; }
+};
+
+/** @endcond */
+
+}
+
+#include "NMSSMGGHVertex.icc"
+
+#endif /* HERWIG_NMSSMGGHVertex_H */
