8bd849ef7ee9c1daf7d1e1d352e0f4c8e29f174a
richardn <none@none>
2008-07-16 16:14:23 +0000

    changes so general hadron-hadron events with extra emission can be handled
    --HG--
    branch : nason

--- Shower/Powheg/HardTree.cc
+++ Shower/Powheg/HardTree.cc
@@ -6,30 +6,32 @@
 
 #include "HardTree.h"
 #include "ThePEG/PDT/EnumParticles.h"
+#include "ThePEG/Repository/CurrentGenerator.h"
 
 using namespace Herwig;
 
 HardTree::HardTree(vector<HardBranchingPtr> branchings,
-		     vector<HardBranchingPtr> spacelike) {
+		   vector<HardBranchingPtr> spacelike) {
   _branchings.insert(branchings.begin(),branchings.end());
   _spacelike .insert(spacelike .begin(),spacelike .end());
 }
 
 void HardBranching::setMomenta(LorentzRotation R,double aparent,
-				Lorentz5Momentum ptparent) {
-  _original=R*_particle->momentum();
+			       Lorentz5Momentum ptparent,
+			       bool setMomentum) {
+  if(setMomentum) _original=R*_particle->momentum();
   // compute the shower variables
-  Energy2 dot=_n*_p;
+  Energy2 dot = _n*_p;
   double alpha = (_original*_n)/dot;
+  _z=alpha/aparent;
   double beta = ((_original*_p)-alpha*sqr(_p.mass()))/dot;
   _qt=_original-alpha*_p-beta*_n-ptparent;
   _pt=sqrt(max(-_qt*_qt,0.*MeV2));
-  _z=alpha/aparent;
   // reconstruct children
   for(unsigned int ix=0;ix<_children.size();++ix) {
     _children[ix]->_p=_p;
     _children[ix]->_n=_n;
-    _children[ix]->setMomenta(R,alpha,_qt);
+    _children[ix]->setMomenta(R,alpha,_qt,setMomentum);
   }
   // calculate the evolution scale and phi
   if(!_children.empty()) {
@@ -43,13 +45,12 @@ void HardBranching::setMomenta(LorentzRotation R,double aparent,
     // get the pt vector
     Lorentz5Momentum vect=_children[0]->_qt;
     Boost beta_bb = -(_p+ _n).boostVector();
-    Lorentz5Momentum p_bb = _p;
+    Lorentz5Momentum p_bb = _p; 
     vect.boost(beta_bb);
     p_bb.boost( beta_bb );
-
     Axis axis(p_bb.vect().unit());
+    LorentzRotation rot;
     if(axis.perp2()>0.) {
-      LorentzRotation rot;
       double sinth(sqrt(1.-sqr(axis.z())));
       rot.setRotate(-acos(axis.z()),
 		    Axis(-axis.y()/sinth,axis.x()/sinth,0.));
@@ -61,8 +62,57 @@ void HardBranching::setMomenta(LorentzRotation R,double aparent,
     _phi= atan2(vect.y(),vect.x());
     if(_phi<0.)                 _phi+=Constants::twopi;
     if(_children[1]->_incoming) _phi+=Constants::pi;
-    
   }
+}
+
+bool HardTree::connect(ShowerTreePtr shower) {
+  _particles.clear();
+  // extract the progenitors from the ShowerTree
+  vector<ShowerProgenitorPtr> progenitors = shower->extractProgenitors();
+  CurrentGenerator::log() << "testing sizes " << branchings().size() << " "
+			  << progenitors.size();
+  // connect the trees up
+  for(set<HardBranchingPtr>::iterator it=branchings().begin();
+      it!=branchings().end();++it) {
+    Energy2 dmin(1e30*GeV2);
+    tShowerParticlePtr partner;  
+    CurrentGenerator::log() << "testing looking for " 
+			    << *(**it).branchingParticle() << "\n"; 
+    for(unsigned int ix=0;ix<progenitors.size();++ix) {
+      CurrentGenerator::log() << "looking at " << *progenitors[ix]->progenitor() << "\n";
+      if((**it).branchingParticle()->id()!=progenitors[ix]->progenitor()->id()) continue;
+      CurrentGenerator::log() << "after A\n";
+      if((**it).branchingParticle()->isFinalState()!=
+	 progenitors[ix]->progenitor()->isFinalState()) continue;
+      CurrentGenerator::log() << "after A\n";
+      Energy2 dtest = 
+	sqr(progenitors[ix]->progenitor()->momentum().x()-(**it).showerMomentum().x())+
+	sqr(progenitors[ix]->progenitor()->momentum().y()-(**it).showerMomentum().y())+
+	sqr(progenitors[ix]->progenitor()->momentum().z()-(**it).showerMomentum().z())+
+	sqr(progenitors[ix]->progenitor()->momentum().t()-(**it).showerMomentum().t());
+      if(dtest<dmin) {
+	partner = progenitors[ix]->progenitor();
+	dmin = dtest;
+      }
+    }
+    if(!partner) return false;
+    connect(partner,*it);
+    if((**it).incoming()) {
+      double z((**it).z());
+      tHardBranchingPtr parent=(**it).parent();
+      while (parent) {
+	z *= parent->z();
+	parent=parent->parent();
+      }
+      partner->x(z);
+    }
+  }
+  // return false if not matched
+  CurrentGenerator::log() << "testing got to end " << particles().size() << " "
+		     << progenitors.size() << "\n";
+  bool output = particles().size()==progenitors.size();
+  CurrentGenerator::log() << "testing answer " << output << "\n";
+  return output;
 }
 
 HardBranching::HardBranching(ShowerParticlePtr particle, SudakovPtr sudakov,
@@ -71,3 +121,16 @@ HardBranching::HardBranching(ShowerParticlePtr particle, SudakovPtr sudakov,
     _sudakov(sudakov)
 {}
 
+void HardBranching::fixColours() {
+  if(!_sudakov) return;
+  if(!_incoming&&_children.empty()) return;
+  if(_incoming && !_parent) return;
+  if(_incoming)
+    _sudakov->splittingFn()->
+      colourConnection(_parent->_particle,_particle,
+		       _parent->children()[1]->_particle,true);
+  else
+    _sudakov->splittingFn()->
+      colourConnection(_particle,_children[0]->_particle,
+		       _children[1]->_particle,false);
+}
