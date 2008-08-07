2966fb44c0cc142f05617ff12851f4c34c8b4e4c
dph3mag <none@none>
2008-08-07 08:59:33 +0000

    A few fixes to UED Higgs stuff

--- Models/UED/UEDF1F0H1Vertex.cc
+++ Models/UED/UEDF1F0H1Vertex.cc
@@ -22,10 +22,10 @@ using namespace Herwig;
 UEDF1F0H1Vertex::UEDF1F0H1Vertex() : theRadius(), theMw(), 
 				     theSinThetaW(0.), theq2Last(-1.*GeV2),
 				     theCoupLast(0.), theLeftLast(0.),
-				     theRightLast(0.), theKKLast(0),
-				     theSMLast(0) {
+				     theRightLast(0.), theAntiLast(0),
+				     theFermLast(0), theHLast(0) {
   vector<long> anti, ferm, kkhiggs;
-  for(long i = 1; i < 6; i += 2) {
+  for(long i = 1; i < 7; i += 2) {
     //outgoing H+
     anti.push_back(-(5100001 + i));
     ferm.push_back(i);
@@ -47,7 +47,7 @@ UEDF1F0H1Vertex::UEDF1F0H1Vertex() : theRadius(), theMw(),
     anti.push_back(-5100000 - i);
     ferm.push_back(i + 1);
     kkhiggs.push_back(-5100037);
-    
+
     anti.push_back(-6100000 - i);
     ferm.push_back(i + 1);
     kkhiggs.push_back(-5100037);    
@@ -55,31 +55,131 @@ UEDF1F0H1Vertex::UEDF1F0H1Vertex() : theRadius(), theMw(),
     anti.push_back(-i);
     ferm.push_back(5100001 + i);
     kkhiggs.push_back(-5100037);
+
+    anti.push_back(-i);
+    ferm.push_back(6100001 + i);
+    kkhiggs.push_back(-5100037);
+  }
+  for(long i = 11; i < 17; i += 2) {
+    //outgoing H+
+    anti.push_back(-5100001 - i);
+    ferm.push_back(i);
+    kkhiggs.push_back(5100037);
+
+    anti.push_back(-i - 1);
+    ferm.push_back(5100000 + i);
+    kkhiggs.push_back(5100037);
+
+    anti.push_back(-i - 1);
+    ferm.push_back(6100000 + i);
+    kkhiggs.push_back(5100037);
+    
+    //outgoing H-
+    anti.push_back(-i);
+    ferm.push_back(5100001 + i);
+    kkhiggs.push_back(-5100037);
+    
+    anti.push_back(-i - 1);
+    ferm.push_back(5100000 + i);
+    kkhiggs.push_back(-5100037);    
+    
+    anti.push_back(-i - 1);
+    ferm.push_back(5100000 + i);
+    kkhiggs.push_back(-5100037);
+  }
+  // Neutral Higgs
+  anti.push_back(-5100006);
+  ferm.push_back(5100006);
+  kkhiggs.push_back(25);
+  anti.push_back(-6100006);
+  ferm.push_back(6100006);
+  kkhiggs.push_back(25);
+  anti.push_back(-5100006);
+  ferm.push_back(6100006);
+  kkhiggs.push_back(25);
+  anti.push_back(-6100006);
+  ferm.push_back(5100006);
+  kkhiggs.push_back(25);
+
+  anti.push_back(-6);
+  ferm.push_back(5100006);
+  kkhiggs.push_back(5100025);
+  anti.push_back(-5100006);
+  ferm.push_back(6);
+  kkhiggs.push_back(5100025);
+  anti.push_back(-6);
+  ferm.push_back(6100006);
+  kkhiggs.push_back(5100025);
+  anti.push_back(-6100006);
+  ferm.push_back(6);
+  kkhiggs.push_back(5100025);
+
+  for(long i = 1; i < 7; ++i) {
+    anti.push_back(-i);
+    ferm.push_back(i + 5100000);
+    kkhiggs.push_back(5100036);
+    
+    anti.push_back(-i - 5100000);
+    ferm.push_back(i);
+    kkhiggs.push_back(5100036);
+    
+    anti.push_back(-i);
+    ferm.push_back(i + 6100000);
+    kkhiggs.push_back(5100036);
+    
+    anti.push_back(-i - 6100000);
+    ferm.push_back(i);
+      kkhiggs.push_back(5100036);
+  }
+  for(long i = 11; i < 17; ++i) {
+    anti.push_back(-i);
+    ferm.push_back(i + 5100000);
+    kkhiggs.push_back(5100036);
+    
+    anti.push_back(-i - 5100000);
+    ferm.push_back(i);
+    kkhiggs.push_back(5100036);
+
+    if( i % 2 != 0 ) {
+      anti.push_back(-i);
+      ferm.push_back(i + 6100000);
+      kkhiggs.push_back(5100036);
+      
+      anti.push_back(-i - 6100000);
+      ferm.push_back(i);
+      kkhiggs.push_back(5100036);
+    }
   }
   setList(anti, ferm, kkhiggs);
 }
 
 void UEDF1F0H1Vertex::doinit() throw(InitException) {
   FFSVertex::doinit();
-  tUEDBasePtr UEDBase = dynamic_ptr_cast<tUEDBasePtr>(generator()->standardModel());
+  tUEDBasePtr UEDBase = 
+    dynamic_ptr_cast<tUEDBasePtr>(generator()->standardModel());
   if(!UEDBase)
     throw InitException() << "UEDF1F0H1Vertex::doinit() - The pointer to "
 			  << "the UEDBase object is null!"
 			  << Exception::runerror;
   theRadius = UEDBase->compactRadius();
   theSinThetaW = sqrt(UEDBase->sin2ThetaW());
+  theCosThetaW = sqrt(1. - UEDBase->sin2ThetaW());
   theMw = getParticleData(24)->mass();
+  theMz = getParticleData(23)->mass();
+
   orderInGs(0);
   orderInGem(1);
 }
 
 
 void UEDF1F0H1Vertex::persistentOutput(PersistentOStream & os) const {
-  os << ounit(theRadius,1/GeV) << ounit(theMw,GeV) << theSinThetaW ;
+  os << ounit(theRadius,1/GeV) << ounit(theMw,GeV) << theSinThetaW 
+     << ounit(theMz, GeV) << theCosThetaW;
 }
 
 void UEDF1F0H1Vertex::persistentInput(PersistentIStream & is, int) {
-  is >> iunit(theRadius,1/GeV) >> iunit(theMw,GeV) >> theSinThetaW;
+  is >> iunit(theRadius,1/GeV) >> iunit(theMw,GeV) >> theSinThetaW
+     >> iunit(theMz, GeV) >> theCosThetaW;
 }
 
 ClassDescription<UEDF1F0H1Vertex> UEDF1F0H1Vertex::initUEDF1F0H1Vertex;
@@ -88,100 +188,98 @@ ClassDescription<UEDF1F0H1Vertex> UEDF1F0H1Vertex::initUEDF1F0H1Vertex;
 void UEDF1F0H1Vertex::Init() {
 
   static ClassDocumentation<UEDF1F0H1Vertex> documentation
-    ("The coupling of a KK-1 fermion to an SM fermion and a charged higgs.");
+    ("The coupling involving a KK-Higgs and a pair of fermions.");
 
 }
 
 void UEDF1F0H1Vertex::setCoupling(Energy2 q2, tcPDPtr part1, tcPDPtr part2,
 				  tcPDPtr part3, int) {
-  long kkferm(0), smferm(0), kkhiggs(0);
-  if(abs(part1->id()) == 5100037) {
-    kkhiggs = part1->id();
-    if(abs(part2->id()) > 5100000) {
-      kkferm = abs(part2->id());
-      smferm = part3->id();
-    }
-    else {
-      kkferm = abs(part3->id());
-      smferm = part2->id();
-    }
-  }
-  else if(abs(part2->id()) == 5100037) {
-    kkhiggs = part2->id();
-    if(abs(part1->id()) > 5100000) {
-      kkferm = abs(part1->id());
-      smferm = part3->id();
-    }
-    else {
-      kkferm = abs(part3->id());
-      smferm = part1->id();
-    }
-  }
-  else if(abs(part3->id()) == 5100037) {
-    kkhiggs = part3->id();
-    if(abs(part1->id()) > 5100000) {
-      kkferm = abs(part1->id());
-      smferm = part2->id();
-    }
-    else {
-      kkferm = abs(part2->id());
-      smferm = part1->id();
-    }
-  }
-  else {
-    throw HelicityLogicalError() << "UEDF1F0H1Vertex::setCoupling - There is "
-				 << "no KK1 charged Higgs in this vertex."
-				 << Exception::warning;
-    return;
-  }
-  bool cc;
-  if(kkhiggs > 0)
-    cc = (smferm > 0) ? false : true;
-  else
-    cc = (smferm > 0) ? true : false;
-  smferm = abs(smferm);
-  if( (smferm >= 1 && smferm <= 6) ||
-      (smferm >= 11 && smferm <= 16) ) {
-    if(q2 != theq2Last) {
-      theq2Last = q2;
-      theCoupLast = sqrt(0.5)*weakCoupling(q2);
-      theCoupLast *= theRadius/sqrt(1. + sqr(theMw*theRadius)) * UnitRemoval::E;
-    }
-    setNorm(theCoupLast);
-    if(kkferm != theKKLast || abs(smferm) != theSMLast) {
-      long smID = (kkferm > 6100000) ? kkferm - 6100000 : kkferm - 5100000;
-      Energy smMass = getParticleData(smID)->mass();
-      double beta = smMass*theRadius;
-      double gamma = beta*beta/(1. + beta*beta);
-      double sinAl = sqrt(0.5 - 0.5*sqrt(1. - gamma));
-      double cosAl = sqrt(1. - sinAl*sinAl);
-      unsigned int kkstate = kkferm/1000000;
+  long anti(abs(part1->id())), ferm(abs(part2->id())), higgs(part3->id());
+  if( ferm > 17 ) swap( ferm, anti);
 
-      theRightLast = smMass/theRadius/theMw * UnitRemoval::InvE;
-      if(kkstate == 5) {
-	theLeftLast = (theMw*cosAl - (smMass*sinAl/theRadius/theMw)) 
+  if( anti != theAntiLast || ferm != theFermLast || higgs != theHLast ) { 
+    theAntiLast = anti;
+    theFermLast = ferm;
+    theHLast = higgs;
+
+    tcPDPtr pd;
+    if( higgs != 25 ) {
+      pd = getParticleData(ferm);
+    }
+    else {
+      long smid = ( ferm/1000000 == 5 ) ? ferm - 5100000 : ferm - 6100000;
+      pd = getParticleData(smid);
+    }
+    Energy mf = pd->mass();
+    double alpha = mf*theRadius/2.;
+    double salpha = sin(alpha);
+    double calpha = cos(alpha);
+    double fact(0.);
+    if( abs(higgs) == 5100037 ) {
+      fact = theRadius/2./sqrt(1. + sqr(theMw*theRadius)) * UnitRemoval::E;
+      theRightLast = theRadius*theMw;
+      if(anti/1000000 == 5) {
+	Energy mfk  = getParticleData(anti - 5100000)->mass();
+	theLeftLast = (theMw*calpha - (mfk*salpha/theRadius/theMw)) 
 	  * UnitRemoval::InvE;
-	theRightLast *= cosAl;
+      
+	theRightLast *= calpha*mfk* UnitRemoval::InvE;
       }
       else {
-	theLeftLast = (theMw*sinAl + (smMass*cosAl/theRadius/theMw))
+	Energy mfk = getParticleData(anti - 6100000)->mass();
+	theLeftLast = (theMw*salpha +(mfk*calpha/theRadius/theMw))
 	  * UnitRemoval::InvE;
-	theRightLast *= -sinAl;
+	theRightLast *= -salpha*mfk*UnitRemoval::InvE;
       }
+      theLeftLast *= fact;
+      theRightLast *= fact;
+      if( higgs < 0 ) swap( theLeftLast, theRightLast );
     }
-    if(kkhiggs < 0) {
-      setLeft(theRightLast);
-      setRight(theLeftLast);
+    else if( higgs == 5100025 ) {
+      fact = mf/theMw/2.;
+      if( anti/1000000 == 5 )
+	theLeftLast = salpha + calpha;
+      else 
+	theLeftLast = salpha - calpha;
+      theRightLast = theLeftLast;
+
+      theLeftLast *= fact;
+      theRightLast *= fact;
+    }
+    else if( higgs == 5100036 ) {
+      fact = theRadius/theCosThetaW/sqrt(1.+sqr(theMz*theRadius))*UnitRemoval::E;
+    
+      double i3f = ( ferm % 2 == 0 ) ? 0.5 : -0.5;
+      double qf = pd->charge()/eplus;
+      if( anti/1000000 == 5 ) {
+	theLeftLast = (theMz*calpha*(i3f - qf*sqr(theSinThetaW))
+		       - mf*salpha/theRadius/theMw) * UnitRemoval::InvE;
+	theRightLast = (-theMz*salpha*qf*sqr(theSinThetaW) 
+			+ mf*calpha/theRadius/theMw) * UnitRemoval::InvE; 
+      }
+      else {
+	theLeftLast = (theMz*salpha*(i3f - qf*sqr(theSinThetaW))
+		       - mf*calpha/theRadius/theMw) * UnitRemoval::InvE;
+	theRightLast = (-theMz*calpha*qf*sqr(theSinThetaW) 
+			+ mf*salpha/theRadius/theMw)*UnitRemoval::InvE; 
+      }
+      theLeftLast *= fact;
+      theRightLast *= fact;
     }
     else {
-      setLeft(theLeftLast);
-      setRight(theRightLast);
+      theLeftLast = mf*calpha*salpha/2./theMw;
+      if( ferm/1000000 == 5 ) theLeftLast *= -1.;  
+      theRightLast = theLeftLast;
     }
   }
-  else 
-    throw HelicityLogicalError() << "UEDF1F0H1Vertex::setCoupling - There is an "
-				 << "unknown particle in this vertex " 
-				 << smferm << " " << kkferm
-				 << Exception::warning;
-  
+
+
+  if(q2 != theq2Last) {
+    theq2Last = q2;
+    theCoupLast = weakCoupling(q2);
+  }
+
+  setNorm(theCoupLast);
+  setLeft(theLeftLast);
+  setRight(theRightLast);
 }
