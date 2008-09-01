8db142993f041034a105c35c2c6ed1ce1ba41616
manuel <none@none>
2008-09-01 17:46:54 +0000

    add debug method-fix bug that was found with it

--- UnderlyingEvent/MPIHandler.cc
+++ UnderlyingEvent/MPIHandler.cc
@@ -212,9 +212,30 @@ void MPIHandler::initialize() {
   }
 
   Probs(UEXSecs);
+  //MultDistribution("probs.test");
   UEXSecs.clear();
 }
 
+void MPIHandler::MultDistribution(string filename) const {
+  ofstream file;
+  double p(0.0), pold(0.0);
+  file.open(filename.c_str());
+  //theMultiplicities  
+  Selector<MPair>::const_iterator it = theMultiplicities.begin();
+
+  while(it != theMultiplicities.end()){
+    p = it->first;
+    file << it->second.first << " " << it->second.second
+	 << " " << p-pold << endl;
+    it++;
+    pold = p;
+  }
+
+  file << "sum of all probabilities: " << theMultiplicities.sum() 
+       << endl;
+
+  file.close();
+}
 
 void MPIHandler::statistics(string os) const {
   ofstream file;
@@ -319,6 +340,10 @@ void MPIHandler::Probs(XSVector UEXSecs) {
       }
       iS = 0;
       do{//loop over soft ints
+	if( ( Algorithm() == -1 && iS==0 && iH==0 ) ){
+	  iS++;
+	  continue;
+	}
 
 	Eikonalization integrand(this, iH*100+iS, *it, softXSec_, softMu2_);
       
