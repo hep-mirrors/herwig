86c13e5945ab1c50c75c59e5106ca78c0ba818c3
richardn <none@none>
2009-04-08 13:40:42 +0000

    split two classes into separate files
    --HG--
    branch : nason-QED

--- Shower/Base/HardTree.cc
+++ Shower/Base/HardTree.cc
@@ -63,79 +63,6 @@ bool HardTree::fillNodes( HardBranchingPtr branch, HardBranchingPtr parentBranch
   return true;
 }
 
-void HardBranching::setMomenta(LorentzRotation R,double aparent,
-			       Lorentz5Momentum ptparent,
-			       bool setMomentum) {
-  if(setMomentum) _original=R*_particle->momentum();
-  // compute the shower variables
-  Energy2 dot = _n*_p;
-  if(dot==ZERO) return;
-  double alpha = (_original*_n)/dot;
-  _z=alpha/aparent;
-  double beta = ((_original*_p)-alpha*sqr(_p.mass()))/dot;
-  _qt = _original - alpha*_p - beta*_n - _z*ptparent;
-  _pt=sqrt(max(-_qt*_qt,ZERO));
-  // reconstruct children
-  for(unsigned int ix=0;ix<_children.size();++ix) {
-    _children[ix]->_p=_p;
-    _children[ix]->_n=_n;
-    _children[ix]->setMomenta( R, alpha, _qt + _z*ptparent, setMomentum);
-  }
-  // calculate the evolution scale and phi
-  if(!_children.empty()) {
-    double z = _children[0]->_z;
-    Energy pt = _children[0]->_pt;
-    IdList ids(3);
-    ids[0]=_particle->id();
-    ids[1]=_children[0]->_particle->id();
-    ids[2]=_children[1]->_particle->id();
-    _scale=_sudakov->calculateScale(z,pt,ids,_status);
-    // get the pt vector
-    Lorentz5Momentum vect=_children[0]->_qt;
-    if(_status!=Decay) {
-      Boost beta_bb = -(_p+ _n).boostVector();
-      Lorentz5Momentum p_bb = _p;
-      vect.boost(beta_bb);
-      p_bb.boost( beta_bb );
-      Axis axis(p_bb.vect().unit());
-      LorentzRotation rot;
-      if(axis.perp2()>0.) {
-	double sinth(sqrt(1.-sqr(axis.z())));
-	rot.setRotate(-acos(axis.z()),
-		      Axis(-axis.y()/sinth,axis.x()/sinth,0.));
-	vect.transform(rot);
-      }
-      else if(axis.z()<0.) {
-	vect.setZ(vect.z());
-      }
-      _phi= atan2(vect.y(),vect.x());
-      if(_phi<0.)                         _phi+=Constants::twopi;
-      if(_children[1]->_status==Incoming) _phi+=Constants::pi;
-    }
-    else {
-      const Boost beta_bb = -pVector().boostVector();
-      Lorentz5Momentum p_bb = pVector();
-      Lorentz5Momentum n_bb = nVector(); 
-      p_bb.boost( beta_bb );
-      n_bb.boost( beta_bb );
-      vect.boost( beta_bb);
-      Axis axis(n_bb.vect().unit());
-      LorentzRotation rot;
-      if(axis.perp2()>0.) {
-	double sinth(sqrt(1.-sqr(axis.z())));
-	rot.setRotate(-acos(axis.z()),
-		      Axis(-axis.y()/sinth,axis.x()/sinth,0.));
-	vect.transform(rot);
-      }
-      else if(axis.z()<0.) {
-	vect.setZ(vect.z());
-      }
-      _phi= atan2(vect.y(),vect.x());
-      if(_phi<0) _phi+=Constants::twopi;
-    }
-  }
-}
-
 bool HardTree::connect(ShowerTreePtr shower) {
   _particles.clear();
   // extract the progenitors from the ShowerTree
@@ -175,27 +102,6 @@ bool HardTree::connect(ShowerTreePtr shower) {
   }
   // return false if not matched
   return particles().size()==progenitors.size();
-}
-
-HardBranching::HardBranching(ShowerParticlePtr particle, SudakovPtr sudakov,
-			     tHardBranchingPtr parent,Status status) 
-  : _particle(particle), _p(particle->momentum()),
-    _shower(particle->momentum()),
-    _status(status), _parent(parent), _sudakov(sudakov)
-{}
-
-void HardBranching::fixColours() {
-  if(!_sudakov) return;
-  if(_status==Outgoing && _children.empty()) return;
-  if(_status==Incoming && !_parent) return;
-  if(_status==Incoming)
-    _sudakov->splittingFn()->
-      colourConnection(_parent->_particle,_particle,
-		       _parent->children()[1]->_particle,true);
-  else
-    _sudakov->splittingFn()->
-      colourConnection(_particle,_children[0]->_particle,
-		       _children[1]->_particle,false);
 }
 
 ostream & Herwig::operator<<(ostream & os, const HardTree & x) {
