05989746daceacbb05ce90d8789dd6cc7f03b2f9
andreasp <none@none>
2010-04-29 11:21:41 +0000

    Rearrangement of left- and right- couplings, correction for S1/2 minus
    --HG--
    branch : BSM

--- Models/Leptoquarks/LeptoquarkModelSLQFFVertex.cc
+++ Models/Leptoquarks/LeptoquarkModelSLQFFVertex.cc
@@ -57,6 +57,9 @@ LeptoquarkModelSLQFFVertex::LeptoquarkModelSLQFFVertex()  {
   
   addToList(-15,5,-9941551);
   addToList(-16,6,-9941551);
+  addToList(15,-5,9941551);
+  addToList(16,-6,9941551);
+
 
 
   //S1/2 tilde doublet
@@ -116,81 +119,82 @@ void LeptoquarkModelSLQFFVertex::setCoupling(Energy2,tcPDPtr aa ,tcPDPtr bb, tcP
   
   //set the couplings to left and right 
   //S0
-  if( fabs(isc) == 9911561 || fabs(isc) == 9911561 || fabs(isc) == 9911561 ) {
+  if( fabs(isc) == 9911561 || fabs(ism) == 9911561 || fabs(ichg) == 9911561 ) {
     _cL = _cL0; _cR = _cR0;
   }
   //~S0
-  if( fabs(isc) == 9911551 || fabs(isc) == 9911551 || fabs(isc) == 9911551 ) {
+  if( fabs(isc) == 9911551 || fabs(ism) == 9911551 || fabs(ichg) == 9911551 ) {
     _cL = 0; _cR = _cR0t;
   }
   
   //S1 triplet
   //Q = + 4/3
-  if( fabs(isc) == 9921551 || fabs(isc) == 9921551 || fabs(isc) == 9921551 ) {
+  if( fabs(isc) == 9921551 || fabs(ism) == 9921551 || fabs(ichg) == 9921551 ) {
     _cL = - sqrt(2.)* _cL1; _cR = 0;
   }
   //Q = + 1/3
-  if( fabs(isc) == 9921561 || fabs(isc) == 9921561 || fabs(isc) == 9921561 ) {
+  if( fabs(isc) == 9921561 || fabs(ism) == 9921561 || fabs(ichg) == 9921561 ) {
     _cL = - _cL1; _cR = 0;
   }
   //Q = - 2/3
-  if( fabs(isc) == 9921661 || fabs(isc) == 9921661 || fabs(isc) == 9921661 ) {
+  if( fabs(isc) == 9921661 || fabs(ism) == 9921661 || fabs(ichg) == 9921661 ) {
     _cL = sqrt(2.) * _cL1; _cR = 0;
   }
   
   //S1/2 doublet
 
   //Q = + 5/3 
-  if( fabs(isc) == 9941561 || fabs(isc) == 9941561 || fabs(isc) == 9941561 ) {
-    _cL = _cL12; _cR = _cR12;
+  if( fabs(isc) == 9941561 || fabs(ism) == 9941561 || fabs(ichg) == 9941561 ) {
+    _cL = _cR12; _cR = _cL12;
   }
   
   
   //Q = + 2/3 
-  if( fabs(isc) == 9941551 || fabs(isc) == 9941551 || fabs(isc) == 9941551 ) {
-    _cL = _cL12; _cR = -1. * _cR12;
+  if( fabs(isc) == 9941551 || fabs(ism) == 9941551 || fabs(ichg) == 9941551 ) {
+    if(isc == 5 || ism == 5 || ichg == 5) { 
+      _cL = - _cR12; _cR = 0.;
+    }
+    if(isc == 6 || ism == 6 || ichg == 6) { 
+      _cL = 0.; _cR = _cL12;
+    }
   }
 
   //S1/2 tilde doublet
 
   //Q = + 2/3 
-  if( fabs(isc) == 9951551 || fabs(isc) == 9951551 || fabs(isc) == 9951551 ) {
-    _cL = _cL12t; _cR = 0.;
+  if( fabs(isc) == 9951551 || fabs(ism) == 9951551 || fabs(ichg) == 9951551 ) {
+    _cL = 0.; _cR = _cL12t;
   }
   
   
   //Q = - 1/3 
-  if( fabs(isc) == 9951651 || fabs(isc) == 9951651 || fabs(isc) == 9951651 ) {
-    _cL = _cL12t; _cR = 0.;
+  if( fabs(isc) == 9951651 || fabs(ism) == 9951651 || fabs(ichg) == 9951651 ) {
+    _cL = 0; _cR = _cL12t;
   }
   
-  
-  //loop over generations (currently only third)
-  for(int nl = 0; nl < 3; nl++) {
-      
-    //no right-handed neutrino (or left-handed anti-neutrino)
-    //neutrino
-    if( isc == (12+2*nl) || ism == (12+2*nl) || ichg == (12+2*nl) ) { 
-      if( fabs(isc) == 9911561 || fabs(isc) == 9911561 || fabs(isc) == 9911561 ) { _cL *= -1; }
-      left(_cL); right(0.0);
+  left(_cL); right(_cR);
+	 
+  if( fabs(isc) != 9951551 && fabs(ism) != 9951551 && fabs(ichg) != 9951551 ) {
+    //loop over generations (currently only third)
+    for(int nl = 0; nl < 3; nl++) {
+      //no right-handed neutrino (or left-handed anti-neutrino)
+      //neutrino
+      if( isc == (12+2*nl) || ism == (12+2*nl) || ichg == (12+2*nl) ) { 
+	left(_cL); right(0.0);
+      }
+      //anti-neutrino      
+      if( isc == -(12+2*nl) || ism == -(12+2*nl) || ichg == -(12+2*nl) ) { 
+	left(0.0); right(_cL);
+      }
     }
-    
-    //anti-neutrino
-    if( isc == -(12+2*nl) || ism == -(12+2*nl) || ichg == -(12+2*nl) ) { 
-      if( fabs(isc) == 9911561 || fabs(isc) == 9911561 || fabs(isc) == 9911561 ) { _cL *= -1; }
-       left(0.0); right(_cL);
-    }
-    //swap left-right couplings if necessary
-    if( isc == (11+2*nl) || ism == (11+2*nl) || ichg == (11+2*nl) ) {   
-      left(_cL);
-      right(_cR);
-    }
-    if( isc == -(11+2*nl) || ism == -(11+2*nl) || ichg == -(11+2*nl) ) {   
-      left(_cR);
-      right(_cL);
-    }
-    
   }
+  //swap couplings
+  if( fabs(isc) > 9900000 || fabs(ism) > 9900000 || fabs(ichg) > 9900000 ) {
+    if( isc < 0 || ism < 0 || ichg < 0 ) {
+      left(_cR); right(_cL);
+    }
+  }
+  
   
   norm(_CFF);
 }
