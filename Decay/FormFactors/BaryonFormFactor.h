fafd49ee07550639adbdb5ab8059fc9c9b562270
richardn <none@none>
2010-08-10 14:29:59 +0000

    minor documentation fix

--- Decay/FormFactors/BaryonFormFactor.h
+++ Decay/FormFactors/BaryonFormFactor.h
@@ -92,7 +92,9 @@ class BaryonFormFactor: public Interfaced {
 
 public:
 
-  /// Default constructor
+  /**
+   * Default constructor
+   */
   BaryonFormFactor() : _numbermodes() {}
 
   /** @name Functions used by the persistent I/O system. */
