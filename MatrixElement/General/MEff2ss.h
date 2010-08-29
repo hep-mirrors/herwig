757ee55a4a4421f4dcf9bb22be0b2462d32d9259
richardn <none@none>
2010-08-29 17:29:12 +0000

    added missing s-channel scalar diagram
    --HG--
    branch : BSM

--- MatrixElement/General/MEff2ss.h
+++ MatrixElement/General/MEff2ss.h
@@ -16,11 +16,9 @@
 #include "ThePEG/Helicity/Vertex/AbstractFFSVertex.h"
 #include "ThePEG/Helicity/Vertex/AbstractFFVVertex.h"
 #include "ThePEG/Helicity/Vertex/AbstractVSSVertex.h"
+#include "ThePEG/Helicity/Vertex/AbstractSSSVertex.h"
 #include "ThePEG/Helicity/Vertex/AbstractFFTVertex.h"
 #include "ThePEG/Helicity/Vertex/AbstractSSTVertex.h"
-#include "ThePEG/Helicity/WaveFunction/SpinorWaveFunction.h"
-#include "ThePEG/Helicity/WaveFunction/SpinorBarWaveFunction.h"
-#include "ThePEG/Helicity/WaveFunction/ScalarWaveFunction.h"
 #include "Herwig++/MatrixElement/ProductionMatrixElement.h"
 
 namespace Herwig {
@@ -184,6 +182,12 @@ private:
    * fermion
    */
   vector<pair<AbstractFFSVertexPtr, AbstractFFSVertexPtr> > fermion_;
+
+  /**
+   * Storage for dynamically cast vertices for a diagram with intermediate
+   * vector
+   */
+  vector<pair<AbstractFFSVertexPtr, AbstractSSSVertexPtr> > scalar_;
 
   /**
    * Storage for dynamically cast vertices for a diagram with intermediate
