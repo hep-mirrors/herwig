2c433deaac63d5362411ddbfb3d86b1d62d64e09
richardn <none@none>
2012-06-29 10:20:48 +0100

    added template for powheg in top decay

---
+++ Decay/Perturbative/SMTopPOWHEGDecayer.cc
@@ -0,0 +1,58 @@
+// -*- C++ -*-
+//
+// This is the implementation of the non-inlined, non-templated member
+// functions of the SMTopPOWHEGDecayer class.
+//
+
+#include "SMTopPOWHEGDecayer.h"
+#include "ThePEG/Interface/ClassDocumentation.h"
+#include "ThePEG/EventRecord/Particle.h"
+#include "ThePEG/Repository/UseRandom.h"
+#include "ThePEG/Repository/EventGenerator.h"
+#include "ThePEG/Utilities/DescribeClass.h"
+
+
+#include "ThePEG/Persistency/PersistentOStream.h"
+#include "ThePEG/Persistency/PersistentIStream.h"
+
+using namespace Herwig;
+
+SMTopPOWHEGDecayer::SMTopPOWHEGDecayer() {}
+
+IBPtr SMTopPOWHEGDecayer::clone() const {
+  return new_ptr(*this);
+}
+
+IBPtr SMTopPOWHEGDecayer::fullclone() const {
+  return new_ptr(*this);
+}
+
+
+// If needed, insert default implementations of virtual function defined
+// in the InterfacedBase class here (using ThePEG-interfaced-impl in Emacs).
+
+
+void SMTopPOWHEGDecayer::persistentOutput(PersistentOStream & os) const {
+  // *** ATTENTION *** os << ; // Add all member variable which should be written persistently here.
+}
+
+void SMTopPOWHEGDecayer::persistentInput(PersistentIStream & is, int) {
+  // *** ATTENTION *** is >> ; // Add all member variable which should be read persistently here.
+}
+
+
+// *** Attention *** The following static variable is needed for the type
+// description system in ThePEG. Please check that the template arguments
+// are correct (the class and its base class), and that the constructor
+// arguments are correct (the class name and the name of the dynamically
+// loadable library where the class implementation can be found).
+DescribeClass<SMTopPOWHEGDecayer,SMTopDecayer>
+  describeHerwigSMTopPOWHEGDecayer("Herwig::SMTopPOWHEGDecayer", "HwPertrubativeDecay.so");
+
+void SMTopPOWHEGDecayer::Init() {
+
+  static ClassDocumentation<SMTopPOWHEGDecayer> documentation
+    ("There is no documentation for the SMTopPOWHEGDecayer class");
+
+}
+
---
+++ Decay/Perturbative/SMTopPOWHEGDecayer.h
@@ -0,0 +1,82 @@
+// -*- C++ -*-
+#ifndef Herwig_SMTopPOWHEGDecayer_H
+#define Herwig_SMTopPOWHEGDecayer_H
+//
+// This is the declaration of the SMTopPOWHEGDecayer class.
+//
+
+#include "SMTopDecayer.h"
+
+namespace Herwig {
+
+using namespace ThePEG;
+
+/**
+ * Here is the documentation of the SMTopPOWHEGDecayer class.
+ *
+ * @see \ref SMTopPOWHEGDecayerInterfaces "The interfaces"
+ * defined for SMTopPOWHEGDecayer.
+ */
+class SMTopPOWHEGDecayer: public SMTopDecayer {
+
+public:
+
+  /**
+   * The default constructor.
+   */
+  SMTopPOWHEGDecayer();
+
+  /** @name Functions used by the persistent I/O system. */
+  //@{
+  /**
+   * Function used to write out object persistently.
+   * @param os the persistent output stream written to.
+   */
+  void persistentOutput(PersistentOStream & os) const;
+
+  /**
+   * Function used to read in object persistently.
+   * @param is the persistent input stream read from.
+   * @param version the version number of the object when written.
+   */
+  void persistentInput(PersistentIStream & is, int version);
+  //@}
+
+  /**
+   * The standard Init function used to initialize the interfaces.
+   * Called exactly once for each class by the class description system
+   * before the main function starts or
+   * when this class is dynamically loaded.
+   */
+  static void Init();
+
+protected:
+
+  /** @name Clone Methods. */
+  //@{
+  /**
+   * Make a simple clone of this object.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr clone() const;
+
+  /** Make a clone of this object, possibly modifying the cloned object
+   * to make it sane.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr fullclone() const;
+  //@}
+
+private:
+
+  /**
+   * The assignment operator is private and must never be called.
+   * In fact, it should not even be implemented.
+   */
+  SMTopPOWHEGDecayer & operator=(const SMTopPOWHEGDecayer &);
+
+};
+
+}
+
+#endif /* Herwig_SMTopPOWHEGDecayer_H */
