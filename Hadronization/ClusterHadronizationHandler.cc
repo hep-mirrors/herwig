91fcb83f57aa3b3056a04088553ade4faf9d4df6
David Grellscheid <david.grellscheid@durham.ac.uk>
2013-02-14 14:46:50 +0000

    Fix for broken muon lifelength across technical 1-1 vertex. Still need to check if original problem remains fixed
    --HG--
    branch : release-2-6

--- Hadronization/ClusterHadronizationHandler.cc
+++ Hadronization/ClusterHadronizationHandler.cc
@@ -266,34 +266,44 @@ handle(EventHandler & ch, const tPVector & tagged,
     assert(_underlyingEventHandler);
     ch.performStep(_underlyingEventHandler,Hint::Default());
   }
-  // zero all positions
+  // calculate positions
   // extract all particles from the event
   tEventPtr event=ch.currentEvent();
   vector<tPPtr> particles;
   particles.reserve(256);
   event->select(back_inserter(particles), ThePEG::AllSelector());
-  // and the final-state particles
-  set<tPPtr> finalstate;
-  event->selectFinalState(inserter(finalstate));
   for(vector<tPPtr>::const_iterator pit=particles.begin();
-      pit!=particles.end();++pit) {
-    // fix the vertex position
-    tPPtr parent = (**pit).parents().empty() ? tPPtr() : (**pit).parents()[0];
-    bool inClusters(false);
-    bool newVertex(false);
-    while(parent&&!parent->parents().empty()) {
-      if(!inClusters&&parent->id()==ParticleID::Cluster) {
+      pit!=particles.end(); ++pit) {
+    if ((**pit).parents().empty()) 
+      continue;
+    tPPtr parent = (**pit).parents()[0];
+    // fudged fix for the shower's technical vertices:
+    // make lifelength for 1-1 vertices zero
+    // \todo sort out shower inserted vertices properly
+    if ( (**pit).id() == parent->id() 
+	 && (**pit).parents().size() == 1
+	 && parent->children().size() == 1 ) {
+      parent->setLifeLength(LorentzDistance());
+      // ??? (**pit).setVertex( parent->vertex() );
+    }
+    // fix the vertex position for particles from clusters
+    bool inClusters = false;
+    bool newVertex  = false;
+    // iterate up the ancestry to find the origin of the parent cluster
+    while( !parent->parents().empty() ) {
+      bool parentIsCluster = parent->id() == ParticleID::Cluster;
+      if ( !inClusters && parentIsCluster ) {
 	inClusters = true;
       }
-      else if(inClusters&&parent->id()!=ParticleID::Cluster) {
+      else if ( inClusters && !parentIsCluster ) {
 	newVertex = true;
 	break;
       }
       parent = parent->parents()[0]; 
     }
-    if(newVertex) (**pit).setVertex(parent->vertex());
+    // parent is now the ancestor of the cluster chain
+    if ( newVertex ) (**pit).setVertex( parent->vertex() );
   }
-  //  currentHandler_ = 0;
 }
 
 void ClusterHadronizationHandler::_setChildren(ClusterVector clusters) const {
