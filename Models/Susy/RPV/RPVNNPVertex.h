c5a060f2a1765b0b5a5f78cc57fcd9ccc09a8e01
Peter Richardson <Peter.Richardson@durham.ac.uk>
2013-03-15 12:03:02 +0000

    added vertex for NNP in RPV models

---
+++ Models/Susy/RPV/RPVNNPVertex.h
@@ -0,0 +1,228 @@
+// -*- C++ -*-
+//
+// RPVNNPVertex.h is a part of Herwig++ - A multi-purpose Monte Carlo event generator
+// Copyright (C) 2002-2011 The Herwig Collaboration
+//
+// Herwig++ is licenced under version 2 of the GPL, see COPYING for details.
+// Please respect the MCnet academic guidelines, see GUIDELINES for details.
+//
+#ifndef HERWIG_RPVNNPVertex_H
+#define HERWIG_RPVNNPVertex_H
+//
+// This is the declaration of the RPVNNPVertex class.
+//
+
+#include "ThePEG/Helicity/Vertex/Vector/GeneralFFVVertex.h"
+#include "Herwig++/Models/Susy/MSSM.h"
+#include "Herwig++/Models/Susy/MixingMatrix.fh"
+
+namespace Herwig {
+using namespace ThePEG;
+
+/**
+ * This class implements the coupling of a photon to a pair of neutralinos
+ * via loop diagrams
+ * It inherits from GeneralFFVertex and implements the setCoupling method.
+ *
+ * @see \ref RPVNNPVertexInterfaces "The interfaces"
+ * defined for RPVNNPVertex.
+ * @see FFVertex
+ */
+class RPVNNPVertex: public GeneralFFVVertex {
+
+public:
+
+  /**
+   * The default constructor.
+   */
+  RPVNNPVertex();
+
+public:
+
+  /** @name Functions used by the persistent I/O system. */
+  //@{
+  /**
+   * Function used to write out object persistently.
+   * @param os the persistent output stream written to.
+   */
+  void persistentOutput(PersistentOStream & os) const;
+
+  /**
+   * Function used to read in object persistently.
+   * @param is the persistent input stream read from.
+   * @param version the version number of the object when written.
+   */
+  void persistentInput(PersistentIStream & is, int version);
+  //@}
+
+  /**
+   * The standard Init function used to initialize the interfaces.
+   * Called exactly once for each class by the class description system
+   * before the main function starts or
+   * when this class is dynamically loaded.
+   */
+  static void Init();
+
+  /**
+   * Calculate the couplings.
+   * @param q2 The scale \f$q^2\f$ for the coupling at the vertex.
+   * @param part1 The ParticleData pointer for the first  particle.
+   * @param part2 The ParticleData pointer for the second particle.
+   * @param part3 The ParticleData pointer for the third  particle.
+   */
+  virtual void setCoupling(Energy2 q2, tcPDPtr part1,
+                           tcPDPtr part2, tcPDPtr part3);
+
+protected:
+
+  /** @name Clone Methods. */
+  //@{
+  /**
+   * Make a simple clone of this object.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr clone() const {return new_ptr(*this);}
+
+  /** Make a clone of this object, possibly modifying the cloned object
+   * to make it sane.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr fullclone() const {return new_ptr(*this);}
+  //@}
+
+protected:
+
+  /**
+   *  Evaluate the loop integrals
+   */
+  void loopIntegrals(Energy Mi, Energy Mj, Energy M, Energy m,
+		     complex<InvEnergy2> & I, complex<InvEnergy2> & J,
+		     complex<InvEnergy2> & K, complex<InvEnergy2> & I2);
+
+protected:
+
+  /** @name Standard Interfaced functions. */
+  //@{
+  /**
+   * Initialize this object after the setup phase before saving an
+   * EventGenerator to disk.
+   * @throws InitException if object could not be initialized properly.
+   */
+  virtual void doinit();
+
+  /**
+   * Initialize this object. Called in the run phase just before
+   * a run begins.
+   */
+  virtual void doinitrun();
+
+  /**
+   * Finalize this object. Called in the run phase just after a
+   * run has ended. Used eg. to write out statistics.
+   */
+  virtual void dofinish();
+  //@}
+
+private:
+
+  /**
+   * The assignment operator is private and must never be called.
+   * In fact, it should not even be implemented.
+   */
+  RPVNNPVertex & operator=(const RPVNNPVertex &);
+
+private:
+
+  /**
+   *  Whether of not to include on-shell intermediate states
+   */
+  bool _includeOnShell;
+
+  /**
+   * Pointer to the stop mixing matrix
+   */
+  tMixingMatrixPtr stop_;
+
+  /**
+   * Pointer to the sbottom mixing matrix
+   */
+  tMixingMatrixPtr sbot_;
+
+  /**
+   * Pointer to the stau mixing matrix
+   */
+  tMixingMatrixPtr stau_;
+
+  /**
+   * The value of \f$sin(\theta_W)\f$
+   */
+  double sw_;
+  
+  /**
+   * The value of \f$cos(\theta_W)\f$
+   */
+  double cw_;
+  
+  /**
+   * Mass of the W
+   */
+  Energy _mw;
+
+  /**
+   * \f$\sin(\beta)\f$
+   */
+  double sb_;
+  
+  /**
+   * \f$\cos(\beta)\f$
+   */
+  double cb_;
+
+  /**
+   * Store the neutralino mixing matrix
+   */
+  tMixingMatrixPtr Nmix_;
+
+  /**
+   * Store the U-type chargino mixing matrix
+   */
+  tMixingMatrixPtr Umix_;
+
+  /**
+   * Store the V-type chargino mixing matrix
+   */
+  tMixingMatrixPtr Vmix_;
+
+  /**
+   * Store the id of the 1st neutralino when the coupling was last evaluated
+   */
+  long id1Last_;
+
+  /**
+   * Store the id of the 1st neutralino when the coupling was last evaluated
+   */
+  long id2Last_;
+
+  /**
+   * Store the value at which the coupling when it was last evalutated
+   */
+  Energy2 q2Last_;
+
+  /**
+   * Store the value of the coupling when it was last evalutated
+   */
+  Complex coupLast_;
+
+  /**
+   * Store the value of the left-coupling when it was last evalutated
+   */
+  complex<InvEnergy> leftLast_;
+
+  /**
+   * Store the value of the right-coupling when it was last evalutated
+   */
+  complex<InvEnergy> rightLast_;
+};
+}
+
+#endif /* HERWIG_RPVNNPVertex_H */
