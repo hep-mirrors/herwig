9871eca4d034dfd613c2962bece3f4fd32d40c77
Simon Plätzer <simon.plaetzer@desy.de>
2013-07-02 10:21:54 +0200

    add simple weight analyzer

---
+++ MatrixElement/Matchbox/Tests/WeightAnalyzer.cc
@@ -0,0 +1,133 @@
+// -*- C++ -*-
+//
+// This is the implementation of the non-inlined, non-templated member
+// functions of the WeightAnalyzer class.
+//
+
+#include "WeightAnalyzer.h"
+#include "ThePEG/Interface/ClassDocumentation.h"
+#include "ThePEG/EventRecord/Particle.h"
+#include "ThePEG/Repository/UseRandom.h"
+#include "ThePEG/Repository/EventGenerator.h"
+#include "ThePEG/Utilities/DescribeClass.h"
+
+#include "ThePEG/EventRecord/SubProcess.h"
+#include "ThePEG/EventRecord/SubProcessGroup.h"
+
+#include "ThePEG/Persistency/PersistentOStream.h"
+#include "ThePEG/Persistency/PersistentIStream.h"
+
+using namespace Herwig;
+
+WeightAnalyzer::WeightAnalyzer() 
+  : sumWeights(0.0), sumPositiveWeights(0.0),
+    sumNegativeWeights(0.0),
+    sumGroupWeights(0.0), sumPositiveGroupWeights(0.0),
+    sumNegativeGroupWeights(0.0) {}
+
+WeightAnalyzer::~WeightAnalyzer() {}
+
+
+
+#ifndef LWH_AIAnalysisFactory_H
+#ifndef LWH 
+#define LWH ThePEGLWH
+#endif
+#include "ThePEG/Analysis/LWH/AnalysisFactory.h"
+#endif
+
+void WeightAnalyzer::analyze(tEventPtr event, long ieve, int loop, int state) {
+  AnalysisHandler::analyze(event, ieve, loop, state);
+
+  sumWeights += event->weight();
+  if ( event->weight() > 0.0 )
+    sumPositiveWeights += event->weight();
+  if ( event->weight() < 0.0 )
+    sumNegativeWeights += event->weight();
+
+  tSubProPtr sub = event->primarySubProcess();
+  Ptr<SubProcessGroup>::tptr grp = 
+    dynamic_ptr_cast<Ptr<SubProcessGroup>::tptr>(sub);
+
+  sumGroupWeights += event->weight()*sub->groupWeight();
+  if ( event->weight()*sub->groupWeight() > 0.0 )
+    sumPositiveGroupWeights += event->weight()*sub->groupWeight();
+  if ( event->weight()*sub->groupWeight() < 0.0 )
+    sumNegativeGroupWeights += event->weight()*sub->groupWeight();
+
+  if ( grp ) {
+    
+    for ( SubProcessVector::const_iterator s = grp->dependent().begin();
+	  s != grp->dependent().end(); ++s ) {
+
+      sumGroupWeights += event->weight()*(**s).groupWeight();
+
+      if ( event->weight()*(**s).groupWeight() > 0.0 )
+	sumPositiveGroupWeights += event->weight()*(**s).groupWeight();
+      if ( event->weight()*(**s).groupWeight() < 0.0 )
+	sumNegativeGroupWeights += event->weight()*(**s).groupWeight();
+
+    }
+
+  }
+
+}
+
+void WeightAnalyzer::dofinish() {
+  AnalysisHandler::dofinish();
+
+  ofstream weightAnalyzerOut("weights.dat");
+
+  weightAnalyzerOut << setprecision(20)
+		    << "--------------------------------------------------------------------------------\n"
+		     << "WeightAnalyzer information\n"
+		     << "--------------------------------------------------------------------------------\n"
+		     << "sum of weights                        : " << sumWeights << "\n"
+		     << "sum of positive weights               : " << sumPositiveWeights << "\n"
+		     << "sum of negative weights               : " << sumNegativeWeights << "\n" 
+		     << "sum of weights (from groups)          : " << sumGroupWeights << "\n"
+		     << "sum of positive weights (from groups) : " << sumPositiveGroupWeights << "\n"
+		     << "sum of negative weights (from groups) : " << sumNegativeGroupWeights << "\n"
+		     << "--------------------------------------------------------------------------------\n"
+		     << flush;
+
+}
+
+void WeightAnalyzer::doinitrun() {
+  AnalysisHandler::doinitrun();
+}
+
+
+IBPtr WeightAnalyzer::clone() const {
+  return new_ptr(*this);
+}
+
+IBPtr WeightAnalyzer::fullclone() const {
+  return new_ptr(*this);
+}
+
+
+// If needed, insert default implementations of virtual function defined
+// in the InterfacedBase class here (using ThePEG-interfaced-impl in Emacs).
+
+
+void WeightAnalyzer::persistentOutput(PersistentOStream &) const {}
+
+void WeightAnalyzer::persistentInput(PersistentIStream &, int) {}
+
+
+// *** Attention *** The following static variable is needed for the type
+// description system in ThePEG. Please check that the template arguments
+// are correct (the class and its base class), and that the constructor
+// arguments are correct (the class name and the name of the dynamically
+// loadable library where the class implementation can be found).
+DescribeClass<WeightAnalyzer,AnalysisHandler>
+  describeHerwigWeightAnalyzer("Herwig::WeightAnalyzer", "HwMatchbox.so");
+
+void WeightAnalyzer::Init() {
+
+  static ClassDocumentation<WeightAnalyzer> documentation
+    ("There is no documentation for the WeightAnalyzer class");
+
+}
+
