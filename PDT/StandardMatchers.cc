2754e78a86fe9d94b8c36585f52727c962f79db9
Peter Richardson <Peter.Richardson@durham.ac.uk>
2013-09-27 13:58:19 +0100

    added matcher for charged leptons, rather than all leptons for FR testing

--- PDT/StandardMatchers.cc
+++ PDT/StandardMatchers.cc
@@ -13,10 +13,6 @@
 #include "ThePEG/PDT/StandardMatchers.h"
 #include "StandardMatchers.h"
 
-#ifdef ThePEG_TEMPLATES_IN_CC_FILE
-#include "Matcher.tcc"
-#endif
-
 using namespace Herwig;
 using namespace ThePEG;
 
@@ -30,6 +26,7 @@ void dummy() {
   static MatchWBoson m03;
   static MatchZBoson m04;
   static MatchHiggsBoson m05;
+  static MatchChargedLepton m06;
 }
 
 }
