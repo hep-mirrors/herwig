b9160f9732aefb237d98c30780602e6e6968c4f6
Christian Reuschle <christian.reuschle@kit.edu>
2014-01-28 20:38:19 +0100

    Implemented cut(s) related to the separation in the legoplot plane

---
+++ MatrixElement/Matchbox/Cuts/MatchboxDeltaRCut.cc
@@ -0,0 +1,189 @@
+// -*- C++ -*-
+//
+// MatchboxDeltaRCut.cc is a part of Herwig++ - A multi-purpose Monte Carlo event generator
+// Copyright (C) 2002-2012 The Herwig Collaboration
+//
+// Herwig++ is licenced under version 2 of the GPL, see COPYING for details.
+// Please respect the MCnet academic guidelines, see GUIDELINES for details.
+//
+//
+// This is the implementation of the non-inlined, non-templated member
+// functions of the MatchboxDeltaRCut class.
+//
+
+#include "MatchboxDeltaRCut.h"
+#include "ThePEG/Interface/ClassDocumentation.h"
+#include "ThePEG/Interface/Parameter.h"
+#include "ThePEG/Interface/Command.h"
+#include "ThePEG/Interface/Reference.h"
+#include "ThePEG/EventRecord/Particle.h"
+#include "ThePEG/Repository/UseRandom.h"
+#include "ThePEG/Repository/EventGenerator.h"
+#include "ThePEG/Utilities/DescribeClass.h"
+#include "ThePEG/Repository/CurrentGenerator.h"
+#include "ThePEG/Cuts/Cuts.h"
+
+#include "ThePEG/Persistency/PersistentOStream.h"
+#include "ThePEG/Persistency/PersistentIStream.h"
+
+using namespace Herwig;
+
+MatchboxDeltaRCut::MatchboxDeltaRCut() 
+  : theDeltaRMin(0.0), theDeltaRMax(Constants::MaxRapidity), theDeltaYMin(0.0), theDeltaYMax(Constants::MaxRapidity), theDeltaPhiMin(0.0), theDeltaPhiMax(2.0*Constants::pi) {}
+
+MatchboxDeltaRCut::~MatchboxDeltaRCut() {}
+
+IBPtr MatchboxDeltaRCut::clone() const {
+  return new_ptr(*this);
+}
+
+IBPtr MatchboxDeltaRCut::fullclone() const {
+  return new_ptr(*this);
+}
+
+bool MatchboxDeltaRCut::passCuts(tcCutsPtr parent, tcPDPtr pitype, tcPDPtr pjtype,
+		                 LorentzMomentum pi, LorentzMomentum pj,
+		                 bool inci, bool incj) const {
+
+  bool match = false;
+  if ( theFirstMatcher->matches(*pitype) && theSecondMatcher->matches(*pjtype) ) match = true;
+  if ( theFirstMatcher->matches(*pjtype) && theSecondMatcher->matches(*pitype) ) match = true;
+  if ( !match ) return true;
+  if ( inci || incj ) return true;
+
+  double weight = 1.0;
+
+  double dY = abs(pi.rapidity() - pj.rapidity());
+  double dPhi = abs(pi.phi() - pj.phi());
+  if ( dPhi > Constants::pi ) dPhi = 2.0*Constants::pi - dPhi;
+  double dR = sqrt(sqr(dY) + sqr(dPhi));
+  if ( !parent->isInside<CutTypes::Rapidity>(dY,deltaYMin(),deltaYMax(),weight) ) 
+  {
+    parent->lastCutWeight(0.0);
+    return false;
+  }
+  if ( !parent->isInside<CutTypes::Azimuth>(dPhi,deltaPhiMin(),deltaPhiMax(),weight) ) 
+  {
+    parent->lastCutWeight(0.0);
+    return false;
+  }
+  if ( !parent->isInside<CutTypes::Rapidity>(dR,deltaRMin(),deltaRMax(),weight) ) 
+  {
+    parent->lastCutWeight(0.0);
+    return false;
+  }
+
+  parent->lastCutWeight(weight);
+  return true;
+
+}
+
+void MatchboxDeltaRCut::describe() const {
+
+  CurrentGenerator::log() 
+    << fullName() << ":\n"
+    << "DeltaRMin = " << theDeltaRMin << " \n"
+    << "DeltaRMax = " << theDeltaRMax << " \n"
+    << "DeltaPhiMin = " << theDeltaPhiMin << " \n"
+    << "DeltaPhiMax = " << theDeltaPhiMax << " \n"
+    << "DeltaYMin = " << theDeltaYMin << " \n"
+    << "DeltaYMax = " << theDeltaYMax << " \n"
+    << "FirstMatcher = " << theFirstMatcher << " \n"
+    << "SecondMatcher = " << theSecondMatcher << " \n\n";
+
+}
+
+// If needed, insert default implementations of virtual function defined
+// in the InterfacedBase class here (using ThePEG-interfaced-impl in Emacs).
+
+
+void MatchboxDeltaRCut::persistentOutput(PersistentOStream & os) const {
+  os << theDeltaYMin << theDeltaYMax 
+     << theDeltaPhiMin << theDeltaPhiMax 
+     << theDeltaRMin << theDeltaRMax 
+     << theFirstMatcher << theSecondMatcher;
+}
+
+void MatchboxDeltaRCut::persistentInput(PersistentIStream & is, int) {
+  is >> theDeltaYMin >> theDeltaYMax 
+     >> theDeltaPhiMin >> theDeltaPhiMax 
+     >> theDeltaRMin >> theDeltaRMax 
+     >> theFirstMatcher >> theSecondMatcher;
+}
+
+// *** Attention *** The following static variable is needed for the type
+// description system in ThePEG. Please check that the template arguments
+// are correct (the class and its base class), and that the constructor
+// arguments are correct (the class name and the name of the dynamically
+// loadable library where the class implementation can be found).
+DescribeClass<MatchboxDeltaRCut,TwoCutBase>
+  describeHerwigMatchboxDeltaRCut("Herwig::MatchboxDeltaRCut", "HwMatchboxCuts.so");
+
+void MatchboxDeltaRCut::Init() {
+
+  static ClassDocumentation<MatchboxDeltaRCut> documentation
+    ("This class implements cuts on legoplot, rapidity and azimuthal separation, "
+     "i.e. on the \\f$\\Delta R\\f$-measure and on \\f$\\Delta Y\\f$ and \\f$\\Delta \\phi\\f$. "
+     "By default the cuts are only applied to coloured particles, but "
+     "may optionally be applied to all particle types. ");
+
+  static Parameter<MatchboxDeltaRCut,double> interfaceDeltaRMin
+    ("DeltaRMin",
+     "The minimum allowed for the legoplot distance "
+     "\\f$\\Delta R_{ij}=\\sqrt{\\Delta \\phi_{ij}^2+\\Delta Y_{ij}^2}\\f$ ",
+     &MatchboxDeltaRCut::theDeltaRMin, 0.0, 0.0, 0,
+     false, false, Interface::lowerlim);
+
+  static Parameter<MatchboxDeltaRCut,double> interfaceDeltaRMax
+    ("DeltaRMax",
+     "The maximum allowed for the legoplot distance "
+     "\\f$\\Delta R_{ij}=\\sqrt{\\Delta \\phi_{ij}^2+\\Delta Y_{ij}^2}\\f$ ",
+     &MatchboxDeltaRCut::theDeltaRMax, Constants::MaxRapidity, 0, 0,
+     false, false, Interface::lowerlim);
+
+  static Parameter<MatchboxDeltaRCut,double> interfaceDeltaPhiMin
+    ("DeltaPhiMin",
+     "The minimum allowed for the azimuthal separation "
+     "\\f$\\Delta \\phi_{ij}\\f$ ",
+     &MatchboxDeltaRCut::theDeltaPhiMin, 0.0, 0.0, 0,
+     false, false, Interface::lowerlim);
+
+  static Parameter<MatchboxDeltaRCut,double> interfaceDeltaPhiMax
+    ("DeltaPhiMax",
+     "The maximum allowed for the azimuthal separation "
+     "\\f$\\Delta \\phi_{ij}\\f$ ",
+     &MatchboxDeltaRCut::theDeltaPhiMax, 2.0*Constants::pi, 0, 0,
+     false, false, Interface::lowerlim);
+
+  static Parameter<MatchboxDeltaRCut,double> interfaceDeltaYMin
+    ("DeltaYMin",
+     "The minimum allowed for the rapidity separation "
+     "\\f$\\Delta Y_{ij}\\f$ ",
+     &MatchboxDeltaRCut::theDeltaYMin, 0.0, 0.0, 0,
+     false, false, Interface::lowerlim);
+
+  static Parameter<MatchboxDeltaRCut,double> interfaceDeltaYMax
+    ("DeltaYMax",
+     "The maximum allowed for the rapidity separation "
+     "\\f$\\Delta Y_{ij}\\f$ ",
+     &MatchboxDeltaRCut::theDeltaYMax, Constants::MaxRapidity, 0, 0,
+     false, false, Interface::lowerlim);
+
+  static Reference<MatchboxDeltaRCut,MatcherBase> interfaceFirstMatcher
+    ("FirstMatcher",
+     "Matcher for first particle of type pitype in the pair (pitype,pjtype). "
+     "If non-null only particles matching this object will be affected "
+     "by the cut. ",
+     &MatchboxDeltaRCut::theFirstMatcher, true, false, true, true, false);
+//      &MatchboxDeltaRCut::theFirstMatcher, false, false, true, false, false);
+
+  static Reference<MatchboxDeltaRCut,MatcherBase> interfaceSecondMatcher
+    ("SecondMatcher",
+     "Matcher for second particle of type pjtype in the pair (pitype,pjtype). "
+     "If non-null only particles matching this object will be affected "
+     "by the cut. ",
+     &MatchboxDeltaRCut::theSecondMatcher, true, false, true, true, false);
+//      &MatchboxDeltaRCut::theSecondMatcher, false, false, true, false, false);
+
+}
+
---
+++ MatrixElement/Matchbox/Cuts/MatchboxDeltaRCut.h
@@ -0,0 +1,233 @@
+// -*- C++ -*-
+//
+// MatchboxDeltaRCut.h is a part of Herwig++ - A multi-purpose Monte Carlo event generator
+// Copyright (C) 2002-2012 The Herwig Collaboration
+//
+// Herwig++ is licenced under version 2 of the GPL, see COPYING for details.
+// Please respect the MCnet academic guidelines, see GUIDELINES for details.
+//
+#ifndef Herwig_MatchboxDeltaRCut_H
+#define Herwig_MatchboxDeltaRCut_H
+//
+// This is the declaration of the MatchboxDeltaRCut class.
+//
+
+#include "ThePEG/Cuts/TwoCutBase.h"
+#include "ThePEG/PDT/MatcherBase.h"
+
+namespace Herwig {
+
+using namespace ThePEG;
+
+/**
+ * \ingroup Matchbox
+ * \author Christian Reuschle
+ *
+ * \brief MatchboxDeltaRCut implements cuts related to the separation in the legoplot plane
+ *
+ * @see \ref MatchboxDeltaRCutInterfaces "The interfaces"
+ * defined for MatchboxDeltaRCut.
+ */
+class MatchboxDeltaRCut: public TwoCutBase {
+
+public:
+
+  /** @name Standard constructors and destructors. */
+  //@{
+  /**
+   * The default constructor.
+   */
+  MatchboxDeltaRCut();
+
+  /**
+   * The destructor.
+   */
+  virtual ~MatchboxDeltaRCut();
+  //@}
+
+public:
+
+  /** @name Virtual functions to be overridden by sub-classes. */
+  //@{
+  /**
+   * Return the minimum allowed value of the longitudinally invariant
+   * \f$k_\perp\f$-algorithms distance measure. This is defined as
+   * \f$\min(p_{\perp i}, p_{\perp
+   * j})\sqrt{\Delta\eta_{ij}^2+\Delta\phi_{ij}^2}\f$ for two outgoing
+   * partons, or simply \f$p_{\perp i}\f$ or \f$p_{\perp j}\f$ for a
+   * single outgoing parton. Returns 0 if both partons are incoming. A
+   * null pointer indicates an incoming parton, hence the type of the
+   * incoming parton is irrelevant.
+   */
+  virtual Energy minDeltaMeasureCuts(tcPDPtr , tcPDPtr ) const { return ZERO; }
+  // virtual Energy minDeltaMeasureCuts(tcPDPtr pi, tcPDPtr pj) const { return ZERO; }
+
+  /**
+   * Return the minimum allowed value of the longitudinally invariant
+   * \f$k_\perp\f$-algorithms distance measure. Returns ZERO.
+   */
+  virtual Energy minKTClus(tcPDPtr , tcPDPtr ) const { return ZERO; }
+  // virtual Energy minKTClus(tcPDPtr pi, tcPDPtr pj) const { return ZERO; }
+
+  /**
+   * Return the minimum allowed squared invariant mass of two outgoing
+   * partons of type \a pi and \a pj. Returns zero.
+   */
+  virtual Energy2 minSij(tcPDPtr , tcPDPtr ) const { return ZERO; }
+  // virtual Energy2 minSij(tcPDPtr pi, tcPDPtr pj) const { return ZERO; }
+
+  /**
+   * Return the minimum allowed value of the negative of the squared
+   * invariant mass of an incoming parton of type \a pi and an
+   * outgoing parton of type \a po. Returns zero.
+   */
+  virtual Energy2 minTij(tcPDPtr , tcPDPtr ) const { return ZERO; }
+  // virtual Energy2 minTij(tcPDPtr pi, tcPDPtr po) const { return ZERO; }
+
+  /**
+   * Return the minimum allowed value of \f$\Delta
+   * R_{ij}=\sqrt{\Delta\eta_{ij}^2+\Delta\phi_{ij}^2}\f$ of two
+   * outgoing partons of type \a pi and \a pj. Returns zero.
+   */
+  virtual double minDeltaR(tcPDPtr , tcPDPtr ) const { return ZERO; }
+  // virtual double minDeltaR(tcPDPtr pi, tcPDPtr pj) const { return ZERO; }
+
+  /**
+   * Return the minimum allowed value of the Durham
+   * \f$k_\perp\f$-algorithms distance measure. This is defined as
+   * \f$2\min(E_j^2, E_j^2)(1-\cos\theta_{ij})/\hat{s}\f$ for two
+   * outgoing partons. Returns zero.
+   */
+  virtual double minDurham(tcPDPtr , tcPDPtr ) const { return 0.0; }
+  // virtual double minDurham(tcPDPtr pi, tcPDPtr pj) const { return 0.0; }
+
+  /**
+   * Return true if a pair of particles with type \a pitype and \a
+   * pjtype and momenta \a pi and \a pj respectively passes the
+   * cuts. \a inci and \a inj indicates if the corresponding particles
+   * are incoming.
+   */
+  virtual bool passCuts(tcCutsPtr parent, tcPDPtr pitype, tcPDPtr pjtype,
+			LorentzMomentum pi, LorentzMomentum pj,
+			bool inci = false, bool incj = false) const;
+
+  /**
+   * Describe the currently active cuts in the log file.
+   */
+  virtual void describe() const;
+  //@}
+
+public:
+
+  /**
+   * Return the minimum and maximum allowed legoplot separation
+   */
+  double deltaRMin() const { return theDeltaRMin; }
+  double deltaRMax() const { return theDeltaRMax; }
+
+  /**
+   * Return the minimum and maximum allowed rapidity separation
+   */
+  double deltaYMin() const { return theDeltaYMin; }
+  double deltaYMax() const { return theDeltaYMax; }
+
+  /**
+   * Return the minimum and maximum allowed azimuthal separation
+   */
+  double deltaPhiMin() const { return theDeltaPhiMin; }
+  double deltaPhiMax() const { return theDeltaPhiMax; }
+
+  /**
+   * Return the matchers for a pair of particles to cut on. Only a pair
+   * of particles, matching these objects, will be affected.
+   */
+  Ptr<MatcherBase>::tptr firstMatcher() const { return theFirstMatcher; }
+  Ptr<MatcherBase>::tptr secondMatcher() const { return theSecondMatcher; }
+
+public:
+
+  /** @name Functions used by the persistent I/O system. */
+  //@{
+  /**
+   * Function used to write out object persistently.
+   * @param os the persistent output stream written to.
+   */
+  void persistentOutput(PersistentOStream & os) const;
+
+  /**
+   * Function used to read in object persistently.
+   * @param is the persistent input stream read from.
+   * @param version the version number of the object when written.
+   */
+  void persistentInput(PersistentIStream & is, int version);
+  //@}
+
+  /**
+   * The standard Init function used to initialize the interfaces.
+   * Called exactly once for each class by the class description system
+   * before the main function starts or
+   * when this class is dynamically loaded.
+   */
+  static void Init();
+
+protected:
+
+  /** @name Clone Methods. */
+  //@{
+  /**
+   * Make a simple clone of this object.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr clone() const;
+
+  /** Make a clone of this object, possibly modifying the cloned object
+   * to make it sane.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr fullclone() const;
+  //@}
+
+
+// If needed, insert declarations of virtual function defined in the
+// InterfacedBase class here (using ThePEG-interfaced-decl in Emacs).
+
+private:
+
+  /**
+   * The minimum and maximum allowed legoplot separation
+   */
+  double theDeltaRMin;
+  double theDeltaRMax;
+
+  /**
+   * The minimum and maximum allowed rapidity separation
+   */
+  double theDeltaYMin;
+  double theDeltaYMax;
+
+  /**
+   * The minimum and maximum allowed azimuthal separation
+   */
+  double theDeltaPhiMin;
+  double theDeltaPhiMax;
+
+  /**
+   * Matchers for a pair of particles to cut on. Only a pair
+   * of particles, matching these objects, will be affected.
+   */
+  Ptr<MatcherBase>::ptr theFirstMatcher;
+  Ptr<MatcherBase>::ptr theSecondMatcher;
+  
+private:
+
+  /**
+   * The assignment operator is private and must never be called.
+   * In fact, it should not even be implemented.
+   */
+  MatchboxDeltaRCut & operator=(const MatchboxDeltaRCut &);
+
+};
+
+}
+
+#endif /* Herwig_MatchboxDeltaRCut_H */
