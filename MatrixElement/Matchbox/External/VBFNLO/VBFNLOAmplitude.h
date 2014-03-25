a2751b54379b669c7c775cd16e03a194d619476d
Simon Plätzer <simon.plaetzer@desy.de>
2014-03-25 10:38:10 +0100

    some more fixes

--- MatrixElement/Matchbox/External/VBFNLO/VBFNLOAmplitude.h
+++ MatrixElement/Matchbox/External/VBFNLO/VBFNLOAmplitude.h
@@ -138,6 +138,25 @@ public:
 
 protected:
 
+  /** @name Standard Interfaced functions. */
+  //@{
+
+  /**
+   * Initialize this object after the setup phase before saving an
+   * EventGenerator to disk.
+   * @throws InitException if object could not be initialized properly.
+   */
+  virtual void doinit();
+
+  /**
+   * Initialize this object. Called in the run phase just before
+   * a run begins.
+   */
+  virtual void doinitrun();
+  //@}
+
+protected:
+
   /** @name Clone Methods. */
   //@{
   /**
