5c44f0173d7fd3decc439f02868f2b717dfb1a62
Christian Reuschle <christian.reuschle@kit.edu>
2014-06-06 22:46:51 +0200

    Matchbox scales: Scales for processes with up to three vector bosons plus jets.

---
+++ MatrixElement/Matchbox/Scales/MatchboxTriVecScales.h
@@ -0,0 +1,150 @@
+// -*- C++ -*-
+//
+// MatchboxTriVecScales.h is a part of Herwig++ - A multi-purpose Monte Carlo event generator
+// Copyright (C) 2002-2012 The Herwig Collaboration
+//
+// Herwig++ is licenced under version 2 of the GPL, see COPYING for details.
+// Please respect the MCnet academic guidelines, see GUIDELINES for details.
+//
+#ifndef Herwig_MatchboxTriVecScales_H
+#define Herwig_MatchboxTriVecScales_H
+//
+// This is the declaration of the MatchboxTriVecScales class.
+//
+
+#include "Herwig++/MatrixElement/Matchbox/Utility/MatchboxScaleChoice.h"
+#include "ThePEG/PDT/MatcherBase.h"
+#include "ThePEG/Cuts/JetFinder.h"
+
+namespace Herwig {
+
+using namespace ThePEG;
+
+/**
+ * \ingroup Matchbox
+ * \author Christian Reuschle
+ *
+ * \brief MatchboxTriVecScales implements scale choices related to transverse momenta and transverse energies
+ * in events with up to three vector bosons in the final state, plus additional jets, where the vector bosons
+ * are associated to lepton pairs of distinct families.
+ *
+ */
+class MatchboxTriVecScales: public MatchboxScaleChoice {
+
+public:
+
+  /** @name Standard constructors and destructors. */
+  //@{
+  /**
+   * The default constructor.
+   */
+  MatchboxTriVecScales();
+
+  /**
+   * The destructor.
+   */
+  virtual ~MatchboxTriVecScales();
+  //@}
+
+public:
+
+  /**
+   * Return the HtPrime scale.
+   */
+  virtual Energy2 HtPrimeScale() const;
+
+  /**
+   * Return the rapidity modified HtPrime scale.
+   */
+  virtual Energy2 HtPrimeModScale() const;
+
+  /**
+   * Return the Et scale.
+   */
+  virtual Energy2 EtScale() const;
+
+  /**
+   * Return the renormalization scale. This default version returns
+   * shat.
+   */
+  virtual Energy2 renormalizationScale() const;
+
+  /**
+   * Return the factorization scale. This default version returns
+   * shat.
+   */
+  virtual Energy2 factorizationScale() const;
+
+public:
+
+  /** @name Functions used by the persistent I/O system. */
+  //@{
+  /**
+   * Function used to write out object persistently.
+   * @param os the persistent output stream written to.
+   */
+  void persistentOutput(PersistentOStream & os) const;
+
+  /**
+   * Function used to read in object persistently.
+   * @param is the persistent input stream read from.
+   * @param version the version number of the object when written.
+   */
+  void persistentInput(PersistentIStream & is, int version);
+  //@}
+
+  /**
+   * The standard Init function used to initialize the interfaces.
+   * Called exactly once for each class by the class description system
+   * before the main function starts or
+   * when this class is dynamically loaded.
+   */
+  static void Init();
+
+protected:
+
+  /** @name Clone Methods. */
+  //@{
+  /**
+   * Make a simple clone of this object.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr clone() const;
+
+  /** Make a clone of this object, possibly modifying the cloned object
+   * to make it sane.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr fullclone() const;
+  //@}
+
+private:
+
+  /**
+   * Reference to the jet finder
+   */
+  Ptr<JetFinder>::ptr theJetFinder;
+
+  /**
+   * The scale choice to use.
+   */
+  unsigned int theTriVecScaleChoice;
+
+
+// If needed, insert declarations of virtual function defined in the
+// InterfacedBase class here (using ThePEG-interfaced-decl in Emacs).
+
+
+private:
+
+  /**
+   * The assignment operator is private and must never be called.
+   * In fact, it should not even be implemented.
+   */
+  MatchboxTriVecScales & operator=(const MatchboxTriVecScales &);
+
+};
+
+}
+
+#endif /* Herwig_MatchboxTriVecScales_H */
