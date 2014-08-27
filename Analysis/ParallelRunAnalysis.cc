d9b2e0bc968e09f45c507560da0b2296250d218a
Daniel Rauch <daniel.rauch2@student.kit.edu>
2014-08-27 14:08:54 +0200

    added ParallelRunAnalysis to create output for the combination of parallel runs

---
+++ Analysis/ParallelRunAnalysis.cc
@@ -0,0 +1,56 @@
+#include <fstream>
+#include "ParallelRunAnalysis.h"
+#include "ThePEG/Handlers/SamplerBase.h"
+#include "ThePEG/Repository/Repository.h"
+#include "ThePEG/Handlers/EventHandler.h"
+#include "ThePEG/Handlers/StandardEventHandler.h"
+#include "ThePEG/Repository/EventGenerator.h"
+#include "ThePEG/Repository/CurrentGenerator.h"
+#include "ThePEG/EventRecord/Event.h"
+#include "ThePEG/Interface/ClassDocumentation.h"
+#include "ThePEG/Persistency/PersistentOStream.h"
+#include "ThePEG/Persistency/PersistentIStream.h"
+
+using namespace Herwig;
+
+ParallelRunAnalysis::ParallelRunAnalysis()
+  // : log("parallel.status",ofstream::out) 
+  {}
+
+void ParallelRunAnalysis::doinitrun() {
+  // log.open("parallel.status",ofstream::out);
+}
+
+void ParallelRunAnalysis::dofinish() {
+  AnalysisHandler::dofinish();
+}
+
+void ParallelRunAnalysis::analyze(tEventPtr, long currev, int, int) {
+  long totev = generator()->N();
+  long i = currev;
+  bool skip = currev%(max(totev/100, 1L));
+  if ( i > totev/2 ) i = totev-i;
+  while ( skip && i >= 10 && !(i%10) ) i /= 10;
+  if ( i == 1 || i == 2 || i == 5 ) skip = false;
+  if ( skip && currev%5000!=0) return;
+
+  tEHPtr curEvtHandler = generator()->currentEventHandler();
+  long attempts = (dynamic_ptr_cast<StdEHPtr>(curEvtHandler))->sampler()->attempts();
+  char str[128];
+  sprintf(str,"event> %lu/%lu/%lu xs = %.10E pb +/- %.10E pb\n",currev,attempts,totev,curEvtHandler->integratedXSec()/picobarn,curEvtHandler->integratedXSecErr()/picobarn);
+  ofstream log("parallel.log",ofstream::app);
+  // log <<  "event> " << currev << "/" << attempts << "/" << totev << " xs = " << setw(10) << curEvtHandler->integratedXSec()/picobarn << " pb +- " << setw(10) << curEvtHandler->integratedXSecErr()/picobarn << " pb\n" << flush;
+  log << str << flush;
+  log.close();
+}
+
+NoPIOClassDescription<ParallelRunAnalysis> ParallelRunAnalysis::initParallelRunAnalysis;
+// Definition of the static class description member.
+
+void ParallelRunAnalysis::Init() {
+
+  static ClassDocumentation<ParallelRunAnalysis> documentation
+    ("Analysis for combining parallel runs with Herwig.");
+
+}
+
---
+++ Analysis/ParallelRunAnalysis.h
@@ -0,0 +1,162 @@
+// -*- C++ -*-
+//
+// HiggsJetAnalysis.h is a part of Herwig++ - A multi-purpose Monte Carlo event generator
+// Copyright (C) 2002-2014 The Herwig Collaboration
+//
+// Herwig++ is licenced under version 2 of the GPL, see COPYING for details.
+// Please respect the MCnet academic guidelines, see GUIDELINES for details.
+//
+#ifndef HERWIG_ParallelRunAnalysis_H
+#define HERWIG_ParallelRunAnalysis_H
+//
+// This is the declaration of the ParallelRunAnalysis class.
+//
+
+//#include <fstream>
+//#include "ThePEG/Repository/CurrentGenerator.h"
+#include "ThePEG/Handlers/AnalysisHandler.h"
+//#include "Herwig++/Utilities/Histogram.h"
+
+namespace Herwig {
+
+using namespace ThePEG;
+
+/** \ingroup Analysis
+ * HiggsJetAnalysis assumes that there is one Higgs in the final state
+ * and books some observables computed from its four momentum.  It
+ * shouldn't do anything in case there is no Higgs in the event.
+ *
+ * @see \ref HiggsJetAnalysisInterfaces "The interfaces"
+ * defined for HiggsJetAnalysis.
+ */
+class ParallelRunAnalysis: public AnalysisHandler {
+
+public:
+
+  /** @name Standard constructors and destructors. */
+  //@{
+  /**
+   * The default constructor.
+   */
+  ParallelRunAnalysis();
+  //@}
+
+public:
+
+  /** @name Virtual functions required by the AnalysisHandler class. */
+  //@{
+  /**
+   * Analyze a given Event. Note that a fully generated event
+   * may be presented several times, if it has been manipulated in
+   * between. The default version of this function will call transform
+   * to make a lorentz transformation of the whole event, then extract
+   * all final state particles and call analyze(tPVector) of this
+   * analysis object and those of all associated analysis objects. The
+   * default version will not, however, do anything on events which
+   * have not been fully generated, or have been manipulated in any
+   * way.
+   * @param event pointer to the Event to be analyzed.
+   * @param ieve the event number.
+   * @param loop the number of times this event has been presented.
+   * If negative the event is now fully generated.
+   * @param state a number different from zero if the event has been
+   * manipulated in some way since it was last presented.
+   */
+  virtual void analyze(tEventPtr event, long ieve, int loop, int state);
+  //@}
+
+public:
+
+  /**
+   * The standard Init function used to initialize the interfaces.
+   * Called exactly once for each class by the class description system
+   * before the main function starts or
+   * when this class is dynamically loaded.
+   */
+  static void Init();
+
+protected:
+
+  /** @name Clone Methods. */
+  //@{
+  /**
+   * Make a simple clone of this object.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr clone() const {return new_ptr(*this);}
+
+  /** Make a clone of this object, possibly modifying the cloned object
+   * to make it sane.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr fullclone() const {return new_ptr(*this);}
+  //@}
+
+protected:
+
+  /** @name Standard Interfaced functions. */
+  //@{
+  /**
+   * Initialize this object. Called in the run phase just before
+   * a run begins.
+   */
+  virtual void doinitrun();
+
+  /**
+   * Finalize this object. Called in the run phase just after a
+   * run has ended. Used eg. to write out statistics.
+   */
+  virtual void dofinish();
+
+private:
+
+  /**
+   * The static object used to initialize the description of this class.
+   * Indicates that this is a concrete class without persistent data.
+   */
+  static NoPIOClassDescription<ParallelRunAnalysis> initParallelRunAnalysis;
+
+  /**
+   * The assignment operator is private and must never be called.
+   * In fact, it should not even be implemented.
+   */
+  ParallelRunAnalysis & operator=(const ParallelRunAnalysis &);
+
+private:
+  
+};
+
+}
+
+#include "ThePEG/Utilities/ClassTraits.h"
+
+namespace ThePEG {
+
+/** @cond TRAITSPECIALIZATIONS */
+
+/** This template specialization informs ThePEG about the
+ *  base classes of ParallelRunAnalysis. */
+template <>
+struct BaseClassTrait<Herwig::ParallelRunAnalysis,1> {
+  /** Typedef of the first base class of ParallelRunAnalysis. */
+  typedef AnalysisHandler NthBase;
+};
+
+/** This template specialization informs ThePEG about the name of
+ *  the ParallelRunAnalysis class and the shared object where it is defined. */
+template <>
+struct ClassTraits<Herwig::ParallelRunAnalysis>
+  : public ClassTraitsBase<Herwig::ParallelRunAnalysis> {
+  /** Return a platform-independent class name */
+  static string className() { return "Herwig::ParallelRunAnalysis"; }
+  /** Return the name(s) of the shared library (or libraries) be loaded to get
+   *  access to the HiggsJetAnalysis class and any other class on which it depends
+   *  (except the base class). */
+  static string library() { return "HwAnalysis.so"; }
+};
+
+/** @endcond */
+
+}
+
+#endif /* HERWIG_ParallelRunAnalysis_H */
