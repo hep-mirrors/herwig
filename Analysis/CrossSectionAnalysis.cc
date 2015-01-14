a5afeb6c02d648880cd416f9ccb931fe70b9007d
Simon Plätzer <simon.plaetzer@desy.de>
2015-01-14 08:03:48 +0000

    add dummy analysis to write out cross section as run data xml

---
+++ Analysis/CrossSectionAnalysis.cc
@@ -0,0 +1,100 @@
+// -*- C++ -*-
+//
+// This is the implementation of the non-inlined, non-templated member
+// functions of the CrossSectionAnalysis class.
+//
+
+#include "CrossSectionAnalysis.h"
+#include "ThePEG/Interface/ClassDocumentation.h"
+#include "ThePEG/EventRecord/Particle.h"
+#include "ThePEG/Repository/UseRandom.h"
+#include "ThePEG/Repository/EventGenerator.h"
+#include "ThePEG/Utilities/DescribeClass.h"
+
+
+#include "ThePEG/Persistency/PersistentOStream.h"
+#include "ThePEG/Persistency/PersistentIStream.h"
+
+#include "ThePEG/Handlers/EventHandler.h"
+#include "ThePEG/Handlers/StandardEventHandler.h"
+#include "Herwig++/Sampling/GeneralSampler.h"
+
+#include "Herwig++/Utilities/XML/ElementIO.h"
+
+using namespace Herwig;
+
+CrossSectionAnalysis::CrossSectionAnalysis() {}
+
+CrossSectionAnalysis::~CrossSectionAnalysis() {}
+
+#ifndef LWH_AIAnalysisFactory_H
+#ifndef LWH 
+#define LWH ThePEGLWH
+#endif
+#include "ThePEG/Analysis/LWH/AnalysisFactory.h"
+#endif
+
+void CrossSectionAnalysis::dofinish() {
+
+  AnalysisHandler::dofinish();
+
+  Ptr<StandardEventHandler>::tptr seh =
+    dynamic_ptr_cast<Ptr<StandardEventHandler>::tptr>(generator()->eventHandler());
+  Ptr<GeneralSampler>::tptr sampler =
+    dynamic_ptr_cast<Ptr<GeneralSampler>::tptr>(seh->sampler());
+
+  unsigned long attemptedPoints = sampler->attempts();
+  double sumOfWeights = sampler->sumWeights();
+  double sumOfSquaredWeights = sampler->sumWeights2();
+  CrossSection maxXSection = sampler->maxXSec();
+
+  XML::Element elem(XML::ElementTypes::Element,"Run");
+
+  elem.appendAttribute("name",generator()->runName());
+  elem.appendAttribute("attemptedPoints",attemptedPoints);
+  elem.appendAttribute("sumOfWeights",sumOfWeights*maxXSection/nanobarn);
+  elem.appendAttribute("sumOfSquaredWeights",sumOfSquaredWeights*sqr(maxXSection/nanobarn));
+
+  XML::Element xhistos(XML::ElementTypes::Element,"Histograms");
+
+  elem.append(xhistos);
+
+  string fname = name() + ".xml";
+  ofstream runXML(fname.c_str());
+  XML::ElementIO::put(elem,runXML);
+
+}
+
+IBPtr CrossSectionAnalysis::clone() const {
+  return new_ptr(*this);
+}
+
+IBPtr CrossSectionAnalysis::fullclone() const {
+  return new_ptr(*this);
+}
+
+
+// If needed, insert default implementations of virtual function defined
+// in the InterfacedBase class here (using ThePEG-interfaced-impl in Emacs).
+
+
+void CrossSectionAnalysis::persistentOutput(PersistentOStream &) const {}
+
+void CrossSectionAnalysis::persistentInput(PersistentIStream &, int) {}
+
+
+// *** Attention *** The following static variable is needed for the type
+// description system in ThePEG. Please check that the template arguments
+// are correct (the class and its base class), and that the constructor
+// arguments are correct (the class name and the name of the dynamically
+// loadable library where the class implementation can be found).
+DescribeClass<CrossSectionAnalysis,AnalysisHandler>
+  describeHerwigCrossSectionAnalysis("Herwig::CrossSectionAnalysis", "HwJetsAnalysis.so");
+
+void CrossSectionAnalysis::Init() {
+
+  static ClassDocumentation<CrossSectionAnalysis> documentation
+    ("There is no documentation for the CrossSectionAnalysis class");
+
+}
+
---
+++ Analysis/CrossSectionAnalysis.h
@@ -0,0 +1,105 @@
+// -*- C++ -*-
+#ifndef Herwig_CrossSectionAnalysis_H
+#define Herwig_CrossSectionAnalysis_H
+//
+// This is the declaration of the CrossSectionAnalysis class.
+//
+
+#include "ThePEG/Handlers/AnalysisHandler.h"
+
+namespace Herwig {
+
+using namespace ThePEG;
+
+/**
+ * Here is the documentation of the CrossSectionAnalysis class.
+ *
+ * @see \ref CrossSectionAnalysisInterfaces "The interfaces"
+ * defined for CrossSectionAnalysis.
+ */
+class CrossSectionAnalysis: public AnalysisHandler {
+
+public:
+
+  /** @name Standard constructors and destructors. */
+  //@{
+  /**
+   * The default constructor.
+   */
+  CrossSectionAnalysis();
+
+  /**
+   * The destructor.
+   */
+  virtual ~CrossSectionAnalysis();
+  //@}
+
+protected:
+
+  /**
+   * Finalize this object. Called in the run phase just after a
+   * run has ended. Used eg. to write out statistics.
+   */
+  virtual void dofinish();
+
+public:
+
+  /** @name Functions used by the persistent I/O system. */
+  //@{
+  /**
+   * Function used to write out object persistently.
+   * @param os the persistent output stream written to.
+   */
+  void persistentOutput(PersistentOStream & os) const;
+
+  /**
+   * Function used to read in object persistently.
+   * @param is the persistent input stream read from.
+   * @param version the version number of the object when written.
+   */
+  void persistentInput(PersistentIStream & is, int version);
+  //@}
+
+  /**
+   * The standard Init function used to initialize the interfaces.
+   * Called exactly once for each class by the class description system
+   * before the main function starts or
+   * when this class is dynamically loaded.
+   */
+  static void Init();
+
+protected:
+
+  /** @name Clone Methods. */
+  //@{
+  /**
+   * Make a simple clone of this object.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr clone() const;
+
+  /** Make a clone of this object, possibly modifying the cloned object
+   * to make it sane.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr fullclone() const;
+  //@}
+
+
+// If needed, insert declarations of virtual function defined in the
+// InterfacedBase class here (using ThePEG-interfaced-decl in Emacs).
+
+
+private:
+
+  /**
+   * The assignment operator is private and must never be called.
+   * In fact, it should not even be implemented.
+   */
+  CrossSectionAnalysis & operator=(const CrossSectionAnalysis &);
+
+};
+
+}
+
+#endif /* Herwig_CrossSectionAnalysis_H */
