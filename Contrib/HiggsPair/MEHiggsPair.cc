35cf21b291d8b31e0f88c5adcd9e40ada416eebe
Andreas Papaefstathiou <andreasp@physik.uzh.ch>
2015-02-26 10:38:25 +0100

    Added D=6 EFT implementation

--- Contrib/HiggsPair/MEHiggsPair.cc
+++ Contrib/HiggsPair/MEHiggsPair.cc
@@ -37,7 +37,7 @@ using namespace Herwig;
 
 
 MEHiggsPair::MEHiggsPair()
-  : _selfcoupling(1.0),_hhHcoupling(1.0), _process(0), _mh(), _wh() {
+  : _selfcoupling(1.0),_hhHcoupling(1.0), _process(0), _mh(), _wh(), _cH(0.0), _c6(0.0), _ct1(0.0),  _cb1(0.0), _ct2(0.0),  _cb2(0.0), _cg1(0.0), _cg2(0.0), _EFTScale(1000*GeV),  _fixedalphaS(0), _alphasfixedvalue(0.1), _alphascale(90.0*GeV), _basescale(125.0*GeV), _fixedscale(0), _scalemultiplier(1.0)   {
   massOption(vector<unsigned int>(2,0));
 }
 
@@ -48,9 +48,12 @@ int MEHiggsPair::nDim() const {
 
 void MEHiggsPair::doinit() {
 
-  if(_process == 4 || _process == 5) { cerr << "HH and hH production not implemented yet, please choose an hh production subprocess" << endl; exit(1); } 
+  if(_process == 5 || _process == 6) { cerr << "HH and hH production not implemented yet, please choose an hh production subprocess" << endl; exit(1); } 
+  if(_process == 4) { cerr << "WARNING: In the EFT, the option SelfCoupling is not used." << endl; }
 
   higgs(getParticleData(ParticleID::h0));
+  PDPtr wboson = getParticleData(ParticleID::Wplus);
+  _Wmass = wboson->mass();
   PDPtr top = getParticleData(ParticleID::t);
   _topmass = top->mass();
   PDPtr zboson = getParticleData(ParticleID::Z0);
@@ -60,8 +63,6 @@ void MEHiggsPair::doinit() {
   PDPtr heavyH = getParticleData(35);
   _heavyHmass = heavyH->mass();
   _heavyHwidth = heavyH->width();
-
-
 
   // higgs stuff
   _mh = _higgs->mass();
@@ -92,16 +93,17 @@ IBPtr MEHiggsPair::fullclone() const {
   }
 
 void MEHiggsPair::persistentOutput(PersistentOStream & os) const {
-  os << _selfcoupling << _hhHcoupling << _process << _higgs << ounit(_topmass,GeV) << ounit(_bottommass,GeV) << ounit(_zmass,GeV) << ounit(_m1,GeV) << ounit(_m2,GeV) << ounit(_mh,GeV) << ounit(_wh,GeV) << _hmass;
+  os << _selfcoupling << _hhHcoupling << _process << _higgs << ounit(_topmass,GeV) << ounit(_bottommass,GeV) << ounit(_zmass,GeV) << ounit(_m1,GeV) << ounit(_m2,GeV) << ounit(_mh,GeV) << ounit(_wh,GeV) << _hmass << _cH << _c6 << _ct1 << _cb1 << _ct2 << _cb2 << _cg1 << _cg2 << ounit(_EFTScale,GeV) << _fixedalphaS << _alphasfixedvalue << ounit(_heavyHmass,GeV) << ounit(_heavyHwidth,GeV) << ounit(_basescale,GeV) << ounit(_alphascale,GeV) << _fixedscale << _scalemultiplier;
 }
 
 void MEHiggsPair::persistentInput(PersistentIStream & is, int) {
-  is >> _selfcoupling >> _hhHcoupling >> _process >> _higgs >> iunit(_topmass,GeV) >> iunit(_bottommass,GeV) >> iunit(_zmass,GeV) >> iunit(_m1,GeV) >> iunit(_m2,GeV) >> iunit(_mh,GeV) >> iunit(_wh,GeV) >> _hmass;
+  is >> _selfcoupling >> _hhHcoupling >> _process >> _higgs >> iunit(_topmass,GeV) >> iunit(_bottommass,GeV) >> iunit(_zmass,GeV) >> iunit(_m1,GeV) >> iunit(_m2,GeV) >> iunit(_mh,GeV) >> iunit(_wh,GeV) >> _hmass >> _cH >> _c6 >> _ct1 >> _cb1 >> _ct2 >> _cb2 >> _cg1 >> _cg2 >> iunit(_EFTScale,GeV) >>  _fixedalphaS >> _alphasfixedvalue >> iunit(_heavyHmass,GeV) >> iunit(_heavyHwidth,GeV) >> iunit(_basescale,GeV) >> iunit(_alphascale,GeV) >> _fixedscale >> _scalemultiplier;
 
 }
 
 Energy2 MEHiggsPair::scale() const {
-   return sHat();
+  if(_fixedscale==0 || _fixedscale==5 ) { return sqr(_scalemultiplier)*sHat(); } 
+  else if (_fixedscale==1 || _fixedscale==2 || _fixedscale==3) { return (sqr(_scalemultiplier)*sqr(_basescale)); }
 }
 
 // Definition of the static class description member.
@@ -110,16 +112,142 @@ ClassDescription<MEHiggsPair> MEHiggsPair::initMEHiggsPair;
 void MEHiggsPair::Init() {
 
   static ClassDocumentation<MEHiggsPair> documentation
-    ("The MEHiggsPair class implements the transplanckian 2->2 processes in hadron-hadron"
+    ("The MEHiggsPair class implements the gg -> hh processes in hadron-hadron"
      " collisions");
 
  static Parameter<MEHiggsPair, double> interfaceSelfCoupling
     ("SelfCoupling",
      "Multiplier for the SM Higgs triple coupling",
-     &MEHiggsPair::_selfcoupling, 1.0, -10.0, 10.0,
+     &MEHiggsPair::_selfcoupling, 1.0, -1000000.0, 1000000.0,
      false, false, Interface::limited);
 
- static Parameter<MEHiggsPair, double> interfacehHHCoupling
+ static Parameter<MEHiggsPair, double> interfacecH
+    ("cH",
+     "Effective theory coefficient cH",
+     &MEHiggsPair::_cH, 0.0, -1000000.0, 1000000.0,
+     false, false, Interface::limited);
+
+ static Parameter<MEHiggsPair, double> interfacec6
+    ("c6",
+     "Effective theory coefficient c6",
+     &MEHiggsPair::_c6, 0.0, -1000000.0, 1000000.0,
+     false, false, Interface::limited);
+
+ static Parameter<MEHiggsPair, double> interfacect1
+    ("ct1",
+     "Effective theory coefficient ct1",
+     &MEHiggsPair::_ct1, 0.0, -1000000.0, 1000000.0,
+     false, false, Interface::limited);
+
+ static Parameter<MEHiggsPair, double> interfacecb1
+    ("cb1",
+     "Effective theory coefficient fb1",
+     &MEHiggsPair::_cb1, 0.0, -1000000.0, 1000000.0,
+     false, false, Interface::limited);
+
+ static Parameter<MEHiggsPair, double> interfacect2
+    ("ct2",
+     "Effective theory coefficient ft2",
+     &MEHiggsPair::_ct2, 0.0, -1000000.0, 1000000.0,
+     false, false, Interface::limited);
+
+ static Parameter<MEHiggsPair, double> interfacecb2
+    ("cb2",
+     "Effective theory coefficient cb2",
+     &MEHiggsPair::_cb2, 0.0, -1000000.0, 1000000.0,
+     false, false, Interface::limited);
+
+ static Parameter<MEHiggsPair, double> interfacecg1
+    ("cg1",
+     "Effective theory coefficient cg1",
+     &MEHiggsPair::_cg1, 0.0, -1000000.0, 1000000.0,
+     false, false, Interface::limited);
+
+static Parameter<MEHiggsPair, double> interfacecg2
+    ("cg2",
+     "Effective theory coefficient cg2",
+     &MEHiggsPair::_cg2, 0.0, -1000000.0, 1000000.0,
+     false, false, Interface::limited);
+
+
+ static Parameter<MEHiggsPair, Energy> interfaceEFTScale
+    ("EFTScale",
+     "Effective theory coefficient f1",
+     &MEHiggsPair::_EFTScale, GeV, 1000.0*GeV, 1.0*GeV, 10000000*GeV,
+     false, false, Interface::limited);
+
+
+  static Switch<MEHiggsPair,unsigned int> interfaceFixedAlphaS
+    ("FixedAlphaS",
+     "Which implementation to use",
+     &MEHiggsPair::_fixedalphaS, 0, false, false);
+  static SwitchOption interfaceFixedAlphaSNo
+    (interfaceFixedAlphaS,
+     "No",
+     "Don't fix the scale of alphaS.",
+     0);
+  static SwitchOption interfaceFixedAlphaSScale
+    (interfaceFixedAlphaS,
+     "Scale",
+     "Fix the scale of alphaS.",
+     1);
+  static SwitchOption interfaceFixedAlphaSAlphaS
+    (interfaceFixedAlphaS,
+     "AlphaS",
+     "Fix alphaS directly via the AlphaS interface.",
+     2);
+
+
+  static Parameter<MEHiggsPair, Energy> interfaceBaseScale
+    ("BaseScale",
+     "Base scale if fixed",
+     &MEHiggsPair::_basescale, GeV, 125.0*GeV, 0.0*GeV, 10000000.0*GeV,
+     false, false, Interface::limited);
+
+ 
+  static Switch<MEHiggsPair,unsigned int> interfaceFixedScale
+    ("FixedScale",
+     "Whether to use fixed base scale or not",
+     &MEHiggsPair::_fixedscale, 0, false, false);
+  static SwitchOption interfaceFixedScaleNo
+    (interfaceFixedScale,
+     "sHat",
+     "Don't fix the base scale, use sHat().",
+     0);
+  static SwitchOption interfaceFixedScaleFixed
+    (interfaceFixedScale,
+     "Fixed",
+     "Fix the scale of the process to chosen (base scale)^2.",
+     2);
+  static SwitchOption interfaceFixedScaleBaseQuad
+    (interfaceFixedScale,
+     "FixedQuadPt",
+     "Fix the scale of the process to (base scale)^2 + pt^2.",
+     1);
+  static SwitchOption interfaceFixedScaleBaseLin
+    (interfaceFixedScale,
+     "FixedLinPt",
+     "Fix the scale of the process to (base scale + pt)^2.",
+     3);
+  static SwitchOption interfaceFixedScaleMHH
+    (interfaceFixedScale,
+     "MHH",
+     "Don't fix the base scale, use MHH.",
+     5);
+
+  static Parameter<MEHiggsPair, double> interfaceScaleMultiplier
+    ("ScaleMultiplier",
+     "Multiplier scale",
+     &MEHiggsPair::_scalemultiplier, 1.0, 0.0, 100000.0,
+     false, false, Interface::limited);
+
+  static Parameter<MEHiggsPair, Energy> interfaceAlphaSScale
+    ("AlphaSScale",
+     "Scale for alphaS if fixed",
+     &MEHiggsPair::_alphascale, GeV, 100.0*GeV, 0.0*GeV, 10000000.0*GeV,
+     false, false, Interface::limited);
+
+  static Parameter<MEHiggsPair, double> interfacehHHCoupling
     ("hhHCoupling",
      "Multiplier for the hh-H triple coupling",
      &MEHiggsPair::_hhHcoupling, 1.0, -10.0, 10.0,
@@ -150,16 +278,31 @@ void MEHiggsPair::Init() {
      "ggToHTohh",
      "Include all gg->hh subprocess, with heavy Higgs",
      3);
+  static SwitchOption interfaceProcessEFT
+    (interfaceProcess,
+     "EFT",
+     "Include all SM gg->hh subprocesses in the EFT",
+     4);
   static SwitchOption interfaceProcessHH
     (interfaceProcess,
      "ggToHH",
      "Include all gg->HH subprocesses",
-     4);
+     5);
   static SwitchOption interfaceProcessHh
     (interfaceProcess,
      "ggToHh",
      "Include only gg->hH subprocesses",
-     5);
+     6);
+
+ 
+
+ static Parameter<MEHiggsPair, double> interfaceAlphaS
+    ("AlphaS",
+     "The value of AlphaS if FixedAlphaS is AlphaS, option 1",
+     &MEHiggsPair::_alphasfixedvalue, 0.1, 0., 100000.0,
+     false, false, Interface::limited);
+
+ 
 }
 
 CrossSection MEHiggsPair::dSigHatDR() const {
@@ -186,11 +329,11 @@ void MEHiggsPair::getDiagrams() const {
   PDPtr boxon = getParticleData(99926);
   PDPtr triangon = getParticleData(99927);
   //cout<<  _higgs->mass() << endl;
-  if(_process==0||_process==1||_process==3) {  
+  if(_process==0||_process==1||_process==3||_process==4) {  
     add(new_ptr((Tree2toNDiagram(2),gluon,gluon,
 		 1,triangon,3,higgs(),higgs(),-1)));    
   }
-  if(_process==0||_process==2||_process==3) {
+  if(_process==0||_process==2||_process==3||_process==4) {
     add(new_ptr((Tree2toNDiagram(2),gluon,gluon,
 		 1,boxon,3,higgs(),higgs(),-2)));    
   }
@@ -232,13 +375,13 @@ double MEHiggsPair::me2() const {
   mesq = MATRIX(s, t, u, Mc, Md);
   //QCD factors
   double ALPS=SM().alphaS(scale());
+  if(_fixedalphaS==1) { ALPS = _alphasfixedvalue; }
   double QCD=sqr(ALPS);
   //XLAM = beta*S
   double XLAM=sqrt(sqr(s+sqr(Mc)-sqr(Md))-4.*s*sqr(Mc));
   //the jacobian is jacobian()*XLAM
   mesq *= jacobian()*XLAM; 
   mesq *= QCD / (2048.*sqr(pi));
-
   return mesq;    
 }
 
@@ -372,15 +515,19 @@ vector<Complex> MEHiggsPair::iniscal(double AMQ, double S, double T,double U, do
 //both top and bottom loops are considered.
 double MEHiggsPair::MATRIX(double S, double T,double U, double M1, double M2) const {
   //C--LO MATRIX ELEMENT FOR GG -> HH
- 
+  using Constants::pi;
+
   Complex A1,A2,H1,H2,Z1,Z2,AA1,AA2,HH1,HH2,AH1,AH2;
   Complex F1,F2,PROH, PROHEAVY;
   
   //bottom and top masses
   double AMT = _topmass/GeV;
   double AMB = _bottommass/GeV;
+  
+  //W mass
+  double MW = _Wmass/GeV;
 
-  //higs width and mass
+  //higgs width and mass
   double FACH=1; 
   double GAMH= _wh/GeV; 
   double AMH = _mh/GeV;
@@ -391,16 +538,31 @@ double MEHiggsPair::MATRIX(double S, double T,double U, double M1, double M2) co
   double GHT=AMT/V;
   double GHB=AMB/V;
 
+  double ALPS=SM().alphaS(scale());
+  if(_fixedalphaS==1) { ALPS = _alphasfixedvalue; }
+  double eftscale = _EFTScale/GeV;
+
   //Higgs propagator
   PROH = Complex(S-sqr(AMH),AMH*GAMH*FACH);
 
+  //EFT-modified self-coupling
+  double GHHH_EFT = 0;
+  if(_process == 4) { 
+    GHHH_EFT = (3.*sqr(AMH) / V) * ( - (3./2.) * _cH + _c6 ) ;  
+  }
+  
+  //EFT-modified top/bottom Yukawas
+  double GHT_EFT = 0, GHB_EFT = 0;
+  if(_process == 4) { 
+    GHT_EFT = AMT/V * ( _ct1 - 0.5 * _cH );
+    GHB_EFT = AMB/V * ( _cb1 - 0.5 * _cH );
+  }
   //Heavy Higgs
   double AMHEAVY = _heavyHmass/GeV;
   double GAMHEAVY = _heavyHwidth/GeV;			      
   double FACHEAVY = 1.;
   PROHEAVY = Complex(S-sqr(AMHEAVY),AMHEAVY*GAMHEAVY*FACHEAVY);
   double GHhh = _hhHcoupling * 3.*sqr(AMHEAVY) / V;  // Hhh coupling (for _process == 3)
-
   
   //calculation of scalar integrals C_ij and D_ijk
   vector<Complex> ScalFacs;
@@ -423,18 +585,59 @@ double MEHiggsPair::MATRIX(double S, double T,double U, double M1, double M2) co
   Complex D0ACB[1] = {ScalFacs[8]};
     //form factors for top quark contributions
   formfac_(amq, ss, tt, uu, m1, m2, C0AB, C0AC, C0AD, C0BC, C0BD, C0CD, D0ABC, D0BAC, D0ACB);
+
+  // this factor needs to be divided out to go from SM EFT to DIM-6 EFT
+  double CH = ALPS / (8. * pi);
   
   F1 = Complex(0.,0.);
   F2 = Complex(0.,0.);
   //triangle
-  if(_process==0||_process==1||_process==3) { 
-    F1 = F1 + (form_.H1*(GHT*GHHH/PROH));
+  if(_process==0||_process==1||_process==3||_process==4) { 
+    F1 = F1 + (form_.H1*(GHT*GHHH/PROH)); //(1)
   }
+  //triangle: EFT
+  if(_process==4) { 
+    F1 = F1 + (form_.H1*(GHT*GHHH_EFT+GHT_EFT*GHHH)/PROH);
+  }
+
+
   //box
-  if(_process==0||_process==2||_process==3) { 
-    F1 = F1 + (form_.HH1*GHT*GHT);
+  if(_process==0||_process==2||_process==3||_process==4) { 
+    F1 = F1 + (form_.HH1*GHT*GHT); //(2)
     F2 = F2 + (form_.HH2*GHT*GHT);
   }
+  //box: EFT
+  if(_process==4) { 
+    F1 = F1 + 2. * (form_.HH1*GHT_EFT*GHT);
+    F2 = F2 + 2. * (form_.HH2*GHT_EFT*GHT);
+  }
+
+  //additional EFT from gg -> h -> hh diagram:
+  if(_process==4) { 
+    F1 = F1 + _cg1 * (2.*S/V)*2*(GHHH/PROH); 
+    /* divide (1) by form_.H1*AMT/(2*S) to remove F_triangle
+     * multiply by 2
+     * and multiply by cg1
+     */
+  }
+  //additional EFT from gg -> hh diagram:
+  if(_process==4) { 
+    F1 = F1 + _cg2 * (2.*S/sqr(V))*2.;
+     /* divide (2) by form_.HH1*sqr(AMT)/(2*S) to remove F_box
+      * multiply by 2 
+      * and multiply by cg2
+      */
+  }
+  //EFT: new t-tbar-h-h diagram
+  if(_process==4) { 
+    double GttHH = (3.*_ct2 - _cH) *AMT/(2.*sqr(V)); // we have already replaced yf/sqrt(2) = mf/V
+    F1 = F1 + (form_.H1*2.*GttHH); //factor of 2 from tthh feynman rule
+  }
+  //TESTING BELOW
+
+  /* cout << "form_.H1 = " << form_.H1 << " form_.HH1 = " << form_.HH1 << endl; 
+  cout << "Ftri = " << real(form_.H1)*AMT/(2*S) << endl; 
+  cout << "Fbox = " << real(form_.HH1)*sqr(AMT)/(2*S) << endl; */
 
   //form factors for bottom quark contributions
   ScalFacs = iniscal(AMB, S, T, U, M1, M2);
@@ -450,15 +653,30 @@ double MEHiggsPair::MATRIX(double S, double T,double U, double M1, double M2) co
   D0ACB[0] = ScalFacs[8];
   formfac_(amq, ss, tt, uu, m1, m2, C0AB, C0AC, C0AD, C0BC, C0BD, C0CD, D0ABC, D0BAC, D0ACB);
   //triangle 
-  if(_process==0||_process==1||_process==3) { 
+  if(_process==0||_process==1||_process==3||_process==4) { 
     F1 = F1 + (form_.H1*(GHB*GHHH/PROH));
   }
+  //triangle: EFT
+  if(_process==4) { 
+    F1 = F1 + (form_.H1*(GHB*GHHH_EFT+GHB_EFT*GHHH)/PROH);
+  }
+
   //box
-  if(_process==0||_process==2||_process==3) { 
+  if(_process==0||_process==2||_process==3||_process==4) { 
     F1 = F1 + (form_.HH1*GHB*GHB);
     F2 = F2 + (form_.HH2*GHB*GHB);
   }
-  
+  //box: EFT
+  if(_process==4) { 
+    F1 = F1 + 2. * (form_.HH1*GHB_EFT*GHB);
+    F2 = F2 + 2. * (form_.HH2*GHB_EFT*GHB);
+  }
+
+  //EFT: new b-bbar-h-h diagram
+  if(_process==4) { 
+    double GbbHH = (3.*_cb2 - _cH) *AMB/(2.*sqr(V));// we have already replaced yf/sqrt(2) = mf/V
+    F1 = F1 + (form_.H1*2.*GbbHH);
+  }
   
   if(_process==3) { 
     m1[0] = AMH;
