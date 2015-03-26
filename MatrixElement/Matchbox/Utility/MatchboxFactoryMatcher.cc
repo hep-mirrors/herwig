756651e5ff66e5fe314834d2332cbf09caad09e3
Simon Plätzer <simon.plaetzer@desy.de>
2015-03-26 15:30:29 +0100

    add matcher to pick up factory particle groups

---
+++ MatrixElement/Matchbox/Utility/MatchboxFactoryMatcher.cc
@@ -0,0 +1,102 @@
+// -*- C++ -*-
+//
+// MatchboxFactoryMatcher.cc is a part of Herwig++ - A multi-purpose Monte Carlo event generator
+// Copyright (C) 2002-2012 The Herwig Collaboration
+//
+// Herwig++ is licenced under version 2 of the GPL, see COPYING for details.
+// Please respect the MCnet academic guidelines, see GUIDELINES for details.
+//
+//
+// This is the implementation of the non-inlined, non-templated member
+// functions of the MatchboxFactoryMatcher class.
+//
+
+#include "MatchboxFactoryMatcher.h"
+#include "ThePEG/Interface/ClassDocumentation.h"
+#include "ThePEG/Interface/Reference.h"
+#include "ThePEG/Interface/Parameter.h"
+#include "ThePEG/EventRecord/Particle.h"
+#include "ThePEG/Repository/UseRandom.h"
+#include "ThePEG/Repository/EventGenerator.h"
+#include "ThePEG/Utilities/DescribeClass.h"
+
+
+#include "ThePEG/Persistency/PersistentOStream.h"
+#include "ThePEG/Persistency/PersistentIStream.h"
+
+using namespace Herwig;
+
+MatchboxFactoryMatcher::MatchboxFactoryMatcher()
+  : theGroup("") {}
+
+MatchboxFactoryMatcher::~MatchboxFactoryMatcher() {}
+
+IBPtr MatchboxFactoryMatcher::clone() const {
+  return new_ptr(*this);
+}
+
+IBPtr MatchboxFactoryMatcher::fullclone() const {
+  return new_ptr(*this);
+}
+
+PMPtr MatchboxFactoryMatcher::pmclone() const {
+  return new_ptr(*this);
+}
+
+bool MatchboxFactoryMatcher::check(const ParticleData & data) const {
+  return theIds.find(data.id()) != theIds.end();
+}
+
+// If needed, insert default implementations of virtual function defined
+// in the InterfacedBase class here (using ThePEG-interfaced-impl in Emacs).
+
+
+void MatchboxFactoryMatcher::persistentOutput(PersistentOStream & os) const {
+  os << theFactory << theGroup << theIds;
+}
+
+void MatchboxFactoryMatcher::persistentInput(PersistentIStream & is, int) {
+  is >> theFactory >> theGroup >> theIds;
+}
+
+void MatchboxFactoryMatcher::doinit() {
+  MatcherBase::doinit();
+  assert(theFactory);
+  map<string,PDVector>::const_iterator grp
+    = theFactory->particleGroups().find(theGroup);
+  if ( grp == theFactory->particleGroups().end() )
+    throw InitException()
+      << "Particle group '" << theGroup << "' not defined in factory object '"
+      << theFactory->name() << "'" << Exception::abortnow;
+  theIds.clear();
+  for ( PDVector::const_iterator p = grp->second.begin();
+	p != grp->second.end(); ++p )
+    theIds.insert((**p).id());
+}
+
+// *** Attention *** The following static variable is needed for the type
+// description system in ThePEG. Please check that the template arguments
+// are correct (the class and its base class), and that the constructor
+// arguments are correct (the class name and the name of the dynamically
+// loadable library where the class implementation can be found).
+DescribeClass<MatchboxFactoryMatcher,ThePEG::MatcherBase>
+  describeHerwigMatchboxFactoryMatcher("Herwig::MatchboxFactoryMatcher", "Herwig.so");
+
+void MatchboxFactoryMatcher::Init() {
+
+  static ClassDocumentation<MatchboxFactoryMatcher> documentation
+    ("MatchboxFactoryMatcher matches particles according to MatchboxFatory particle groups");
+
+  static Reference<MatchboxFactoryMatcher,MatchboxFactory> interfaceFactory
+    ("Factory",
+     "Set the factory to query for particle groups.",
+     &MatchboxFactoryMatcher::theFactory, false, false, true, false, false);
+
+  static Parameter<MatchboxFactoryMatcher,string> interfaceGroup
+    ("Group",
+     "Set the group name to match.",
+     &MatchboxFactoryMatcher::theGroup, "",
+     false, false);
+
+}
+
---
+++ MatrixElement/Matchbox/Utility/MatchboxFactoryMatcher.h
@@ -0,0 +1,152 @@
+// -*- C++ -*-
+//
+// MatchboxFactoryMatcher.h is a part of Herwig++ - A multi-purpose Monte Carlo event generator
+// Copyright (C) 2002-2012 The Herwig Collaboration
+//
+// Herwig++ is licenced under version 2 of the GPL, see COPYING for details.
+// Please respect the MCnet academic guidelines, see GUIDELINES for details.
+//
+#ifndef Herwig_MatchboxFactoryMatcher_H
+#define Herwig_MatchboxFactoryMatcher_H
+//
+// This is the declaration of the MatchboxFactoryMatcher class.
+//
+
+#include "ThePEG/PDT/MatcherBase.h"
+#include "Herwig++/MatrixElement/Matchbox/MatchboxFactory.h"
+
+namespace Herwig {
+
+using namespace ThePEG;
+
+/**
+ * \ingroup Matchbox
+ * \author Simon Platzer
+ *
+ * \brief MatchboxFactoryMatcher matches particles according to
+ * MatchboxFatory particle groups
+ *
+ * @see \ref MatchboxFactoryMatcherInterfaces "The interfaces"
+ * defined for MatchboxFactoryMatcher.
+ */
+class MatchboxFactoryMatcher: public ThePEG::MatcherBase {
+
+public:
+
+  /** @name Standard constructors and destructors. */
+  //@{
+  /**
+   * The default constructor.
+   */
+  MatchboxFactoryMatcher();
+
+  /**
+   * The destructor.
+   */
+  virtual ~MatchboxFactoryMatcher();
+  //@}
+
+public:
+
+  /**
+   * Check if a particle type meets the criteria.
+   */
+  virtual bool check(const ParticleData &) const;
+
+  /**
+   * Specialized clone method for MatcherBase used by the
+   * Repository. A sub class must make sure that also the MatcherBase
+   * object corresponding to the complex conjugate of this is cloned.
+   */
+  virtual PMPtr pmclone() const;
+
+public:
+
+  /** @name Functions used by the persistent I/O system. */
+  //@{
+  /**
+   * Function used to write out object persistently.
+   * @param os the persistent output stream written to.
+   */
+  void persistentOutput(PersistentOStream & os) const;
+
+  /**
+   * Function used to read in object persistently.
+   * @param is the persistent input stream read from.
+   * @param version the version number of the object when written.
+   */
+  void persistentInput(PersistentIStream & is, int version);
+  //@}
+
+  /**
+   * The standard Init function used to initialize the interfaces.
+   * Called exactly once for each class by the class description system
+   * before the main function starts or
+   * when this class is dynamically loaded.
+   */
+  static void Init();
+
+protected:
+
+  /** @name Clone Methods. */
+  //@{
+  /**
+   * Make a simple clone of this object.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr clone() const;
+
+  /** Make a clone of this object, possibly modifying the cloned object
+   * to make it sane.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr fullclone() const;
+  //@}
+
+
+// If needed, insert declarations of virtual function defined in the
+// InterfacedBase class here (using ThePEG-interfaced-decl in Emacs).
+
+
+protected:
+
+  /** @name Standard Interfaced functions. */
+  //@{
+  /**
+   * Initialize this object after the setup phase before saving an
+   * EventGenerator to disk.
+   * @throws InitException if object could not be initialized properly.
+   */
+  virtual void doinit();
+  //@}
+
+private:
+
+  /**
+   * A pointer to the factory to be used
+   */
+  Ptr<MatchboxFactory>::ptr theFactory;
+
+  /**
+   * The particle group to be matched
+   */
+  string theGroup;
+
+  /**
+   * The set of particle ids to be matched
+   */
+  set<long> theIds;
+
+private:
+
+  /**
+   * The assignment operator is private and must never be called.
+   * In fact, it should not even be implemented.
+   */
+  MatchboxFactoryMatcher & operator=(const MatchboxFactoryMatcher &);
+
+};
+
+}
+
+#endif /* Herwig_MatchboxFactoryMatcher_H */
