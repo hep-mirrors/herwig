36ba2a837b849204fe345abaa40e5045f755f424
Johannes Bellm <johannes.bellm@kit.edu>
2015-07-08 11:29:48 +0200

    argparse --> optparse

--- MatrixElement/Matchbox/External/GoSam/GoSamHelper.py
+++ MatrixElement/Matchbox/External/GoSam/GoSamHelper.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 
-import os,sys,glob,errno,shutil,argparse,time
+import os,sys,glob,errno,shutil,time     
+from optparse import OptionParser   # should be argparse replace options by args
+
  
 
 #  helper to replace all sourceText in fileName with replaceText
@@ -28,36 +30,50 @@ def find(name, path):
             return os.path.join(root, name)      
 
   
-parser = argparse.ArgumentParser()
-parser.add_argument('--usrinfile', help='usrinfile')
-parser.add_argument('--infile', help='infile')
-parser.add_argument('--definfile', help='definfile')
-parser.add_argument('--formtempdir', help='formtempdir')
-parser.add_argument('--reduction', help='reduction')
-parser.add_argument('--formopt', help='formopt')
-parser.add_argument('--higgseff', help='higgseff')
-parser.add_argument('--makelink', help='makelink', action="store_true")
-parser.add_argument('--makelinkfrom', help='makelinkfrom')
-parser.add_argument('--makelinkto', help='makelinkto')
+#parser = argparse.ArgumentParser()
+#parser.add_argument('--usrinfile', help='usrinfile')
+#parser.add_argument('--infile', help='infile')
+#parser.add_argument('--definfile', help='definfile')
+#parser.add_argument('--formtempdir', help='formtempdir')
+#parser.add_argument('--reduction', help='reduction')
+#parser.add_argument('--formopt', help='formopt')
+#parser.add_argument('--higgseff', help='higgseff')
+#parser.add_argument('--makelink', help='makelink', action="store_true")
+#parser.add_argument('--makelinkfrom', help='makelinkfrom')
+#parser.add_argument('--makelinkto', help='makelinkto')
+#args = parser.parse_args()
 
-args = parser.parse_args()
+  
+parser = OptionParser()
+parser.add_option("-a",'--usrinfile'   ,dest='usrinfile'    , help='usrinfile')
+parser.add_option("-b",'--infile'      ,dest='infile'       , help='infile')
+parser.add_option("-c",'--definfile'   ,dest='definfile'    , help='definfile')
+parser.add_option("-d",'--formtempdir' ,dest='formtempdir'  , help='formtempdir')
+parser.add_option("-e",'--reduction'   ,dest='reduction'    , help='reduction')
+parser.add_option("-f",'--formopt'     ,dest='formopt'      , help='formopt')
+parser.add_option("-g",'--higgseff'    ,dest='higgseff'     , help='higgseff')
+parser.add_option("-i",'--makelink'    ,dest='makelink'     , default=False, help='makelink', action="store_true")
+parser.add_option("-j",'--makelinkfrom',dest='makelinkfrom' , help='makelinkfrom')
+parser.add_option("-k",'--makelinkto'  ,dest='makelinkto'   , help='makelinkto')
+(options, args) = parser.parse_args()
 
 
-if args.makelink and not os.path.islink(args.makelinkto):
-   os.symlink(args.makelinkfrom,args.makelinkto)
+
+if options.makelink and not os.path.islink(options.makelinkto):
+   os.symlink(options.makelinkfrom,options.makelinkto)
    exit()
   
 
 
 
-if not os.path.isfile(args.usrinfile):
-  shutil.copyfile(args.definfile, args.infile)
+if not os.path.isfile(options.usrinfile):
+  shutil.copyfile(options.definfile, options.infile)
 
-if os.path.isfile(args.usrinfile):
-  shutil.copyfile(args.usrinfile, args.infile)
+if os.path.isfile(options.usrinfile):
+  shutil.copyfile(options.usrinfile, options.infile)
 
-replacetext(args.infile,"@MODEL@", "model="+args.higgseff)
-replacetext(args.infile,"@REDUCTIONPROGRAMS@", args.reduction)
-replacetext(args.infile, "@FORMOPT@", args.formopt)
-replacetext(args.infile, "@FORMTEMPDIR@", args.formtempdir)
+replacetext(options.infile,"@MODEL@", "model="+options.higgseff)
+replacetext(options.infile,"@REDUCTIONPROGRAMS@", options.reduction)
+replacetext(options.infile, "@FORMOPT@", options.formopt)
+replacetext(options.infile, "@FORMTEMPDIR@", options.formtempdir)
 
