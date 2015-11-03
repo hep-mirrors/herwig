b8b2ad9c6969173ffec0b82d902b44f8b10dcc6e
Peter Richardson <Peter.Richardson@durham.ac.uk>
2015-11-03 10:16:56 +0100

    numerics improvement
    --HG--
    branch : herwig-7-0

--- Decay/Perturbative/SMWFermionsPOWHEGDecayer.cc
+++ Decay/Perturbative/SMWFermionsPOWHEGDecayer.cc
@@ -601,26 +601,23 @@ bool SMWFermionsPOWHEGDecayer::checkZMomenta(double x1, double x2, double x3,
 					     double y, Energy pT, double muj,
 					     double muk) const {
   double xPerp2 = 4.*pT*pT/mW_/mW_;
-  double root1 = sqr(x2)-4.*sqr(muk);
-  double root2 = sqr(x1)-xPerp2 - 4.*sqr(muj);
-  if(root1<0. || root2<0.) cerr << "problem in root " << root1 << " " << root2 << "\n";
-  root1 = max(root1,0.);
-  root2 = max(root2,0.);
+  double root1 = sqrt(max(0.,sqr(x2)-4.*sqr(muk)));
+  double root2 = sqrt(max(0.,sqr(x1)-xPerp2 - 4.*sqr(muj)));
   static double tolerance = 1e-6; 
   bool isMomentaReconstructed = false;  
 
   if(pT*sinh(y) > ZERO) {
-    if(abs(-sqrt(root1) + sqrt(sqr(x3)-xPerp2)  + sqrt(root2)) <= tolerance ||
-       abs(-sqrt(root1) + sqrt(sqr(x3)-xPerp2)  - sqrt(root2))  <= tolerance)
+    if(abs(-root1 + sqrt(sqr(x3)-xPerp2)  + root2) <= tolerance ||
+       abs(-root1 + sqrt(sqr(x3)-xPerp2)  - root2)  <= tolerance)
       isMomentaReconstructed=true;
   }
   else if(pT*sinh(y) < ZERO){
-    if(abs(-sqrt(root1) - sqrt(sqr(x3)-xPerp2)  + sqrt(root2)) <= tolerance ||
-       abs(-sqrt(root1) - sqrt(sqr(x3)-xPerp2)  - sqrt(root2))  <= tolerance) 
+    if(abs(-root1 - sqrt(sqr(x3)-xPerp2)  + root2) <= tolerance ||
+       abs(-root1 - sqrt(sqr(x3)-xPerp2)  - root2)  <= tolerance)
 	isMomentaReconstructed=true;
   }
   else 
-    if(abs(-sqrt(root1)+ sqrt(sqr(x1)-xPerp2 - 4.*(muj))) <= tolerance) 
+    if(abs(-root1+ sqrt(sqr(x1)-xPerp2 - 4.*(muj))) <= tolerance)
       isMomentaReconstructed=true;
       
   return isMomentaReconstructed;
