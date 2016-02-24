84b9e8e93f5b05d6739fb7479845f604dbaf4653
Peter Richardson <Peter.Richardson@durham.ac.uk>
2016-02-24 10:25:39 +0100

    improvements to tau correlation analysis

--- Contrib/TauAnalysis/TauCorrelationAnalysis.h
+++ Contrib/TauAnalysis/TauCorrelationAnalysis.h
@@ -117,12 +117,12 @@ protected:
    * Analyze the given particle for correlations with pi.
    * @param particle pointer to the particle to be analyzed.
    */
-  void analyzePi(tPPtr particle);
+  void analyzePi(tPPtr particle, ParticleVector children);
   /**
    * Analyze the given particle for correlations with rho.
    * @param particle pointer to the particle to be analyzed.
    */
-  void analyzeRho(tPPtr particle);
+  void analyzeRho(tPPtr particle, ParticleVector children);
   //@}
 
 private:
