3eeba7efc4bfc731d757a3a02579b488b1bd559c
David Grellscheid <david.grellscheid@durham.ac.uk>
2016-10-11 16:08:35 +0100

    De-boostify foreach.

--- MatrixElement/Matchbox/Base/MatchboxMEBase.cc
+++ MatrixElement/Matchbox/Base/MatchboxMEBase.cc
@@ -33,7 +33,6 @@
 #include "Herwig/MatrixElement/ProductionMatrixElement.h"
 #include "Herwig/MatrixElement/HardVertex.h"
 
-#include <boost/foreach.hpp>
 #include <cctype>
 
 #include <iterator>
@@ -1471,7 +1470,7 @@ void MatchboxMEBase::prepareXComb(MatchboxXCombData& xc) const {
   if ( initVerbose() ) {
     ostringstream fname_strm;
     // only allow alphanumeric, / and _ in filename
-    BOOST_FOREACH (const char c, name()) {
+    for (const char c : name()) {
         switch (c) {
           case '+' : fname_strm << "+"; break;
           case '-' : fname_strm << "-"; break;
