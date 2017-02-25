7660ba7a944e18f94c1927ccd464d8fc16cd399e
Stephen Webster <s.j.webster@durham.ac.uk>
2017-02-25 18:52:11 +0000

    Added the shower scale options to the TopMT  and TopMass scales.
    --HG--
    branch : herwig-7-1

--- MatrixElement/Matchbox/Scales/MatchboxTopMassScale.cc
+++ MatrixElement/Matchbox/Scales/MatchboxTopMassScale.cc
@@ -13,6 +13,7 @@
 
 #include "MatchboxTopMassScale.h"
 #include "ThePEG/Interface/ClassDocumentation.h"
+#include "ThePEG/Interface/Switch.h"
 #include "ThePEG/Interface/Parameter.h"
 #include "ThePEG/Interface/Reference.h"
 #include "ThePEG/EventRecord/Particle.h"
@@ -26,7 +27,8 @@
 
 using namespace Herwig;
 
-MatchboxTopMassScale::MatchboxTopMassScale() {}
+MatchboxTopMassScale::MatchboxTopMassScale() :
+  theShowerScaleMode(1) {}
 
 MatchboxTopMassScale::~MatchboxTopMassScale() {}
 
@@ -74,13 +76,76 @@ Energy2 MatchboxTopMassScale::factorizationScale() const {
   return(renormalizationScale());
 }
 
+
+Energy2 MatchboxTopMassScale::showerScale() const {
+  
+  if ( theShowerScaleMode == 1 )
+    return factorizationScale();
+  
+  else {
+    assert(theShowerScaleMode == 2);
+    size_t k = 2;
+    int top = -1;
+    int antitop = -1;
+    int emission = -1;
+  
+    if ( mePartonData().size() > 5 )
+      assert(false && "MatchboxTopMassScale.cc: mePartonData().size() > 5.\n");
+
+    while ( (top == -1 || antitop == -1 || emission == -1) && k < mePartonData().size() ){
+      if ( mePartonData()[k]->id() == 6 ) {
+	if ( top < 0 )
+	  top = k;
+	else
+	  assert(false);
+      } else if ( mePartonData()[k]->id() == -6 ) {
+	if ( antitop < 0 )
+	  antitop = k;
+	else
+	  assert(false);
+      }
+      else if ( abs(mePartonData()[k]->id()) < 6 || mePartonData()[k]->id() == 21 ) {
+	if ( emission < 0 )
+	  emission = k;
+	else
+	  assert(false && "More than one emission in MatchboxTopMassScale.\n");
+      }
+    
+      k++;
+    }
+
+    if ( top < 2 || antitop < 2 ){
+      throw Exception() << "MatchboxTopMassScale: Could not find a top-antitop-pair in the final state!\n"
+			<< Exception::runerror;
+    }
+  
+
+    if ( mePartonData().size() > 4 && emission < 2 ){
+      throw Exception() << "MatchboxTopMassScale: Could not find an emission in a state where mePartonData().size() > 4.\n"
+			<< Exception::runerror;
+    }
+
+    if ( emission < 2 )
+      return (1./2.)*(meMomenta()[top].mt2()+meMomenta()[antitop].mt2());
+  
+    else
+      return (1./3.)*(meMomenta()[top].mt2()+meMomenta()[antitop].mt2() + meMomenta()[emission].mt2());
+  }
+}
+
+
+
 // If needed, insert default implementations of virtual function defined
 // in the InterfacedBase class here (using ThePEG-interfaced-impl in Emacs).
 
 
-void MatchboxTopMassScale::persistentOutput(PersistentOStream &) const {}
+void MatchboxTopMassScale::persistentOutput(PersistentOStream & os) const {
+  os << theShowerScaleMode;
+}
 
-void MatchboxTopMassScale::persistentInput(PersistentIStream &, int) {}
+void MatchboxTopMassScale::persistentInput(PersistentIStream & is, int) {
+  is >> theShowerScaleMode;
+}
 
 
 // *** Attention *** The following static variable is needed for the type
@@ -96,6 +161,15 @@ void MatchboxTopMassScale::Init() {
   static ClassDocumentation<MatchboxTopMassScale> documentation
     ("MatchboxTopMassScale implements invariant mass of top-antitop pair as scale choice.");
 
+
+  static Switch<MatchboxTopMassScale, unsigned int> interfaceShowerScaleMode
+    ("ShowerScaleMode",
+     "Choose the definition of the shower hard scale.",
+     &MatchboxTopMassScale::theShowerScaleMode, 1, false, false);
+  static SwitchOption FactorizationScale
+    (interfaceShowerScaleMode,"FactorizationScale","Use the factorization scale.", 1);
+  static SwitchOption MeanMT2
+    (interfaceShowerScaleMode,"MeanMT2","Use the mean squared transverse mass of the outgoing particles.", 2);
 
 }
 
