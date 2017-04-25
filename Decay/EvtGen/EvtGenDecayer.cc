cc9242c2fee1a5e1524d90a5524e78227297c818
Johannes Bellm <johannes.bellm@durham.ac.uk>
2017-04-25 08:40:39 +0100

    Print decay products in EVTGen
    --HG--
    branch : herwig-7-1

--- Decay/EvtGen/EvtGenDecayer.cc
+++ Decay/EvtGen/EvtGenDecayer.cc
@@ -42,14 +42,16 @@ ParticleVector EvtGenDecayer::decay(const DecayMode & dm,
     }
     if(abs(ptotal.x())>0.001*MeV||abs(ptotal.y())>0.001*MeV||
        abs(ptotal.z())>0.001*MeV||abs(ptotal.e())>0.001*MeV) {
-      generator()->log() << "Decay of " << parent.PDGName() 
-			 << " violates momentum conservation in"
-			 << "EvtGenDecayer::decay\n";
+      generator()->log() << "Decay of " << parent.PDGName()<<" -> ";
+      for (auto const & dec : output)
+        generator()->log()<< dec->dataPtr()->PDGName()<<" ";
+      generator()->log() << " violates momentum conservation in"
+			 << " EvtGenDecayer::decay\n";
     }
     if(charge!=0) {
       generator()->log() << "Decay of " << parent.PDGName() 
 			 << " violates charge conservation in"
-			 << "EvtGenDecayer::decay\n";
+			 << " EvtGenDecayer::decay\n";
     }
   }
   return output;
@@ -133,8 +135,11 @@ void EvtGenDecayer::checkDecay(PPtr in) const {
   if(abs(ptotal.x())>MeV||abs(ptotal.y())>MeV||
      abs(ptotal.z())>MeV||abs(ptotal.e())>MeV) {
     generator()->log() 
-      << "Decay of " << in->PDGName() << " violates momentum conservation"
-      << "in EvtGenDecayer::checkDecay";
+      << "SubDecay of " << in->PDGName() <<" -> ";
+      for (auto const & dec : in->children())
+        generator()->log()<< dec->dataPtr()->PDGName()<<" ";
+      generator()->log() <<" violates momentum conservation"
+      << " in EvtGenDecayer::checkDecay\n";
   }
   if(charge!=0) generator()->log() << "Decay of " << in->PDGName() 
 				   << " violates charge conservation in "
