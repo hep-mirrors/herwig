67c77aeddfd82f73b0aa8932031f6ec47039a9ed
Peter Richardson <Peter.Richardson@durham.ac.uk>
2018-03-06 14:03:08 +0100

    check to prevent division by zero due to numeric issues
    --HG--
    branch : herwig-7-1

--- Decay/DecayPhaseSpaceChannel.cc
+++ Decay/DecayPhaseSpaceChannel.cc
@@ -232,7 +232,10 @@ double DecayPhaseSpaceChannel::generateWeight(const vector<Lorentz5Momentum> & o
       // factor for the kinematics
       pcm = Kinematics::pstarTwoBodyDecay(intmass[ix],intmass[idau[0]],
 				   intmass[idau[1]]);
-      wgt *= intmass[ix]*8.*pi*pi/pcm;
+      if(pcm>ZERO)
+	wgt *= intmass[ix]*8.*pi*pi/pcm;
+      else
+	wgt = 0.;
     }
     // only first off-shell
     else if(_intdau1[ix]<0) {
@@ -243,8 +246,11 @@ double DecayPhaseSpaceChannel::generateWeight(const vector<Lorentz5Momentum> & o
 	lower+=output[_intext[idau[0]][iy]].mass();
       wgt *=scale*massWeight(idau[0],intmass[idau[0]],lower,upper);
       pcm = Kinematics::pstarTwoBodyDecay(intmass[ix],intmass[idau[0]],
-				   output[idau[1]].mass());
-      wgt *= intmass[ix]*8.*pi*pi/pcm;
+					  output[idau[1]].mass());
+      if(pcm>ZERO)
+	wgt *= intmass[ix]*8.*pi*pi/pcm;
+      else
+	wgt = 0.;
     }
     // only second off-shell
     else if(_intdau2[ix]<0) {
@@ -255,14 +261,17 @@ double DecayPhaseSpaceChannel::generateWeight(const vector<Lorentz5Momentum> & o
 	lower+=output[_intext[idau[1]][iy]].mass();
       wgt *=scale*massWeight(idau[1],intmass[idau[1]],lower,upper);
       pcm = Kinematics::pstarTwoBodyDecay(intmass[ix],intmass[idau[1]],
-				   output[idau[0]].mass());
-      wgt *=intmass[ix]*8.*pi*pi/pcm;
+					  output[idau[0]].mass());
+      if(pcm>ZERO)
+	wgt *=intmass[ix]*8.*pi*pi/pcm;
+      else
+	wgt=0.;
     }
     // both on-shell
     else {
       pcm = Kinematics::pstarTwoBodyDecay(intmass[ix],output[idau[1]].mass(),
 					  output[idau[0]].mass());
-      if(pcm!=ZERO)
+      if(pcm>ZERO)
 	wgt *=intmass[ix]*8.*pi*pi/pcm;
       else
 	wgt = 0.;
