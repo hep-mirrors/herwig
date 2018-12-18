9c815c2a63989f79923001193ce0e0f9f8155a1d
Peter Richardson <Peter.Richardson@durham.ac.uk>
2018-12-18 08:39:59 +0000

    first version of f1 decayer

---
+++ Decay/VectorMeson/f1RhoPiPiDecayer.cc
@@ -0,0 +1,216 @@
+// -*- C++ -*-
+//
+// This is the implementation of the non-inlined, non-templated member
+// functions of the f1RhoPiPiDecayer class.
+//
+
+#include "f1RhoPiPiDecayer.h"
+#include "ThePEG/Interface/ClassDocumentation.h"
+#include "ThePEG/Interface/ParVector.h"
+#include "ThePEG/EventRecord/Particle.h"
+#include "ThePEG/Repository/UseRandom.h"
+#include "ThePEG/Repository/EventGenerator.h"
+#include "ThePEG/Utilities/DescribeClass.h"
+#include "ThePEG/Persistency/PersistentOStream.h"
+#include "ThePEG/Persistency/PersistentIStream.h"
+#include "ThePEG/Helicity/epsilon.h"
+
+using namespace Herwig;
+
+f1RhoPiPiDecayer::f1RhoPiPiDecayer() :
+  gRhoPiPi_(6.), ga1RhoPi_(4.8*GeV), gf1a1Pi_(1./GeV), maxWeight_({1.,1.,1.}) {
+  generateIntermediates(true);
+}
+
+// bool f1RhoPiPiDecayer::accept(const DecayMode & dm) const {
+//   return false;
+// }
+
+// ParticleVector f1RhoPiPiDecayer::decay(const DecayMode & dm,
+// 				  const Particle & parent) const {
+//   ParticleVector children = dm.produceProducts();
+//   return children;
+// }
+
+
+IBPtr f1RhoPiPiDecayer::clone() const {
+  return new_ptr(*this);
+}
+
+IBPtr f1RhoPiPiDecayer::fullclone() const {
+  return new_ptr(*this);
+}
+
+void f1RhoPiPiDecayer::persistentOutput(PersistentOStream & os) const {
+  os << gRhoPiPi_ << ounit(ga1RhoPi_,GeV) << ounit(gf1a1Pi_,1./GeV) << maxWeight_;
+}
+
+void f1RhoPiPiDecayer::persistentInput(PersistentIStream & is, int) {
+  is >> gRhoPiPi_ >> iunit(ga1RhoPi_,GeV) >> iunit(gf1a1Pi_,1./GeV) >> maxWeight_;
+}
+
+// The following static variable is needed for the type
+// description system in ThePEG.
+DescribeClass<f1RhoPiPiDecayer,DecayIntegrator>
+describeHerwigf1RhoPiPiDecayer("Herwig::f1RhoPiPiDecayer", "HwVMDecay.so");
+
+void f1RhoPiPiDecayer::Init() {
+
+  static ClassDocumentation<f1RhoPiPiDecayer> documentation
+    ("The f1RhoPiPiDecayer class implements a simple model for "
+     "f1 -> pipirho via an intermediate a_1");
+
+  static ParVector<f1RhoPiPiDecayer,double> interfaceMaxWeight
+    ("MaxWeight",
+     "Maximum weights for the decays",
+     &f1RhoPiPiDecayer::maxWeight_, 3, 1.0, 0.0, 100.0,
+     false, false, Interface::limited);
+
+  static Parameter<f1RhoPiPiDecayer,double> interfacegRhoPiPi
+    ("gRhoPiPi",
+     "The coupling of the rho to two pions",
+     &f1RhoPiPiDecayer::gRhoPiPi_, 6., 0.0, 10.0,
+     false, false, Interface::limited);
+
+  static Parameter<f1RhoPiPiDecayer,Energy> interfacega1RhoPi
+    ("ga1RhoPi",
+     "Coupling of the a_1 to rho pi",
+     &f1RhoPiPiDecayer::ga1RhoPi_, GeV, 4.8*GeV, 0.*GeV, 20.*GeV,
+     false, false, Interface::limited);
+  
+  static Parameter<f1RhoPiPiDecayer,InvEnergy> interfacegf1a1Pi
+    ("gf1a1Pi",
+     "Coupling of f_1 to a_1 pi",
+     &f1RhoPiPiDecayer::gf1a1Pi_, 1./GeV, 1.0/GeV, 0.0/GeV, 10.0/GeV,
+     false, false, Interface::limited);
+
+}
+
+void f1RhoPiPiDecayer::doinit() {
+  DecayIntegrator::doinit();
+  // pointers to the particles we need as external particles
+  tPDPtr f1 = getParticleData(ParticleID::f_1);
+  tPDPtr a1p = getParticleData(ParticleID::a_1plus);
+  tPDPtr a1m = getParticleData(ParticleID::a_1minus);
+  tPDPtr a10 = getParticleData(ParticleID::a_10);
+  tPDPtr pip = getParticleData(ParticleID::piplus);
+  tPDPtr pim = getParticleData(ParticleID::piminus);
+  tPDPtr pi0 = getParticleData(ParticleID::pi0);
+  tPDPtr rhop = getParticleData(ParticleID::rhoplus);
+  tPDPtr rhom = getParticleData(ParticleID::rhominus);
+  tPDPtr rho0 = getParticleData(ParticleID::rho0);
+  // decay mode f_1 -> pi+ pi- rho0
+  PhaseSpaceModePtr mode = new_ptr(PhaseSpaceMode(f1,{pip,pim,rho0},maxWeight_[0]));
+  mode->addChannel((PhaseSpaceChannel(mode),0,a1m,0,1,1,2,1,3));
+  mode->addChannel((PhaseSpaceChannel(mode),0,a1p,0,2,1,1,1,3));
+  addMode(mode);
+  // decay mode f_1 -> pi- pi0 rho+
+  mode = new_ptr(PhaseSpaceMode(f1,{pim,pi0,rhop},maxWeight_[0]));
+  mode->addChannel((PhaseSpaceChannel(mode),0,a1p,0,1,1,2,1,3));
+  mode->addChannel((PhaseSpaceChannel(mode),0,a10,0,2,1,1,1,3));
+  addMode(mode);
+  // decay mode f_1 -> pi+ pi0 rho-
+  mode = new_ptr(PhaseSpaceMode(f1,{pip,pi0,rhom},maxWeight_[0]));
+  mode->addChannel((PhaseSpaceChannel(mode),0,a1m,0,1,1,2,1,3));
+  mode->addChannel((PhaseSpaceChannel(mode),0,a10,0,2,1,1,1,3));
+  addMode(mode);
+  ma1_ = a1p->mass();
+  ga1_ = a1p->width(); 
+}
+
+void f1RhoPiPiDecayer::doinitrun() {
+  DecayIntegrator::doinitrun();
+  if(initialize()) {
+    for(unsigned int ix=0;ix<maxWeight_.size();++ix)
+      maxWeight_[ix] = mode(ix)->maxWeight();
+  }
+}
+
+int f1RhoPiPiDecayer::modeNumber(bool & cc,tcPDPtr parent,
+				const tPDVector & children) const {
+  if(children.size()!=3 || parent->id()!=ParticleID::f_1) return -1;
+  // check the pions
+  int npi0(0),npiplus(0),npiminus(0),idrho(0);
+  for(auto child : children) {
+    int idtemp= child->id();
+    if(idtemp==ParticleID::piplus)       ++npiplus;
+    else if(idtemp==ParticleID::piminus) ++npiminus;
+    else if(idtemp==ParticleID::pi0)     ++npi0;
+    else                                 idrho=idtemp;
+  }
+  cc = false;
+  // f_1 -> pi+pi-rho0 mode
+  if(idrho==113 && npiplus==1 && npiminus==1)    return 0;
+  // f_1 -> pi-pi0rho+ mode
+  else if(idrho== 213 && npiminus==1 && npi0==1) return 1;
+  // f_1 -> pi+pi0rho- mode
+  else if(idrho==-213 && npiplus ==1 && npi0==1) return 2;
+  // not found
+  return -1;
+}
+
+void f1RhoPiPiDecayer::
+constructSpinInfo(const Particle & part, ParticleVector decay) const {
+  VectorWaveFunction::constructSpinInfo(vectors_[0],const_ptr_cast<tPPtr>(&part),
+  					incoming,true,false);
+  // set up the spin information for the decay products
+  // pions
+  for(unsigned int ix=0;ix<2;++ix)
+    ScalarWaveFunction::constructSpinInfo(decay[ix],outgoing,true);
+  // rho
+  VectorWaveFunction::constructSpinInfo(vectors_[1],decay[2],outgoing,true,false);
+}
+
+double f1RhoPiPiDecayer::me2(const int ichan, const Particle & part,
+			    const tPDVector & ,
+			    const vector<Lorentz5Momentum> & momenta,
+			    MEOption meopt) const {
+  if(!ME())
+    ME(new_ptr(GeneralDecayMatrixElement(PDT::Spin1,PDT::Spin0,PDT::Spin0,PDT::Spin1)));
+  useMe();
+  // polarization vectors for incoming
+  if(meopt==Initialize) {
+    VectorWaveFunction::calculateWaveFunctions(vectors_[0],rho_,
+					       const_ptr_cast<tPPtr>(&part),
+					       incoming,false);
+  }
+  // polarization vectors for rho
+  vectors_[1].resize(3);
+  for(unsigned int ix=0;ix<3;++ix)
+    vectors_[1][ix] = HelicityFunctions::polarizationVector(-momenta[2],ix,Helicity::outgoing);
+  // breit wigners
+  Lorentz5Momentum pa1[2] = {part.momentum()-momenta[0],
+			     part.momentum()-momenta[1]};
+  for(unsigned int ix=0;ix<2;++ix) pa1[ix].rescaleMass();
+  complex<InvEnergy2> bwa1[2] = {Resonance::BreitWignera1(pa1[0].mass2(),ma1_,ga1_)/sqr(ma1_),
+				 Resonance::BreitWignera1(pa1[1].mass2(),ma1_,ga1_)/sqr(ma1_)};
+  if(ichan>0) bwa1[ ichan == 0 ? 1 : 0 ] = ZERO;
+  // compute the matrix element
+  for(unsigned int ihel=0;ihel<3;++ihel) {
+    LorentzVector<complex<Energy2> > pol[2] = {epsilon(vectors_[0][ihel],part.momentum(),pa1[0]),
+					       epsilon(vectors_[0][ihel],part.momentum(),pa1[1])};
+    for(unsigned int ohel=0;ohel<3;++ohel) {
+      (*ME())(ihel,0,0,0,ohel) = gf1a1Pi_*ga1RhoPi_*(pol[0]*bwa1[0]-pol[1]*bwa1[1]).dot(vectors_[1][ohel]);
+    }
+  } 
+  // matrix element and identical particle factor
+  double output=ME()->contract(rho_).real();
+  // return the answer
+  return output;
+}
+
+void f1RhoPiPiDecayer::dataBaseOutput(ofstream & output,
+					    bool header) const {
+  if(header) output << "update decayers set parameters=\"";
+  // parameters for the DecayIntegrator base class
+  DecayIntegrator::dataBaseOutput(output,false);
+  output << "newdef " << name() << ":gRhoPiPi " << gRhoPiPi_     << "\n";
+  output << "newdef " << name() << ":ga1RhoPi " << ga1RhoPi_/GeV << "\n";
+  output << "newdef " << name() << ":gf1a1Pi "  << gf1a1Pi_*GeV  << "\n";
+  for(unsigned int ix=0;ix<maxWeight_.size();++ix) {
+    output << "newdef    " << name() << ":maxWeight " << ix << " "
+  	   << maxWeight_[ix] << "\n";
+  }
+  if(header) output << "\n\" where BINARY ThePEGName=\"" 
+  		    << fullName() << "\";" << endl;
+}
---
+++ Decay/VectorMeson/f1RhoPiPiDecayer.h
@@ -0,0 +1,202 @@
+// -*- C++ -*-
+#ifndef Herwig_f1RhoPiPiDecayer_H
+#define Herwig_f1RhoPiPiDecayer_H
+//
+// This is the declaration of the f1RhoPiPiDecayer class.
+//
+
+#include "DecayIntegrator.h"
+
+namespace Herwig {
+
+using namespace ThePEG;
+
+/**
+ * Here is the documentation of the f1RhoPiPiDecayer class.
+ *
+ * @see \ref f1RhoPiPiDecayerInterfaces "The interfaces"
+ * defined for f1RhoPiPiDecayer.
+ */
+class f1RhoPiPiDecayer: public DecayIntegrator {
+
+public:
+
+  /**
+   * The default constructor.
+   */
+  f1RhoPiPiDecayer();
+
+  /**
+   * Which of the possible decays is required
+   * @param cc Is this mode the charge conjugate
+   * @param parent The decaying particle
+   * @param children The decay products
+   */
+  virtual int modeNumber(bool & cc, tcPDPtr parent, 
+			 const tPDVector & children) const;
+
+  /**
+   * Return the matrix element squared for a given mode and phase-space channel.
+   * @param ichan The channel we are calculating the matrix element for. 
+   * @param part The decaying Particle.
+   * @param outgoing The particles produced in the decay
+   * @param momenta  The momenta of the particles produced in the decay
+   * @param meopt Option for the calculation of the matrix element
+   * @return The matrix element squared for the phase-space configuration.
+   */
+  double me2(const int ichan,const Particle & part,
+	     const tPDVector & outgoing,
+	     const vector<Lorentz5Momentum> & momenta,
+	     MEOption meopt) const;
+
+  /**
+   *   Construct the SpinInfos for the particles produced in the decay
+   */
+  virtual void constructSpinInfo(const Particle & part,
+				 ParticleVector outgoing) const;
+
+  /**
+   * Output the setup information for the particle database
+   * @param os The stream to output the information to
+   * @param header Whether or not to output the information for MySQL
+   */
+  virtual void dataBaseOutput(ofstream & os,bool header) const;
+  
+// public:
+
+//   /** @name Virtual functions required by the Decayer class. */
+//   //@{
+//   /**
+//    * Check if this decayer can perfom the decay specified by the
+//    * given decay mode.
+//    * @param dm the DecayMode describing the decay.
+//    * @return true if this decayer can handle the given mode, otherwise false.
+//    */
+//   virtual bool accept(const DecayMode & dm) const;
+
+//   /**
+//    * Perform a decay for a given DecayMode and a given Particle instance.
+//    * @param dm the DecayMode describing the decay.
+//    * @param p the Particle instance to be decayed.
+//    * @return a ParticleVector containing the decay products.
+//    */
+//   virtual ParticleVector decay(const DecayMode & dm, const Particle & p) const;
+//   //@}
+
+public:
+
+  /** @name Functions used by the persistent I/O system. */
+  //@{
+  /**
+   * Function used to write out object persistently.
+   * @param os the persistent output stream written to.
+   */
+  void persistentOutput(PersistentOStream & os) const;
+
+  /**
+   * Function used to read in object persistently.
+   * @param is the persistent input stream read from.
+   * @param version the version number of the object when written.
+   */
+  void persistentInput(PersistentIStream & is, int version);
+  //@}
+
+  /**
+   * The standard Init function used to initialize the interfaces.
+   * Called exactly once for each class by the class description system
+   * before the main function starts or
+   * when this class is dynamically loaded.
+   */
+  static void Init();
+
+protected:
+
+  /** @name Clone Methods. */
+  //@{
+  /**
+   * Make a simple clone of this object.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr clone() const;
+
+  /** Make a clone of this object, possibly modifying the cloned object
+   * to make it sane.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr fullclone() const;
+  //@}
+  
+protected:
+
+  /** @name Standard Interfaced functions. */
+  //@{
+  /**
+   * Initialize this object after the setup phase before saving an
+   * EventGenerator to disk.
+   * @throws InitException if object could not be initialized properly.
+   */
+  virtual void doinit();
+
+  /**
+   * Initialize this object. Called in the run phase just before
+   * a run begins.
+   */
+  virtual void doinitrun();
+  //@}
+
+private:
+
+  /**
+   * The assignment operator is private and must never be called.
+   * In fact, it should not even be implemented.
+   */
+  f1RhoPiPiDecayer & operator=(const f1RhoPiPiDecayer &);
+
+private:
+
+  /**
+   *  rho-pi-pi coupling
+   */
+  double gRhoPiPi_;
+
+  /**
+   *  a_1-rho-pi
+   */
+  Energy ga1RhoPi_;
+
+  /**
+   *  f1-a1-pi
+   */
+  InvEnergy gf1a1Pi_;
+
+  /**
+   *  a1 mass
+   */
+  Energy ma1_;
+
+  /**
+   * a1 width
+   */
+  Energy ga1_;
+  
+
+  /**
+   *  Weights for the different decay modes
+   */
+  vector<double> maxWeight_;
+
+  /**
+   *  Spin Density matrix
+   */
+  mutable RhoDMatrix rho_;
+
+  /**
+   *  Polarization vectors
+   */
+  mutable vector<Helicity::LorentzPolarizationVector> vectors_[2];
+  
+};
+
+}
+
+#endif /* Herwig_f1RhoPiPiDecayer_H */
