6cf9ca53538d83a50d2916c0baed2943112d57c3
Peter Richardson <peter.richardson@durham.ac.uk>
2020-05-04 15:46:03 +0100

    fix imax value

--- Models/Feynrules/python/ufo2peg/check_lorentz.py
+++ Models/Feynrules/python/ufo2peg/check_lorentz.py
@@ -206,6 +206,7 @@ def processTensorCouplings(lorentztag,vertex,model,parmsubs,all_couplings,order)
                     if(all_couplings[icolor][lmax+ix] ==
                        all_couplings[icolor][2*lmax+ix]) :
                         test[0]  = all_couplings[icolor][lmax+ix]
+                        imax=1
                     else :
                         test[1] = all_couplings[icolor][lmax+ix]
                         test[2] = all_couplings[icolor][2*lmax+ix]
