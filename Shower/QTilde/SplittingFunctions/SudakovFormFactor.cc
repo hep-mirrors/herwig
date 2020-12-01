dd006e2f6fc5626a79c37c9477fe0539cf5ccc14
Peter Richardson <Peter.Richardson@durham.ac.uk>
2020-12-01 12:14:02 +0000

    new layer

---
+++ Shower/QTilde/SplittingFunctions/SudakovFormFactor.cc
@@ -0,0 +1,334 @@
+// -*- C++ -*-
+//
+// This is the implementation of the non-inlined, non-templated member
+// functions of the SudakovFormFactor class.
+//
+
+#include "SudakovFormFactor.h"
+#include "ThePEG/Interface/ClassDocumentation.h"
+#include "ThePEG/Interface/Switch.h"
+#include "ThePEG/Interface/Parameter.h"
+#include "ThePEG/EventRecord/Particle.h"
+#include "ThePEG/Repository/UseRandom.h"
+#include "ThePEG/Repository/EventGenerator.h"
+#include "ThePEG/Utilities/DescribeClass.h"
+#include "ThePEG/Persistency/PersistentOStream.h"
+#include "ThePEG/Persistency/PersistentIStream.h"
+#include "ThePEG/Utilities/EnumIO.h"
+#include "Herwig/Shower/QTilde/QTildeShowerHandler.h"
+
+using namespace Herwig;
+
+
+void SudakovFormFactor::persistentOutput(PersistentOStream & os) const {
+  os << pdfMax_ << pdfFactor_ << angularOrdered_
+     << oenum(interactionType_) << oenum(colourStructure_) << particles_ ;
+}
+
+void SudakovFormFactor::persistentInput(PersistentIStream & is, int) {
+  is >> pdfMax_ >> pdfFactor_ >> angularOrdered_ 
+     >> ienum(interactionType_) >> ienum(colourStructure_) >> particles_;
+}
+
+
+// The following static variable is needed for the type
+// description system in ThePEG.
+DescribeAbstractClass<SudakovFormFactor,Interfaced>
+describeHerwigSudakovFormFactor("Herwig::SudakovFormFactor", "HwShower.so");
+
+void SudakovFormFactor::Init() {
+
+  static ClassDocumentation<SudakovFormFactor> documentation
+    ("There is no documentation for the SudakovFormFactor class");
+
+  static Switch<SudakovFormFactor,ShowerInteraction> 
+    interfaceInteractionType
+    ("InteractionType",
+     "Type of the interaction",
+     &SudakovFormFactor::interactionType_, 
+     ShowerInteraction::UNDEFINED, false, false);
+  static SwitchOption interfaceInteractionTypeQCD
+    (interfaceInteractionType,
+     "QCD","QCD",ShowerInteraction::QCD);
+  static SwitchOption interfaceInteractionTypeQED
+    (interfaceInteractionType,
+     "QED","QED",ShowerInteraction::QED);
+
+  static Switch<SudakovFormFactor,bool> interfaceAngularOrdered
+    ("AngularOrdered",
+     "Whether or not this interaction is angular ordered, "
+     "normally only g->q qbar and gamma-> f fbar are the only ones which aren't.",
+     &SudakovFormFactor::angularOrdered_, true, false, false);
+  static SwitchOption interfaceAngularOrderedYes
+    (interfaceAngularOrdered,
+     "Yes",
+     "Interaction is angular ordered",
+     true);
+  static SwitchOption interfaceAngularOrderedNo
+    (interfaceAngularOrdered,
+     "No",
+     "Interaction isn't angular ordered",
+     false);
+  
+  static Switch<SudakovFormFactor,ColourStructure> interfaceColourStructure
+    ("ColourStructure",
+     "The colour structure for the splitting function",
+     &SudakovFormFactor::colourStructure_, Undefined, false, false);
+  static SwitchOption interfaceColourStructureTripletTripletOctet
+    (interfaceColourStructure,
+     "TripletTripletOctet",
+     "3 -> 3 8",
+     TripletTripletOctet);
+  static SwitchOption interfaceColourStructureOctetOctetOctet
+    (interfaceColourStructure,
+     "OctetOctetOctet",
+     "8 -> 8 8",
+     OctetOctetOctet);
+  static SwitchOption interfaceColourStructureOctetTripletTriplet
+    (interfaceColourStructure,
+     "OctetTripletTriplet",
+     "8 -> 3 3bar",
+     OctetTripletTriplet);
+  static SwitchOption interfaceColourStructureTripletOctetTriplet
+    (interfaceColourStructure,
+     "TripletOctetTriplet",
+     "3 -> 8 3",
+     TripletOctetTriplet); 
+  static SwitchOption interfaceColourStructureSextetSextetOctet
+    (interfaceColourStructure,
+     "SextetSextetOctet",
+     "6 -> 6 8",
+     SextetSextetOctet);
+  static SwitchOption interfaceColourStructureTripletTripletSinglet
+    (interfaceColourStructure,
+     "TripletTripletSinglet",
+     "3 -> 3 1",
+     TripletTripletSinglet);
+  static SwitchOption interfaceColourStructureOctetOctetSinglet
+    (interfaceColourStructure,
+     "OctetOctetSinglet",
+     "8 -> 8 1",
+     OctetOctetSinglet);
+  static SwitchOption interfaceColourStructureEpsilon
+    (interfaceColourStructure,
+     "Epsilon",
+     "3 -> 3 3",
+     Epsilon);
+  static SwitchOption interfaceColourStructureChargedChargedNeutral
+    (interfaceColourStructure,
+     "ChargedChargedNeutral",
+     "q -> q 0",
+     ChargedChargedNeutral);
+  static SwitchOption interfaceColourStructureNeutralChargedCharged
+    (interfaceColourStructure,
+     "NeutralChargedCharged",
+     "0 -> q qbar",
+     NeutralChargedCharged);
+  static SwitchOption interfaceColourStructureChargedNeutralCharged
+    (interfaceColourStructure,
+     "ChargedNeutralCharged",
+     "q -> 0 q",
+     ChargedNeutralCharged);
+
+  static Parameter<SudakovFormFactor,double> interfacePdfMax
+    ("PDFmax",
+     "Maximum value of PDF weight. ",
+     &SudakovFormFactor::pdfMax_, 35.0, 1.0, 1000000.0,
+     false, false, Interface::limited);
+
+  static Switch<SudakovFormFactor,unsigned int> interfacePDFFactor
+    ("PDFFactor",
+     "Include additional factors in the overestimate for the PDFs",
+     &SudakovFormFactor::pdfFactor_, 0, false, false);
+  static SwitchOption interfacePDFFactorNo
+    (interfacePDFFactor,
+     "No",
+     "Don't include any factors",
+     0);
+  static SwitchOption interfacePDFFactorOverZ
+    (interfacePDFFactor,
+     "OverZ",
+     "Include an additional factor of 1/z",
+     1);
+  static SwitchOption interfacePDFFactorOverOneMinusZ
+    (interfacePDFFactor,
+     "OverOneMinusZ",
+     "Include an additional factor of 1/(1-z)",
+     2);
+  static SwitchOption interfacePDFFactorOverZOneMinusZ
+    (interfacePDFFactor,
+     "OverZOneMinusZ",
+     "Include an additional factor of 1/z/(1-z)",
+     3);
+  static SwitchOption interfacePDFFactorOverRootZ
+    (interfacePDFFactor,
+     "OverRootZ",
+     "Include an additional factor of 1/sqrt(z)",
+     4);
+  static SwitchOption interfacePDFFactorRootZ
+    (interfacePDFFactor,
+     "RootZ",
+     "Include an additional factor of sqrt(z)",
+     5);
+}
+
+bool SudakovFormFactor::checkColours(const IdList & ids) const {
+  if(colourStructure_==TripletTripletOctet) {
+    if(ids[0]!=ids[1]) return false;
+    if((ids[0]->iColour()==PDT::Colour3||ids[0]->iColour()==PDT::Colour3bar) &&
+       ids[2]->iColour()==PDT::Colour8) return true;
+    return false;
+  }
+  else if(colourStructure_==OctetOctetOctet) {
+    for(unsigned int ix=0;ix<3;++ix) {
+      if(ids[ix]->iColour()!=PDT::Colour8) return false;
+    }
+    return true;
+  }
+  else if(colourStructure_==OctetTripletTriplet) {
+    if(ids[0]->iColour()!=PDT::Colour8) return false;
+    if(ids[1]->iColour()==PDT::Colour3&&ids[2]->iColour()==PDT::Colour3bar)
+      return true;
+    if(ids[1]->iColour()==PDT::Colour3bar&&ids[2]->iColour()==PDT::Colour3)
+      return true;
+    return false;
+  }
+  else if(colourStructure_==TripletOctetTriplet) {
+    if(ids[0]!=ids[2]) return false;
+    if((ids[0]->iColour()==PDT::Colour3||ids[0]->iColour()==PDT::Colour3bar) &&
+       ids[1]->iColour()==PDT::Colour8) return true;
+    return false;
+  }
+  else if(colourStructure_==SextetSextetOctet) {
+    if(ids[0]!=ids[1]) return false;
+    if((ids[0]->iColour()==PDT::Colour6 || ids[0]->iColour()==PDT::Colour6bar) &&
+       ids[2]->iColour()==PDT::Colour8) return true;
+    return false;
+  }
+  else if(colourStructure_==TripletTripletSinglet) {
+    if(ids[0]->iColour()!=ids[1]->iColour()) return false;
+    if((ids[0]->iColour()==PDT::Colour3||ids[0]->iColour()==PDT::Colour3bar) &&
+       ids[2]->iColour()==PDT::Colour0) return true;
+    return false;
+  }
+  else if(colourStructure_==OctetOctetSinglet) {
+    if(ids[0]!=ids[1]) return false;
+    if(ids[0]->iColour()==PDT::Colour8 && ids[2]->iColour()==PDT::Colour0) return true;
+    return false;
+  }
+  else if(colourStructure_==Epsilon) {
+    if(ids[0]->iColour()!=PDT::Colour3&&ids[0]->iColour()!=PDT::Colour3bar) return false;
+    if(ids[0]->iColour()!=-ids[1]->iColour()) return false;
+    if(ids[0]->iColour()!=-ids[2]->iColour()) return false;
+    return true;
+  }
+  else if(colourStructure_==ChargedChargedNeutral) {
+    if(ids[0]!=ids[1]) return false;
+    if(ids[2]->iCharge()!=0) return false;
+    if(ids[0]->iCharge()==ids[1]->iCharge()) return true;
+    return false;
+  }
+  else if(colourStructure_==ChargedNeutralCharged) {
+    if(ids[0]!=ids[2]) return false;
+    if(ids[1]->iCharge()!=0) return false;
+    if(ids[0]->iCharge()==ids[2]->iCharge()) return true;
+    return false;
+  }
+  else if(colourStructure_==NeutralChargedCharged) {
+    if(ids[1]->id()!=-ids[2]->id()) return false;
+    if(ids[0]->iCharge()!=0) return false;
+    if(ids[1]->iCharge()==-ids[2]->iCharge()) return true;
+    return false;
+  }
+  else {
+    assert(false);
+  }
+  return false;
+}
+
+void SudakovFormFactor::addSplitting(const IdList & in) {
+  bool add=true;
+  for(unsigned int ix=0;ix<particles_.size();++ix) {
+    if(particles_[ix].size()==in.size()) {
+      bool match=true;
+      for(unsigned int iy=0;iy<in.size();++iy) {
+	if(particles_[ix][iy]!=in[iy]) {
+	  match=false;
+	  break;
+	}
+      }
+      if(match) {
+	add=false;
+	break;
+      }
+    }
+  }
+  if(add) particles_.push_back(in);
+}
+
+void SudakovFormFactor::removeSplitting(const IdList & in) {
+  for(vector<IdList>::iterator it=particles_.begin();
+      it!=particles_.end();++it) {
+    if(it->size()==in.size()) {
+      bool match=true;
+      for(unsigned int iy=0;iy<in.size();++iy) {
+	if((*it)[iy]!=in[iy]) {
+	  match=false;
+	  break;
+	}
+      }
+      if(match) {
+	vector<IdList>::iterator itemp=it;
+	--itemp;
+	particles_.erase(it);
+	it = itemp;
+      }
+    }
+  }
+}
+
+bool SudakovFormFactor::PDFVeto(const Energy2 t, const double x, const double z,
+				const tcPDPtr parton0, const tcPDPtr parton1,
+				Ptr<BeamParticleData>::transient_const_pointer beam) const {
+  double ratio=PDFVetoRatio(t,x,z,parton0,parton1,beam,1.);
+  return UseRandom::rnd() > ratio;
+}
+
+double SudakovFormFactor::PDFVetoRatio(const Energy2 t, const double x, const double z,
+					   const tcPDPtr parton0, const tcPDPtr parton1,
+					   Ptr<BeamParticleData>::transient_const_pointer beam,double factor) const {
+  assert(pdf_);
+  Energy2 theScale = t * sqr(ShowerHandler::currentHandler()->factorizationScaleFactor()*factor);
+  if (theScale < sqr(freeze_)) theScale = sqr(freeze_);
+
+  const double newpdf=pdf_->xfx(beam,parton0,theScale,x/z);
+  if(newpdf<=0.) return 0.;
+
+  const double oldpdf=pdf_->xfx(beam,parton1,theScale,x);
+  if(oldpdf<=0.) return 1.;
+  
+  const double ratio = newpdf/oldpdf;
+  double maxpdf = pdfMax_;
+
+  switch (pdfFactor_) {
+  case 0: break;
+  case 1: maxpdf /= z; break;
+  case 2: maxpdf /= 1.-z; break;
+  case 3: maxpdf /= (z*(1.-z)); break;
+  case 4: maxpdf /= sqrt(z); break;
+  case 5: maxpdf *= sqrt(z); break;
+  default :
+    throw Exception() << "SudakovFormFactor::PDFVetoRatio invalid PDFfactor = "
+		      << pdfFactor_ << Exception::runerror;
+    
+  }
+
+  if (ratio > maxpdf) {
+    generator()->log() << "PDFVeto warning: Ratio > " << name()
+                       << ":PDFmax (by a factor of "
+                       << ratio/maxpdf <<") for "
+                       << parton0->PDGName() << " to "
+                       << parton1->PDGName() << "\n";
+  }
+  return ratio/maxpdf ;
+}
---
+++ Shower/QTilde/SplittingFunctions/SudakovFormFactor.h
@@ -0,0 +1,336 @@
+// -*- C++ -*-
+#ifndef Herwig_SudakovFormFactor_H
+#define Herwig_SudakovFormFactor_H
+//
+// This is the declaration of the SudakovFormFactor class.
+//
+
+#include "ThePEG/Interface/Interfaced.h"
+#include "Herwig/Shower/QTilde/ShowerConfig.h"
+#include "Herwig/Shower/QTilde/Kinematics/ShowerKinematics.fh"
+#include "ThePEG/EventRecord/RhoDMatrix.h"
+#include "ThePEG/PDF/BeamParticleData.h"
+
+namespace Herwig {
+
+using namespace ThePEG;
+
+/**  \ingroup Shower
+ * Enum to define the possible types of colour structure which can occur in
+ * the branching.
+ */
+enum ColourStructure {Undefined=0,
+		      TripletTripletOctet  = 1, OctetOctetOctet       = 2,
+		      OctetTripletTriplet  = 3, TripletOctetTriplet   = 4,
+		      SextetSextetOctet    = 5, TripletTripletSinglet = 6,
+		      OctetOctetSinglet    = 7, Epsilon               = 8,
+		      ChargedChargedNeutral=-1,
+		      ChargedNeutralCharged=-2,
+		      NeutralChargedCharged=-3};
+
+/**
+ *  A typedef for the BeamParticleData
+ */
+typedef Ptr<BeamParticleData>::transient_const_pointer tcBeamPtr;
+
+/**
+ * Here is the documentation of the SudakovFormFactor class.
+ *
+ * @see \ref SudakovFormFactorInterfaces "The interfaces"
+ * defined for SudakovFormFactor.
+ */
+class SudakovFormFactor: public Interfaced {
+
+  /**
+   *  The SplittingGenerator is a friend to insert the particles in the 
+   *  branchings at initialisation
+   */
+  friend class SplittingGenerator;
+
+public:
+
+  /** @name Standard constructors and destructors. */
+  //@{
+  /**
+   * The default constructor.
+   */
+  SudakovFormFactor() : interactionType_(ShowerInteraction::UNDEFINED),
+                        angularOrdered_(true),
+			colourStructure_(Undefined),
+			pdfMax_(35.0), pdfFactor_(0)
+  {}
+  //@}
+
+public:
+
+  /**
+   *  Members to generate the scale of the next branching
+   */
+  //@{
+  /**
+   * Return the scale of the next time-like branching. If there is no 
+   * branching then it returns ZERO.
+   * @param startingScale starting scale for the evolution
+   * @param ids The PDG codes of the particles in the splitting
+   * @param enhance The radiation enhancement factor
+   * defined.
+   */
+  virtual ShoKinPtr generateNextTimeBranching(const Energy startingScale,
+					      const IdList &ids,
+					      const RhoDMatrix & rho,
+					      double enhance, double detuning) = 0;
+
+  /**
+   * Return the scale of the next space-like decay branching. If there is no 
+   * branching then it returns ZERO.
+   * @param startingScale starting scale for the evolution
+   * @param stoppingScale stopping scale for the evolution
+   * @param minmass The minimum mass allowed for the spake-like particle.
+   * @param ids The PDG codes of the particles in the splitting
+   * defined.
+   * @param enhance The radiation enhancement factor
+   */
+  virtual ShoKinPtr generateNextDecayBranching(const Energy startingScale,
+						 const Energy stoppingScale,
+						 const Energy minmass,
+						 const IdList &ids,
+						 const RhoDMatrix & rho,
+						 double enhance,
+						 double detuning)=0;
+
+  /**
+   * Return the scale of the next space-like branching. If there is no 
+   * branching then it returns ZERO.
+   * @param startingScale starting scale for the evolution
+   * @param ids The PDG codes of the particles in the splitting
+   * @param x The fraction of the beam momentum
+   * defined.
+   * @param beam The beam particle
+   * @param enhance The radiation enhancement factor
+   */
+   virtual ShoKinPtr generateNextSpaceBranching(const Energy startingScale,
+						 const IdList &ids,double x,
+						 const RhoDMatrix & rho,
+						 double enhance,
+						 tcBeamPtr beam,
+						 double detuning)=0;
+  //@}
+
+  /**
+   *  Azimuthal angle generation
+   */
+  //@{
+  /**
+   * Generate the azimuthal angle of the branching for forward evolution
+   * @param particle The branching particle
+   * @param ids The PDG codes of the particles in the branchings
+   * @param The Shower kinematics
+   */
+  virtual double generatePhiForward(ShowerParticle & particle,const IdList & ids,
+				    ShoKinPtr kinematics, const RhoDMatrix & rho)=0;
+
+  /**
+   *  Generate the azimuthal angle of the branching for backward evolution
+   * @param particle The branching particle
+   * @param ids The PDG codes of the particles in the branchings
+   * @param The Shower kinematics
+   */
+  virtual double generatePhiBackward(ShowerParticle & particle,const IdList & ids,
+				     ShoKinPtr kinematics, const RhoDMatrix & rho)=0;
+
+  /**
+   *  Generate the azimuthal angle of the branching for ISR in decays
+   * @param particle The branching particle
+   * @param ids The PDG codes of the particles in the branchings
+   * @param The Shower kinematics
+   */
+  virtual double generatePhiDecay(ShowerParticle & particle,const IdList & ids,
+				  ShoKinPtr kinematics, const RhoDMatrix & rho)=0;
+  //@}
+  
+public:
+
+  /**
+   *  Return the colour structure
+   */
+  ColourStructure colourStructure() const {return colourStructure_;}
+
+  /**
+   *  Method to check the colours are correct
+   */
+  bool checkColours(const IdList & ids) const;
+
+public:
+
+  /**
+   *  Methods to return the interaction type and order for the Sudakov form factor
+   */
+  //@{
+  /**
+   *  Purely virtual method which should determine whether this splitting
+   *  function can be used for a given set of particles.
+   *  @param ids The PDG codes for the particles in the splitting.
+   */
+  virtual bool accept(const IdList & ids) const = 0;
+  //@}
+
+
+  /**
+   *  Method to return the evolution scale given the
+   *  transverse momentum, \f$p_T\f$ and \f$z\f$.
+   */
+  virtual Energy calculateScale(double z, Energy pt, IdList ids,unsigned int iopt) = 0;
+  
+  /**
+   *  Return the type of the interaction
+   */
+  ShowerInteraction interactionType() const {return interactionType_;}
+
+  /**
+   *  Whether or not the interaction is angular ordered
+   */
+  bool angularOrdered() const {return angularOrdered_;}
+
+protected:
+
+  /**
+   *  Set the particles in the splittings
+   */
+  void addSplitting(const IdList &);
+
+  /**
+   *  Delete the particles in the splittings
+   */
+  void removeSplitting(const IdList &);
+
+  /**
+   *  Access the potential branchings
+   */
+  const vector<IdList> & particles() const { return particles_;}
+
+  /**
+   *  The PDF factor
+   */
+  unsigned pdfFactor() const {return pdfFactor_;}
+
+  /**
+   * Maximum value of the PDF weight
+   */
+  double pdfMax() const {return pdfMax_;}
+
+  /**
+   * Veto on the PDF for the initial-state shower
+   * @param t The scale
+   * @param x The fraction of the beam momentum
+   * @param parton0 Pointer to the particleData for the 
+   *                new parent (this is the particle we evolved back to)
+   * @param parton1 Pointer to the particleData for the 
+   *                original particle
+   * @param beam The BeamParticleData object
+   */
+  bool PDFVeto(const Energy2 t, const double x, const double z,
+	       const tcPDPtr parton0, const tcPDPtr parton1,
+	       tcBeamPtr beam) const;
+  /**
+   * The PDF veto ratio
+   */
+  double PDFVetoRatio(const Energy2 t, const double x, const double z,
+               const tcPDPtr parton0, const tcPDPtr parton1,
+               tcBeamPtr beam,double factor) const;
+
+  /**
+   *   Set the PDF
+   */
+  void setPDF(tcPDFPtr pdf, Energy scale) {
+    pdf_ = pdf;
+    freeze_ = scale;
+  }
+
+public:
+
+  /** @name Functions used by the persistent I/O system. */
+  //@{
+  /**
+   * Function used to write out object persistently.
+   * @param os the persistent output stream written to.
+   */
+  void persistentOutput(PersistentOStream & os) const;
+
+  /**
+   * Function used to read in object persistently.
+   * @param is the persistent input stream read from.
+   * @param version the version number of the object when written.
+   */
+  void persistentInput(PersistentIStream & is, int version);
+  //@}
+
+  /**
+   * The standard Init function used to initialize the interfaces.
+   * Called exactly once for each class by the class description system
+   * before the main function starts or
+   * when this class is dynamically loaded.
+   */
+  static void Init();
+
+
+private:
+
+  /**
+   * The assignment operator is private and must never be called.
+   * In fact, it should not even be implemented.
+   */
+  SudakovFormFactor & operator=(const SudakovFormFactor &);
+
+private:
+
+  /**
+   *  The interaction type for the splitting function.
+   */
+  ShowerInteraction interactionType_;
+
+  /**
+   *  Whether or not this interaction is angular-ordered
+   */
+  bool angularOrdered_;
+
+  /**
+   *  The colour structure
+   */
+  ColourStructure colourStructure_;
+
+  /**
+   * List of the particles this Sudakov is used for to aid in setting up
+   * interpolation tables if needed
+   */
+  vector<IdList> particles_;
+
+  /**
+   *  Stuff for the PDFs
+   */
+  //@{
+  /**
+   *  PDf
+   */
+  tcPDFPtr pdf_;
+
+  /**
+   *  Freezing scale
+   */
+  Energy freeze_;
+
+  /**
+   * Maximum value of the PDF weight
+   */
+  double pdfMax_;
+
+  /**
+   *  Option for the inclusion of a factor \f$1/(1-z)\f$ in the PDF estimate
+   */
+  unsigned pdfFactor_;
+  //@}
+
+};
+
+}
+
+#endif /* Herwig_SudakovFormFactor_H */
