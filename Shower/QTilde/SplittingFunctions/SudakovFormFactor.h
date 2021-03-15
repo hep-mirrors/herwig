f06e0d7e9f955df0f44d08e2863bcd166d8b52e9
Peter Richardson <peter.richardson@durham.ac.uk>
2021-03-15 08:11:27 +0000

    fix delete in SudakovFormFactor

--- Shower/QTilde/SplittingFunctions/SudakovFormFactor.h
+++ Shower/QTilde/SplittingFunctions/SudakovFormFactor.h
@@ -302,7 +302,7 @@ private:
    * The assignment operator is private and must never be called.
    * In fact, it should not even be implemented.
    */
-  SudakovFormFactor & operator=(const SudakovFormFactor &);
+  SudakovFormFactor & operator=(const SudakovFormFactor &) = delete;
 
 private:
 
