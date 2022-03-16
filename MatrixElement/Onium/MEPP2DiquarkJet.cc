af14817cc58717b9c70b44b71859619e1e71ddd0
Peter Richardson <peter.richardson@durham.ac.uk>
2022-03-16 12:33:23 +0000

    add g qbar process and use newer version of python script to generate code

--- MatrixElement/Onium/MEPP2DiquarkJet.cc
+++ MatrixElement/Onium/MEPP2DiquarkJet.cc
@@ -32,21 +32,21 @@ void MEPP2DiquarkJet::getDiagrams() const {
     add(new_ptr((Tree2toNDiagram(2), g, q2, 1, q2 , 3, state_, 3, q1->CC(), -1)));
     add(new_ptr((Tree2toNDiagram(3), g, q2, q2, 2, state_, 1, q1->CC(),     -2)));
   }
-  // // q1 q2 -< diquark g (swap automatically included)
-  // add(new_ptr((Tree2toNDiagram(3), q1, q2, q2, 1, state_, 2, g,     -3)));
-  // add(new_ptr((Tree2toNDiagram(3), q1, q1, q2, 2, state_, 1, g,     -4)));
-  // // CC processes
-  // // first  g q1bar -> antidiquark q2
-  // add(new_ptr((Tree2toNDiagram(2), g, q1->CC(), 1, q1->CC() , 3, state_->CC(), 3, q2, -5)));
-  // add(new_ptr((Tree2toNDiagram(3), g, q1->CC(), q1->CC(), 2, state_->CC(), 1, q2,     -6)));
-  // // second g q2bar -> antidiquark q1
-  // if(q1!=q2) {
-  //   add(new_ptr((Tree2toNDiagram(2), g, q2->CC(), 1, q2->CC() , 3, state_->CC(), 3, q1, -5)));
-  //   add(new_ptr((Tree2toNDiagram(3), g, q2->CC(), q2->CC(), 2, state_->CC(), 1, q1,     -6)));
-  // }
-  // // q1 q2 -< diquark g (swap automatically included)
-  // add(new_ptr((Tree2toNDiagram(3), q1->CC(), q2->CC(), q2->CC(), 1, state_->CC(), 2, g,     -7)));
-  // add(new_ptr((Tree2toNDiagram(3), q1->CC(), q2->CC(), q1->CC(), 2, state_->CC(), 1, g,     -8)));
+  // q1 q2 -< diquark g (swap automatically included)
+  add(new_ptr((Tree2toNDiagram(3), q1, q2, q2, 1, state_, 2, g,     -3)));
+  add(new_ptr((Tree2toNDiagram(3), q1, q1, q2, 2, state_, 1, g,     -4)));
+  // CC processes
+  // first  g q1bar -> antidiquark q2
+  add(new_ptr((Tree2toNDiagram(2), g, q1->CC(), 1, q1->CC() , 3, state_->CC(), 3, q2, -5)));
+  add(new_ptr((Tree2toNDiagram(3), g, q1->CC(), q1->CC(), 2, state_->CC(), 1, q2,     -6)));
+  // second g q2bar -> antidiquark q1
+  if(q1!=q2) {
+    add(new_ptr((Tree2toNDiagram(2), g, q2->CC(), 1, q2->CC() , 3, state_->CC(), 3, q1, -5)));
+    add(new_ptr((Tree2toNDiagram(3), g, q2->CC(), q2->CC(), 2, state_->CC(), 1, q1,     -6)));
+  }
+  // q1 q2 -< diquark g (swap automatically included)
+  add(new_ptr((Tree2toNDiagram(3), q1->CC(), q2->CC(), q2->CC(), 1, state_->CC(), 2, g,     -7)));
+  add(new_ptr((Tree2toNDiagram(3), q1->CC(), q2->CC(), q1->CC(), 2, state_->CC(), 1, g,     -8)));
 }
 
 Energy2 MEPP2DiquarkJet::scale() const {
@@ -66,8 +66,12 @@ MEPP2DiquarkJet::diagrams(const DiagramVector & diags) const {
 
 Selector<const ColourLines *>
 MEPP2DiquarkJet::colourGeometries(tcDiagPtr diag) const {
-  static ColourLines c[2] = {ColourLines("1 3 -6, 2 -1,-4 -6, -5 -6"),
-			     ColourLines("-1 -5, 1 2 -6,3 -6, -4 -6")};
+  static ColourLines c[6] = {ColourLines("1 3 -6, 2 -1,-4 -6, -5 -6"),
+			     ColourLines("-1 -5, 1 2 -6,3 -6, -4 -6"),
+			     ColourLines("1 -6, 3 5, -4 -6, -5 -2 -6"),
+			     ColourLines("1 5, 3 -6, 4 -6, -5 -2 -6"),
+			     ColourLines("-1 -3 6, -2 1,4 6, 5 6"),
+			     ColourLines("1 5, -1 -2 6,-3 6, 4 6")};
   Selector<const ColourLines *> sel;
   sel.insert(1.0, &c[abs(diag->id())-1]);
   return sel;
@@ -89,7 +93,6 @@ void MEPP2DiquarkJet::doinit() {
     throw Exception() << "No diquark state with pid = " << pid << "in MEPP2DiquarkJet::doinit()" << Exception::runerror;
   setMassGenerator(dynamic_ptr_cast<GenericMassGeneratorPtr>(state_->massGenerator()));
 }
-
 
 // The following static variable is needed for the type
 // description system in ThePEG. 
--- MatrixElement/Onium/MEPP2cc1Jet.cc
+++ MatrixElement/Onium/MEPP2cc1Jet.cc
@@ -48,51 +48,52 @@ double MEPP2cc1Jet::me2() const {
     VectorWaveFunction g1w(rescaledMomenta()[0],mePartonData()[0],incoming);
     vector<VectorWaveFunction> g1;
     for(unsigned int ix=0;ix<2;++ix) {
+      // for gauge test g1w.reset(10);
       g1w.reset(2*ix);
       g1.push_back(g1w);
     }
-    // g q -> diquark q bar
+    // g q -> diquark qbar
     if(mePartonData()[1]->id()>0) {
       SpinorWaveFunction      q2w(rescaledMomenta()[1],mePartonData()[1],incoming);
       SpinorBarWaveFunction   q4w(rescaledMomenta()[3],mePartonData()[3],outgoing);
-      vector<SpinorWaveFunction> q2;
-      vector<SpinorBarWaveFunction> q4;
+      vector<SpinorWaveFunction> u2;
+      vector<SpinorBarWaveFunction> ubar4;
       for(unsigned int ix=0;ix<2;++ix) {
-	q2w.reset(ix);
-	q2.push_back(q2w);
-	q4w.reset(ix);
-	q4.push_back(q4w);
+      	q2w.reset(ix);
+      	u2.push_back(q2w);
+      	q4w.reset(ix);
+      	ubar4.push_back(q4w);
       }
       // matrix element squared
       // loop over the helicities
       for(unsigned int ih2=0;ih2<2;++ih2) {
-	for(unsigned int ih4=0;ih4<2;++ih4) {
-	  complex<Energy> amp1 = q2[ih2].dimensionedWave().scalar(q4[ih4].dimensionedWave());
-	  LorentzPolarizationVectorE vec1 = q2[ih2].dimensionedWave().vectorCurrent(q4[ih4].dimensionedWave());
-	  complex<Energy2> amp5 = vec1*rescaledMomenta()[0];
-	  for(unsigned int ih3=0;ih3<3;++ih3) {
-	    complex<Energy> amp4 = vec1*v3[ih3].wave();
-	    complex<Energy>  dot2 = v3[ih3].wave()*rescaledMomenta()[0];
-	    complex<Energy>  dot4 = v3[ih3].wave()*rescaledMomenta()[1];
-	    LorentzPolarizationVectorE vec2 = q2[ih2].dimensionedWave().slash(v3[ih3].wave()).vectorCurrent(q4[ih4].dimensionedWave());
-	    complex<Energy2> amp8 = vec2*rescaledMomenta()[0];
-	    for(unsigned int ih1=0;ih1<2;++ih1) {
-	      complex<Energy> amp2 = vec1*g1[ih1].wave();
-	      complex<Energy> amp3 = vec2*g1[ih1].wave();
-	      LorentzPolarizationVectorE vec3 = q2[ih2].dimensionedWave().slash(g1[ih1].wave()).vectorCurrent(q4[ih4].dimensionedWave());
-	      LorentzPolarizationVectorE vec4 = q2[ih2].dimensionedWave().slash(v3[ih3].wave()).slash(g1[ih1].wave()).vectorCurrent(q4[ih4].dimensionedWave());
-	      complex<Energy2> amp6 = vec3*rescaledMomenta()[0];
-	      complex<Energy2> amp7 = vec4*rescaledMomenta()[0];
-	      Complex dot1 = g1[ih1].wave()*v3[ih3].wave();
-	      complex<Energy>  dot3 = g1[ih1].wave()*rescaledMomenta()[1];
-	      complex<Energy>  dot5 = g1[ih1].wave()*rescaledMomenta()[3];
+       	for(unsigned int ih4=0;ih4<2;++ih4) {
+	  complex<Energy> dot6=u2[ih2].dimensionedWave().scalar(ubar4[ih4].dimensionedWave());
+	  LorentzPolarizationVectorE vec1 = u2[ih2].dimensionedWave().vectorCurrent(ubar4[ih4].dimensionedWave());
+	  complex<Energy2> dot7 = vec1*rescaledMomenta()[0];
+      	  for(unsigned int ih3=0;ih3<3;++ih3) {
+	    complex<Energy> dot1 = rescaledMomenta()[0]*v3[ih3].wave();
+	    complex<Energy> dot3 = rescaledMomenta()[1]*v3[ih3].wave();
+	    LorentzPolarizationVectorE vec2 = u2[ih2].dimensionedWave().slash(v3[ih3].wave()).vectorCurrent(ubar4[ih4].dimensionedWave());
+	    complex<Energy2> dot11 = vec2*rescaledMomenta()[0];
+	    complex<Energy> dot9 = vec1*v3[ih3].wave();
+       	    for(unsigned int ih1=0;ih1<2;++ih1) {
+	      complex<Energy> dot2 = rescaledMomenta()[1]*g1[ih1].wave();
+	      complex<Energy> dot4 = rescaledMomenta()[3]*g1[ih1].wave();
+	      Complex dot5 = g1[ih1].wave()*v3[ih3].wave();
+	      LorentzPolarizationVectorE vec3 = u2[ih2].dimensionedWave().slash(g1[ih1].wave()).vectorCurrent(ubar4[ih4].dimensionedWave());
+	      LorentzPolarizationVectorE vec4 = u2[ih2].dimensionedWave().slash(v3[ih3].wave()).slash(g1[ih1].wave()).vectorCurrent(ubar4[ih4].dimensionedWave());
+	      complex<Energy> dot8 = vec1*g1[ih1].wave();
+	      complex<Energy2> dot10 = vec4*rescaledMomenta()[0];
+	      complex<Energy> dot12 = vec2*g1[ih1].wave();
+	      complex<Energy2> dot13 = vec3*rescaledMomenta()[0];
 	      // now for the diagrams
 	      Complex diag[5];
-	      diag[0] = -64.*(amp7 - 2*amp5*dot1 + 2*amp2*dot2 + 2*amp4*dot3)*M2/sqr(M2-4*sh);
-	      diag[1] =  16.*M*(-4*amp6*(dot2 + 2*dot4) - 8*amp1*dot2*dot5 + 4*amp8*(dot3 + dot5) + 2*amp7*M - 2*amp4*dot3*M + 2*amp4*dot5*M + amp1*dot1*M2 - 2*amp3*sh + 2*amp3*uh - 4*amp1*dot1*uh)/((M2 - th)*(M2 - 4*uh));
-	      diag[2] =  16.*M*(4*amp6*(dot2 + 2*dot4) + 8*amp1*dot2*dot5 - 4*amp8*(dot3 + dot5) - 2*amp7*M + 4*amp5*dot1*M - 4*amp2*dot2*M - 2*amp4*dot3*M + 2*amp4*dot5*M - amp1*dot1*M2 + 2*amp3*sh - 2*amp3*uh + 4*amp1*dot1*uh)/((M2 - 4*sh)*(M2 - th));
-	      diag[3] =  64.*(amp7 + 2*amp4*dot5)*M2/sqr(M2 - 4*uh);
-	      diag[4] = -64.*M*(4*amp6*(dot2 + 2*dot4) + 8*amp1*dot2*dot5 - 4*amp8*(dot3 + dot5) + 2*amp4*dot3*M + 2*amp4*dot5*M - amp1*dot1*M2 + 2*amp3*sh - 2*amp3*uh + 4*amp1*dot1*uh)/((M2 - 4*sh)*(M2 - 4*uh));
+	      diag[0] = (-64.*(dot10 - 2.*dot5*dot7 + 2.*dot1*dot8 + 2.*dot2*dot9)*M2)/sqr(M2-4.*sh);
+	      diag[1] = (16.*M*(-8.*dot13*dot3 + 4.*dot11*(dot2 + dot4) - 4.*dot1*(dot13 + 2.*dot4*dot6) + M*(2.*dot10 - 2.*dot2*dot9 + 2.*dot4*dot9 + dot5*dot6*M) - 4.*dot5*dot6*uh + 2.*dot12*(-sh + uh)))/((M2 - th)*(M2 - 4.*uh));
+	      diag[2] = (16.*M*(8.*dot13*dot3 - 4.*dot11*(dot2 + dot4) - M*(2.*dot10 - 4.*dot5*dot7 + 2.*dot2*dot9 - 2.*dot4*dot9 + dot5*dot6*M) + 4.*dot1*(dot13 + 2.*dot4*dot6 - dot8*M) + 2.*dot12*(sh - uh) + 4.*dot5*dot6*uh))/((M2 - 4.*sh)*(M2 - th));
+	      diag[3] = (64.*(dot10 + 2.*dot4*dot9)*M2)/sqr(M2-4.*uh);
+	      diag[4] = (-64.*M*(8.*dot13*dot3 - 4.*dot11*(dot2 + dot4) + 4.*dot1*(dot13 + 2.*dot4*dot6) + M*(2.*(dot2 + dot4)*dot9 - dot5*dot6*M) + 2.*dot12*(sh - uh) + 4.*dot5*dot6*uh))/((M2 - 4.*sh)*(M2 - 4.*uh));
 	      Complex flow[2];
 	      flow[0] = 4.*diag[0]/3.+diag[1]+diag[4]+diag[2]/3.;
 	      flow[1] = 4.*diag[3]/3.+diag[2]-diag[4]+diag[1]/3.;
@@ -106,14 +107,72 @@ double MEPP2cc1Jet::me2() const {
 	  }
 	}
       }
-      // prefactors, spin and colour average
+      // // prefactors, spin and colour average
       me2Sum *= R02_*sqr(Constants::pi)*pow<3,1>(standardModel()->alphaS(scale())/M)/96.;
     }
-    else
-      assert(false);
+    // g qbar -> antidiquark q
+    else {
+      SpinorBarWaveFunction q2w(rescaledMomenta()[1],mePartonData()[1],incoming);
+      SpinorWaveFunction    q4w(rescaledMomenta()[3],mePartonData()[3],outgoing);
+      vector<SpinorBarWaveFunction> vbar2;
+      vector<SpinorWaveFunction> v4;
+      for(unsigned int ix=0;ix<2;++ix) {
+      	q2w.reset(ix);
+      	vbar2.push_back(q2w);
+      	q4w.reset(ix);
+      	v4.push_back(q4w);
+      }
+      me2Sum=0.;
+      // matrix element squared
+      // loop over the helicities
+      for(unsigned int ih2=0;ih2<2;++ih2) {
+       	for(unsigned int ih4=0;ih4<2;++ih4) {
+	  complex<Energy> dot6=v4[ih4].dimensionedWave().scalar(vbar2[ih2].dimensionedWave());
+	  LorentzPolarizationVectorE vec1 = v4[ih4].dimensionedWave().vectorCurrent(vbar2[ih2].dimensionedWave());
+	  complex<Energy2> dot9 = vec1*rescaledMomenta()[0];
+      	  for(unsigned int ih3=0;ih3<3;++ih3) {
+	    complex<Energy>  dot1 = rescaledMomenta()[0]*v3[ih3].wave();
+	    complex<Energy>  dot3 = rescaledMomenta()[1]*v3[ih3].wave();
+	    LorentzPolarizationVectorE vec2 = v4[ih4].dimensionedWave().slash(v3[ih3].wave()).vectorCurrent(vbar2[ih2].dimensionedWave());
+	    complex<Energy2> dot11 = vec2*rescaledMomenta()[0];
+	    complex<Energy> dot7 = vec1*v3[ih3].wave();
+       	    for(unsigned int ih1=0;ih1<2;++ih1) {
+	      complex<Energy>  dot2 = rescaledMomenta()[1]*g1[ih1].wave();
+	      complex<Energy>  dot4 = rescaledMomenta()[3]*g1[ih1].wave();
+	      Complex dot5 = g1[ih1].wave()*v3[ih3].wave();
+	      LorentzPolarizationVectorE vec3 = v4[ih4].dimensionedWave().slash(g1[ih1].wave()).vectorCurrent(vbar2[ih2].dimensionedWave());
+	      LorentzPolarizationVectorE vec4 = v4[ih4].dimensionedWave().slash(v3[ih3].wave()).slash(g1[ih1].wave()).vectorCurrent(vbar2[ih2].dimensionedWave());
+	      complex<Energy2> dot8  = vec4*rescaledMomenta()[0];
+	      complex<Energy>  dot10 = vec1*g1[ih1].wave();
+	      complex<Energy>  dot12 = vec2*g1[ih1].wave();
+	      complex<Energy2> dot13 = vec3*rescaledMomenta()[0];
+	      // now for the diagrams
+	      Complex diag[5];
+	      diag[0] = (64*(2*dot2*dot7 - dot8)*M2)/sqr(M2-4*sh);
+	      diag[1] = (16.*M*(8.*dot13*dot3 - 4.*dot11*(dot2 + dot4) + 4.*dot1*(dot13 + 2.*dot2*dot6 + dot10*M) + M*(2.*dot2*dot7 - 2.*dot4*dot7 + 2.*dot8 - 4.*dot5*dot9 + dot5*dot6*M) - 4.*dot5*dot6*sh + 2.*dot12*(sh - uh)))/((M2 - th)*(M2 - 4.*uh));
+	      diag[2] = (-16.*M*(8.*dot13*dot3 - 4.*dot11*(dot2 + dot4) + 4.*dot1*(dot13 + 2.*dot2*dot6) - 2.*dot2*dot7*M + 2.*dot4*dot7*M + 2.*dot8*M + dot5*dot6*M2 + 2.*dot12*sh - 4.*dot5*dot6*sh - 2.*dot12*uh))/((M2 - 4.*sh)*(M2 - th));
+	      diag[3] = (64.*(2.*dot1*dot10 - 2.*dot4*dot7 + dot8 - 2.*dot5*dot9)*M2)/sqr(M2-4.*uh);
+	      diag[4] = (64.*M*(8.*dot13*dot3 - 4.*dot11*(dot2 + dot4) + 4.*dot1*(dot13 + 2.*dot2*dot6) + M*(2.*(dot2 + dot4)*dot7 + dot5*dot6*M) - 4.*dot5*dot6*sh + 2.*dot12*(sh - uh)))/((M2 - 4.*sh)*(M2 - 4.*uh));
+	      Complex flow[2];
+	      flow[0] = 4.*diag[0]/3.+diag[1]+diag[4]+diag[2]/3.;
+	      flow[1] = 4.*diag[3]/3.+diag[2]-diag[4]+diag[1]/3.;
+	      for(unsigned int ix=0;ix<2;++ix) {
+		flows[ix]+=norm(flow[ix]);
+		for(unsigned int iy=0;iy<2;++iy) {
+		  me2Sum+=cMatrix[ix][iy]*flow[ix]*std::conj(flow[iy]);
+		}
+	      }
+	    }
+	  }
+	}
+      }
+      // // prefactors, spin and colour average
+      me2Sum *= R02_*sqr(Constants::pi)*pow<3,1>(standardModel()->alphaS(scale())/M)/96.;
+    }
   }
-  else
-    assert(false);
+  else {
+    me2Sum=0.;
+  }
   // return the answer
   meInfo(flows);
   return me2Sum.real();
