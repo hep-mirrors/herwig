2fb7996a8087164bc1841079317c0b14b984ac89
Peter Richardson <peter.richardson@durham.ac.uk>
2022-05-16 14:41:35 +0100

    add decayer PseudoVectorMesonVectorVectorDecayer
    --HG--
    branch : decay

---
+++ Decay/VectorMeson/PseudoVectorMesonVectorVectorDecayer.h
@@ -0,0 +1,211 @@
+// -*- C++ -*-
+//
+// PseudoVectorMesonVectorVectorDecayer.h is a part of Herwig - A multi-purpose Monte Carlo event generator
+// Copyright (C) 2002-2019 The Herwig Collaboration
+//
+// Herwig is licenced under version 3 of the GPL, see COPYING for details.
+// Please respect the MCnet academic guidelines, see GUIDELINES for details.
+//
+#ifndef HERWIG_PseudoVectorMesonVectorVectorDecayer_H
+#define HERWIG_PseudoVectorMesonVectorVectorDecayer_H
+//
+// This is the declaration of the PseudoVectorMesonVectorVectorDecayer class.
+//
+#include "Herwig/Decay/DecayIntegrator.h"
+#include "Herwig/Decay/PhaseSpaceMode.h"
+#include "ThePEG/Helicity/LorentzPolarizationVector.h"
+
+namespace Herwig {
+using namespace  ThePEG;
+
+/** \ingroup Decay
+ *
+ *  This class is designed for the decay of a vector meson to two spin
+ *  one particles, either other vector mesons or photons.
+ *
+ *  The current for the decay is taken to be
+ *
+ *  \f[\mathcal{M}= g_0 \epsilon_\alpha \epsilon^*_{1\beta}\epsilon^*_{2\gamma} \left[\epsilon^{p_0\alpha\beta\gamma} 
+ * -\frac1{m^2_0}{p_{\rm cm}^2}\left( E_1p_2^\beta\epsilon^{\alpha\gamma p_1p_2} -  E_2p_1^\gamma\epsilon^{\alpha\beta p_1p_2}     \right)\right]
+ * where \f$p_{0,1,2}\f$ are the momenta of the incoming and two outgoing
+ * vector mesons and \f$\epsilon_{1,2}\f$ are the polarization vectors of
+ * the outgoing mesons and \f$\epsilon\f$ is the polarization vector of the 
+ * incoming meson. 
+ *
+ *  The incoming vector mesons together with their decay products and the coupling 
+ *  \f$g\f$ can be specified using the interfaces for the class. The maximum weights
+ *  for the decays can be calculated using the Initialize interface of the
+ *  DecayIntegrator class or specified using the interface.
+ *
+ *  The incoming and outgoing particles, couplings and maximum weights for
+ *  many of the common \f$V\to VV\f$ decays are specified in the default
+ *  constructor.                                 
+ *
+ * @see DecayIntegrator.
+ * @see \ref PseudoVectorMesonVectorVectorDecayerInterfaces "The interfaces"
+ * defined for PseudoVectorMesonVectorVectorDecayer.
+ */
+class PseudoVectorMesonVectorVectorDecayer: public DecayIntegrator {
+
+public:
+
+  /**
+   * Default constructor.
+   */
+  PseudoVectorMesonVectorVectorDecayer();
+
+  /**
+   * Which of the possible decays is required
+   * @param cc Is this mode the charge conjugate
+   * @param parent The decaying particle
+   * @param children The decay products
+   */
+  virtual int modeNumber(bool & cc, tcPDPtr parent, 
+			 const tPDVector & children) const;
+
+  /**
+   * Specify the \f$1\to2\f$ matrix element to be used in the running width calculation.
+   * @param dm The DecayMode
+   * @param mecode The code for the matrix element as described
+   *               in the GenericWidthGenerator class, in this case 5.
+   * @param coupling The coupling for the matrix element.
+   * @return True or False if this mode can be handled.
+   */
+  bool twoBodyMEcode(const DecayMode & dm, int & mecode, double & coupling) const;
+
+  /**
+   * Return the matrix element squared for a given mode and phase-space channel.
+   * @param ichan The channel we are calculating the matrix element for. 
+   * @param part The decaying Particle.
+   * @param outgoing The particles produced in the decay
+   * @param momenta  The momenta of the particles produced in the decay
+   * @param meopt Option for the calculation of the matrix element
+   * @return The matrix element squared for the phase-space configuration.
+   */
+  double me2(const int ichan,const Particle & part,
+	     const tPDVector & outgoing,
+	     const vector<Lorentz5Momentum> & momenta,
+	     MEOption meopt) const;
+
+  /**
+   *   Construct the SpinInfos for the particles produced in the decay
+   */
+  virtual void constructSpinInfo(const Particle & part,
+				 ParticleVector outgoing) const;
+
+  /**
+   * Output the setup information for the particle database
+   * @param os The stream to output the information to
+   * @param header Whether or not to output the information for MySQL
+   */
+  virtual void dataBaseOutput(ofstream & os,bool header) const;
+
+public:
+
+  /** @name Functions used by the persistent I/O system. */
+  //@{
+  /**
+   * Function used to write out object persistently.
+   * @param os the persistent output stream written to.
+   */
+  void persistentOutput(PersistentOStream & os) const;
+
+  /**
+   * Function used to read in object persistently.
+   * @param is the persistent input stream read from.
+   * @param version the version number of the object when written.
+   */
+  void persistentInput(PersistentIStream & is, int version);
+  //@}
+
+  /**
+   * Standard Init function used to initialize the interfaces.
+   */
+  static void Init();
+
+protected:
+
+  /** @name Clone Methods. */
+  //@{
+  /**
+   * Make a simple clone of this object.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr clone() const {return new_ptr(*this);}
+  
+  /** Make a clone of this object, possibly modifying the cloned object
+   * to make it sane.
+   * @return a pointer to the new object.
+   */
+  virtual IBPtr fullclone() const {return new_ptr(*this);}
+  //@}
+  
+protected:
+
+  /** @name Standard Interfaced functions. */
+  //@{
+  /**
+   * Initialize this object after the setup phase before saving and
+   * EventGenerator to disk.
+   * @throws InitException if object could not be initialized properly.
+   */
+  virtual void doinit();
+
+  /**
+   * Initialize this object to the begining of the run phase.
+   */
+  virtual void doinitrun();
+  //@}
+
+private:
+
+  /**
+   * Private and non-existent assignment operator.
+   */
+  PseudoVectorMesonVectorVectorDecayer & operator=(const PseudoVectorMesonVectorVectorDecayer &) = delete;
+
+public:
+
+  /**
+   *   Set the parameters for a decay mode
+   */
+  string setUpDecayMode(string arg);
+
+private:
+
+  /**
+   * the coupling for the decay
+   */
+  vector<double> coupling_;
+
+  /**
+   * the PDG codes for the incoming particles
+   */
+  vector<int> incoming_;
+
+  /**
+   * the PDG codes for the 1st outgoing particle
+   */
+  vector<pair<long,long> > outgoing_;
+
+  /**
+   * maximum weight for a decay
+   */
+  vector<double> maxWeight_;
+
+  /**
+   *  Spin density matrix
+   */
+  mutable RhoDMatrix rho_;
+
+  /**
+   *  Polarization vectors
+   */
+  mutable vector<Helicity::LorentzPolarizationVector> vectors_[3];
+
+};
+
+}
+
+
+#endif /* HERWIG_PseudoVectorMesonVectorVectorDecayer_H */
