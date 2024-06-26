11a90a8b574196d67ee03f8aa879dff6142e4fa4
Peter Richardson ext:(%5Bui%5D) <peter.richardson@durham.ac.uk>
2024-06-26 18:32:49 +0100

    more photon tests

---
+++ Tests/Analysis/MC_Simple_Onium.cc
@@ -0,0 +1,69 @@
+// -*- C++ -*-
+#include "Rivet/Analysis.hh"
+#include "Rivet/Projections/FinalState.hh"
+#include "Rivet/Projections/UnstableParticles.hh"
+
+namespace Rivet {
+
+
+  /// @brief MC analysis for onium spectra in e+e-
+  class MC_Simple_Onium : public Analysis {
+  public:
+
+    /// Constructor
+    RIVET_DEFAULT_ANALYSIS_CTOR(MC_Simple_Onium);
+
+
+    /// @name Analysis methods
+    ///@{
+
+    /// Book histograms and initialise projections before the run
+    void init() {
+
+      // Initialise and register projections
+
+      declare(UnstableParticles(),"UFS");
+      // Book histograms
+      vector<long> states ={441,10441,100441,443,10443,20443,100443,30443,445,100445,20445,447,
+			    551,10551,100551,110551,200551,210551,553,10553,20553,100553,30553,110553,
+			    120553,130553,200553,210553,220553,300553,555,10555,20555,100555,120555,200555,557,100557,
+			    541,10541,543,10543,20543,545,10545,20545,547,100541,100543,30543,
+			    4403,5503,5401,5403};
+      for( long pid : states ) {
+	book(_h[pid],"h_"+to_string(pid),100,0.,1.);
+      }
+    }
+
+
+    /// Perform the per-event analysis
+    void analyze(const Event& event) {
+      for(const Particle & p : apply<UnstableParticles>(event,"UFS").particles()) {
+	map<long,Histo1DPtr>::const_iterator loc = _h.find(p.abspid());
+	if(loc!=_h.end())
+	 loc->second->fill(2.*p.momentum().E()/sqrtS());
+      }
+    }
+
+
+    /// Normalise histograms etc., after the run
+    void finalize() {
+      for (const auto & kv : _h) {
+	scale(kv.second,0.5/sumW());
+      }
+    }
+
+    ///@}
+
+
+    /// @name Histograms
+    ///@{
+    map<long,Histo1DPtr> _h;
+    ///@}
+
+
+  };
+
+
+  RIVET_DECLARE_PLUGIN(MC_Simple_Onium);
+
+}
