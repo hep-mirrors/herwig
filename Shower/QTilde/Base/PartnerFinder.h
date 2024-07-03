de2b9909309cf01ef9476cf544f6effe57b72fd2
Dominic Stafford <dominic.william.stafford@cern.ch>
2024-07-03 11:39:43 +0200

    Don't do EWK shower from dark sector
    --HG--
    branch : hadronspectrum

--- Shower/QTilde/Base/PartnerFinder.h
+++ Shower/QTilde/Base/PartnerFinder.h
@@ -244,8 +244,8 @@ protected:
       is = true;
     if ((id>=1 && id<=6 ) || (id>=11 && id<=16))
       is = true;
-    // for SUSY and excited particles
-    if(id > 25) {
+    // for SUSY and excited particles (but not dark sector)
+    if(id > 25 && (id < 4900000 || id > 4999999)) {
       long bsm_id = id % 1000000;
       if ( bsm_id==ParticleID::Wplus || bsm_id ==ParticleID::Z0 || bsm_id ==ParticleID::h0 || bsm_id ==ParticleID::gamma)
         is = true;
